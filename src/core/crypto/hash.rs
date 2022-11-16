22b3b008840a7ff0e19b0f66642f57cf5cecbeae
Preston Evans <preston.b.evans@gmail.com>
2022-11-16 17:53:31 -0600

    Add sha2 implementation

--- src/core/crypto/hash.rs
+++ src/core/crypto/hash.rs
@@ -1,6 +1,31 @@
+use sha2::{Digest, Sha256};
+
+use crate::core::traits::AsBytes;
+
 pub type DefaultHash = Sha2Hash;
 
 /// The output of a sha2-256 hash
 ///
-#[derive(Debug, Default, Clone)]
-pub struct Sha2Hash([u8; 32]);
+#[derive(Debug, Default, Clone, PartialEq, Eq, Hash)]
+pub struct Sha2Hash(pub [u8; 32]);
+
+pub fn sha2(item: &[u8]) -> Sha2Hash {
+    let mut hasher = Sha256::new();
+    hasher.update(item);
+
+    let mut output = Sha2Hash::default();
+    output.0.copy_from_slice(&hasher.finalize()[..]);
+    output
+}
+
+#[test]
+fn test_sha2() {
+    let res = sha2(b"hello world");
+    assert_eq!(
+        res.0,
+        [
+            185, 77, 39, 185, 147, 77, 62, 8, 165, 46, 82, 215, 218, 125, 171, 250, 196, 132, 239,
+            227, 122, 83, 128, 238, 144, 136, 247, 172, 226, 239, 205, 233
+        ]
+    )
+}
