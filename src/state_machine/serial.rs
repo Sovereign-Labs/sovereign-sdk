3bae5f520049cacb96c1440e812e85d60ed96ed1
Preston Evans <preston.b.evans@gmail.com>
2023-02-01 16:43:58 -0600

    Implement Encode/Decode for wrapper types
    The following types are blanket implemented when wrapping types that
    implement Encode/Decode
    
    - Arc<T> (requires "sync" feature)
    - Rc<T>
    - 2-tuple: (T, U)
    - Vec<T>

--- src/state_machine/serial.rs
+++ src/state_machine/serial.rs
@@ -35,11 +35,10 @@ pub trait Decode: Sized {
     fn decode(target: &mut &[u8]) -> Result<Self, Self::Error>;
 }
 
-// Automatically implement encode for smart pointers
-impl<T, U> Encode for T
+#[cfg(feature = "sync")]
+impl<T> Encode for Arc<T>
 where
-    T: Deref<Target = U>,
-    U: Encode,
+    T: Encode,
 {
     fn encode(&self, target: &mut impl std::io::Write) {
         self.deref().encode(target)
@@ -58,6 +57,15 @@ where
     }
 }
 
+impl<T> Encode for Rc<T>
+where
+    T: Encode,
+{
+    fn encode(&self, target: &mut impl std::io::Write) {
+        self.deref().encode(target)
+    }
+}
+
 impl<T, E> Decode for Rc<T>
 where
     T: Decode<Error = E>,
@@ -66,5 +74,67 @@ where
 
     fn decode(target: &mut &[u8]) -> Result<Self, Self::Error> {
         Ok(Rc::new(T::decode(target)?))
+    }
+}
+
+impl<T: Encode> Encode for Vec<T> {
+    fn encode(&self, target: &mut impl std::io::Write) {
+        target
+            .write_all(&(self.len() as u64).to_le_bytes())
+            .expect("Serialization should not fail");
+        for item in self.iter() {
+            item.encode(target)
+        }
+    }
+}
+
+impl<T: Decode<Error = E>, E> Decode for Vec<T>
+where
+    DeserializationError: From<E>,
+{
+    type Error = DeserializationError;
+
+    fn decode(target: &mut &[u8]) -> Result<Self, Self::Error> {
+        if target.len() < 8 {
+            return Err(DeserializationError::DataTooShort {
+                expected: 8,
+                got: target.len(),
+            });
+        }
+        let mut serialized_len = [0u8; 8];
+        serialized_len.copy_from_slice(&target[..8]);
+        let len: u64 = u64::from_le_bytes(serialized_len);
+        *target = &mut &target[8..];
+
+        let mut out = Vec::new();
+        for _ in 0..len {
+            out.push(T::decode(target)?)
+        }
+        Ok(out)
+    }
+}
+
+impl<T, U> Encode for (T, U)
+where
+    T: Encode,
+    U: Encode,
+{
+    fn encode(&self, target: &mut impl std::io::Write) {
+        self.0.encode(target);
+        self.1.encode(target)
+    }
+}
+
+impl<T, U, E1, E2> Decode for (T, U)
+where
+    T: Decode<Error = E1>,
+    U: Decode<Error = E2>,
+    DeserializationError: From<E1>,
+    DeserializationError: From<E2>,
+{
+    type Error = DeserializationError;
+
+    fn decode(target: &mut &[u8]) -> Result<Self, Self::Error> {
+        Ok((T::decode(target)?, U::decode(target)?))
     }
 }
