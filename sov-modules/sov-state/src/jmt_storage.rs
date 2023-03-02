e52816f880fbab40999035c5de9e82fdb9c812b9
bkolad <blazejkolad@gmail.com>
2023-03-02 14:18:14 +0100

    better doc in jmt_storage.rs

--- sov-modules/sov-state/src/jmt_storage.rs
+++ sov-modules/sov-state/src/jmt_storage.rs
@@ -9,6 +9,7 @@ impl ValueReader for StateDB {
     fn read_value(&self, key: StorageKey) -> Option<StorageValue> {
         match self.get_value_option_by_key(0, key.as_ref()) {
             Ok(value) => value.map(StorageValue::new_from_bytes),
+            // It is ok to panic here, we assume the db is available and consistent.
             Err(e) => panic!("Unable to read value from db: {e}"),
         }
     }
