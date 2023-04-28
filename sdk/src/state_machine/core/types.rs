feb1f90c3c17db7d3bc1f8df5456b40702b21546
Preston Evans <preston.b.evans@gmail.com>
2023-04-28 16:58:20 -0500

    Update zkvm trait docs. Encode/Decode ArrayWitness

--- sdk/src/state_machine/core/types.rs
+++ sdk/src/state_machine/core/types.rs
@@ -1,11 +1,43 @@
 use std::{cell::RefCell, sync::atomic::AtomicUsize};
 
+use borsh::maybestd;
+
+use crate::serial::{Decode, DecodeBorrowed, Encode};
+
 use super::traits::Witness;
 
-#[derive(Default)]
+#[derive(Default, Debug)]
 pub struct ArrayWitness {
     next_idx: AtomicUsize,
     hints: RefCell<Vec<Vec<u8>>>,
+}
+
+impl Encode for ArrayWitness {
+    fn encode(&self, target: &mut impl std::io::Write) {
+        let hints = self.hints.borrow();
+        let next_idx = self.next_idx.load(std::sync::atomic::Ordering::SeqCst);
+        hints[next_idx..].as_ref().encode(target);
+    }
+}
+
+impl<'de> DecodeBorrowed<'de> for ArrayWitness {
+    type Error = maybestd::io::Error;
+
+    fn decode_from_slice(target: &'de [u8]) -> Result<Self, Self::Error> {
+        Self::decode(&mut std::io::Cursor::new(target))
+    }
+}
+
+impl Decode for ArrayWitness {
+    type Error = maybestd::io::Error;
+
+    fn decode<R: std::io::Read>(target: &mut R) -> Result<Self, <Self as Decode>::Error> {
+        let hints = Vec::<Vec<u8>>::decode(target)?;
+        Ok(ArrayWitness {
+            next_idx: AtomicUsize::new(0),
+            hints: RefCell::new(hints),
+        })
+    }
 }
 
 impl Witness for ArrayWitness {
