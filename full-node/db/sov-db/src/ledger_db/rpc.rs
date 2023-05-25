a4033376a07797af971fb8ac32555358f1e2ee47
Preston Evans <preston.b.evans@gmail.com>
2023-05-25 12:27:18 -0500

    WIP: update RPC

--- full-node/db/sov-db/src/ledger_db/rpc.rs
+++ full-node/db/sov-db/src/ledger_db/rpc.rs
@@ -26,6 +26,7 @@ const MAX_EVENTS_PER_REQUEST: u64 = 500;
 
 use super::LedgerDB;
 
+
 impl LedgerRpcProvider for LedgerDB {
     fn get_slots<B: DeserializeOwned, T: DeserializeOwned>(
         &self,
