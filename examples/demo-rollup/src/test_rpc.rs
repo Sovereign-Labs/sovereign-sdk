b95680977dd0bc1a86f06e2a1f1f5b15b811a8da
Theodore Chapuis <theodore.chap@gmail.com>
2023-06-06 10:42:46 -0600

    Tests rpc

--- examples/demo-rollup/src/test_rpc.rs
+++ examples/demo-rollup/src/test_rpc.rs
@@ -1,25 +1,37 @@
-use std::{net::SocketAddr, path::PathBuf, time::Duration};
+use std::{
+    collections::hash_map::DefaultHasher,
+    hash::{self, Hash, Hasher},
+    net::SocketAddr,
+    path::PathBuf,
+    time::Duration,
+};
 
-use demo_stf::app::{DemoBatchReceipt, DemoTxReceipt, NativeAppRunner};
+use demo_stf::app::{get_rpc_methods, DemoBatchReceipt, DemoTxReceipt, NativeAppRunner};
 use jupiter::{
     da_service::{CelestiaService, DaServiceConfig},
     verifier::RollupParams,
 };
 use risc0_adapter::host::Risc0Host;
-use sov_rollup_interface::{services::da::DaService, stf::StateTransitionRunner};
-use sov_state::config::Config;
-use tokio::{process::Command, time::sleep};
-use tracing::Level;
+use sha2::digest::typenum::private::PrivateAnd;
+use sov_db::ledger_db::{LedgerDB, SlotCommit};
+use sov_modules_api::RpcRunner;
+use sov_rollup_interface::{
+    services::da::DaService,
+    stf::{BatchReceipt, StateTransitionFunction, StateTransitionRunner},
+};
+use sov_state::{config::Config, Storage};
+use tracing::{debug, info, Level};
 
 use demo_stf::runner_config::Config as RunnerConfig;
 use demo_stf::runner_config::{from_toml_path, StorageConfig};
 
 use crate::{
     config::{RollupConfig, RpcConfig},
-    initialize_ledger, ledger_rpc, start_rpc_server, ROLLUP_NAMESPACE,
+    get_genesis_config, initialize_ledger, ledger_rpc, start_rpc_server, ROLLUP_NAMESPACE,
 };
 
 use curl::easy::{Easy2, Handler, List, WriteError};
+use tokio::{sync::oneshot, time::sleep};
 
 struct Collector(Vec<u8>);
 
@@ -47,53 +59,58 @@ fn query_test_helper(data: &[u8], expected: &str) {
     assert_eq!(String::from_utf8_lossy(&contents.0), expected);
 }
 
+fn populate_ledger(ledger_db: LedgerDB) {
+    let mut hasher = DefaultHasher::new();
+    1.hash(&mut hasher);
+
+    let slot: SlotCommit<&str> = SlotCommit {
+        slot_data: "Hello word",
+        batch_receipts: vec![BatchReceipt {
+            batch_hash: hasher.finish(),
+        }],
+    };
+    ledger_db.commit_slot(slot);
+}
+
 #[tokio::test]
-async fn readme_test_rpc() {
-    //celestia light start --core.ip https://limani.celestia-devops.dev --p2p.network arabica --gateway --rpc.port 11111
-    let mut celestia_task = Command::new("celestia")
-        .arg("light")
-        .arg("start")
-        .arg("--core.ip")
-        .arg("https://limani.celestia-devops.dev")
-        .arg("--p2p.network")
-        .arg("arabica")
-        .arg("--gateway")
-        .arg("--rpc.port")
-        .arg("11111")
-        .spawn()
-        .expect("failed to execute process");
+async fn simple_test_rpc() {
+    let (tx, rx) = oneshot::channel();
+    let rpc_config = RpcConfig {
+        bind_host: "127.0.0.1".to_string(),
+        bind_port: 12345,
+    };
 
-    sleep(Duration::from_secs(10)).await;
+    let address = SocketAddr::new(rpc_config.bind_host.parse().unwrap(), rpc_config.bind_port);
 
-    let mut rpc_server = Command::new("cargo")
-        .arg("run")
-        .spawn()
-        .expect("failed to execute process");
+    // Initialize the ledger database, which stores blocks, transactions, events, etc.
+    let ledger_db = LedgerDB::temporary();
 
-    sleep(Duration::from_secs(10)).await;
+    let ledger_rpc_module =
+        ledger_rpc::get_ledger_rpc::<DemoBatchReceipt, DemoTxReceipt>(ledger_db.clone());
+
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .enable_io()
+        .enable_time()
+        .build()
+        .unwrap();
+
+    let _handle = rt.spawn(async move {
+        let server = jsonrpsee::server::ServerBuilder::default()
+            .build([address].as_ref())
+            .await
+            .unwrap();
+        let _server_handle = server.start(ledger_rpc_module).unwrap();
+        tx.send("server started").unwrap();
+        futures::future::pending::<()>().await;
+    });
+
+    // Wait for the server to start
+    rx.await.unwrap();
 
     let data = r#"{"jsonrpc":"2.0","method":"ledger_getSlots","params":[[7], "Compact"],"id":1}"#
         .as_bytes();
     let expected = r#"{"jsonrpc":"2.0","result":[{"number":7,"hash":"0x4083ae3bf35acdcfe6e6d78841bbab2b28b8e051b1e3f89ea01a9bf740dd4d67","batch_range":{"start":2,"end":2}}],"id":1}"#;
     query_test_helper(data, expected);
 
-    let data = r#"{"jsonrpc":"2.0","method":"ledger_getBatches","params":[["0xf784a42555ed652ed045cc8675f5bc11750f1c7fb0fbc8d6a04470a88c7e1b6c"]],"id":1}"#
-        .as_bytes();
-    let expected = r#"{"jsonrpc":"2.0","result":[{"hash":"0xf784a42555ed652ed045cc8675f5bc11750f1c7fb0fbc8d6a04470a88c7e1b6c","tx_range":{"start":1,"end":1},"txs":[],"custom_receipt":{"Slashed":"InvalidTransactionEncoding"}}],"id":1}"#;
-    query_test_helper(data, expected);
-
-    let data = r#"{"jsonrpc":"2.0","method":"ledger_getTransactions","params":[[{ "batch_id": 1, "offset": 0}]],"id":1}"#
-        .as_bytes();
-    let expected = r#"{"jsonrpc":"2.0","result":[null],"id":1}"#;
-    query_test_helper(data, expected);
-
-    let data = r#"{"jsonrpc":"2.0","method":"ledger_getEvents","params":[1],"id":1}"#.as_bytes();
-    let expected = r#"{"jsonrpc":"2.0","result":[null],"id":1}"#;
-    query_test_helper(data, expected);
-
-    rpc_server.kill().await.unwrap();
-    println!("Killed the rpc server");
-
-    celestia_task.kill().await.unwrap();
-    println!("Killed the celestia server");
+    println!("Sent request");
 }
