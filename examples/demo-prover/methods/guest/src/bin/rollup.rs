dbf8a199af632afb0869b8a90b2a555134462f1d
Theodore Chapuis <theodore.chap@gmail.com>
2023-06-12 17:43:46 -0600

    Fix demo-prover

--- examples/demo-prover/methods/guest/src/bin/rollup.rs
+++ examples/demo-prover/methods/guest/src/bin/rollup.rs
@@ -72,7 +72,7 @@ fn state_transition(guest: &Risc0Guest, batches: Vec<BlobWithSender>) {
     demo.begin_slot(witness);
     env::write(&"Slot has begun\n");
     for batch in batches {
-        demo.apply_blob(&batch, None);
+        demo.apply_blob(&mut batch, None);
         env::write(&"Blob applied\n");
     }
     let (state_root, _) = demo.end_slot();
