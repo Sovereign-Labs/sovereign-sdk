cca965d164afd40443de9263d4034212e5890f58
bkolad <blazejkolad@gmail.com>
2023-06-29 10:21:06 +0200

    Add comment

--- module-system/module-implementations/sov-evm/src/call.rs
+++ module-system/module-implementations/sov-evm/src/call.rs
@@ -32,6 +32,7 @@ impl<C: sov_modules_api::Context> Evm<C> {
         let block_env = self.block_env.get(working_set).unwrap_or_default();
         let evm_db: EvmDb<'_, C> = self.get_db(working_set);
 
+        // It is ok to use the unwrap here because the error type is `Infallible`.
         executor::execute_tx(evm_db, block_env, tx, cfg_env).unwrap();
         Ok(CallResponse::default())
     }
