bebec029371b4f6b9df36d4c8824a1a0b45a3563
Nikolai Golub <nikolai@sovlabs.io>
2023-07-04 15:22:08 +0200

    Comment tx verification in demo-prover so it works with latest main

--- examples/demo-prover/methods/guest/src/bin/rollup.rs
+++ examples/demo-prover/methods/guest/src/bin/rollup.rs
@@ -6,16 +6,17 @@ use const_rollup_config::ROLLUP_NAMESPACE_RAW;
 use demo_stf::app::ZkAppRunner;
 use demo_stf::ArrayWitness;
 use jupiter::types::NamespaceId;
-use jupiter::verifier::{CelestiaSpec, CelestiaVerifier};
+use jupiter::verifier::{CelestiaSpec, CelestiaVerifier, ChainValidityCondition};
 use jupiter::{BlobWithSender, CelestiaHeader};
 use log::info;
 use risc0_adapter::guest::Risc0Guest;
 use risc0_zkvm::guest::env;
-use sov_rollup_interface::crypto::NoOpHasher;
+// use sov_rollup_interface::crypto::NoOpHasher;
 use sov_rollup_interface::da::{DaSpec, DaVerifier};
 use sov_rollup_interface::services::stf_runner::StateTransitionRunner;
 use sov_rollup_interface::stf::{StateTransitionFunction, ZkConfig};
-use sov_rollup_interface::zk::traits::{StateTransition, ValidityCondition, ZkvmGuest};
+// use sov_rollup_interface::zk::traits::{StateTransition, ValidityCondition, ZkvmGuest};
+use sov_rollup_interface::zk::traits::{StateTransition, ZkvmGuest};
 
 // The rollup stores its data in the namespace b"sov-test" on Celestia
 const ROLLUP_NAMESPACE: NamespaceId = NamespaceId(ROLLUP_NAMESPACE_RAW);
@@ -35,30 +36,31 @@ pub fn main() {
     info!("Should not be printed from guest");
     let guest = Risc0Guest;
 
-    let verifier = CelestiaVerifier::new(jupiter::verifier::RollupParams {
+    let _verifier = CelestiaVerifier::new(jupiter::verifier::RollupParams {
         namespace: ROLLUP_NAMESPACE,
     });
     // Step 1: read tx list
-    let header: CelestiaHeader = guest.read_from_host();
+    let _header: CelestiaHeader = guest.read_from_host();
     env::write(&"header read\n");
     let txs: Vec<BlobWithSender> = guest.read_from_host();
     env::write(&"txs read\n");
-    let inclusion_proof: <CelestiaSpec as DaSpec>::InclusionMultiProof = guest.read_from_host();
-    let completeness_proof: <CelestiaSpec as DaSpec>::CompletenessProof = guest.read_from_host();
+    let _inclusion_proof: <CelestiaSpec as DaSpec>::InclusionMultiProof = guest.read_from_host();
+    let _completeness_proof: <CelestiaSpec as DaSpec>::CompletenessProof = guest.read_from_host();
 
     // Step 2: Verify tx list
-    let validity_condition = verifier
-        .verify_relevant_tx_list::<NoOpHasher>(&header, &txs, inclusion_proof, completeness_proof)
-        .expect("Transaction list must be correct");
-    env::write(&"Relevant txs verified\n");
-
-    state_transition(&guest, txs, validity_condition);
+    // TODO: uncomment when https://github.com/Sovereign-Labs/sovereign-sdk/issues/456 is resolved
+    // let validity_condition = verifier
+    //     .verify_relevant_tx_list::<NoOpHasher>(&header, &txs, inclusion_proof, completeness_proof)
+    //     .expect("Transaction list must be correct");
+    // env::write(&"Relevant txs verified\n");
+    // state_transition(&guest, txs, validity_condition);
+    state_transition(&guest, txs);
 }
 
 fn state_transition(
     guest: &Risc0Guest,
     batches: Vec<BlobWithSender>,
-    validity_condition: impl ValidityCondition,
+    // validity_condition: impl ValidityCondition,
 ) {
     let prev_state_root_hash: [u8; 32] = guest.read_from_host();
     env::write(&"Prev root hash read\n");
@@ -81,6 +83,10 @@ fn state_transition(
     }
     let (state_root, _) = demo.end_slot();
     env::write(&"Slot has ended\n");
+    let validity_condition = ChainValidityCondition {
+        prev_hash: prev_state_root_hash,
+        block_hash: [0; 32],
+    };
     let output = StateTransition {
         initial_state_root: prev_state_root_hash,
         final_state_root: state_root.0,
