1fd472fe342d759ed6e6fef38528c6b06b2aed18
dubbelosix <dub@006.com>
2023-07-05 14:26:18 +0200

    add new file benches/test_helper.rs

---
+++ examples/demo-rollup/benches/test_helper.rs
@@ -0,0 +1,10 @@
+use std::io;
+use std::fs;
+
+pub fn remove_dir_if_exists<P: AsRef<std::path::Path>>(path: P) -> io::Result<()> {
+    if path.as_ref().exists() {
+        fs::remove_dir_all(&path)
+    } else {
+        Ok(())
+    }
+}
