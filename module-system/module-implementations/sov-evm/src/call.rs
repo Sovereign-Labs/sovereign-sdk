c5a87ca35bed0acdeca8ea14f23bf268a9f971d3
bkolad <blazejkolad@gmail.com>
2023-07-10 17:09:01 +0200

    remove unused variable

--- module-system/module-implementations/sov-evm/src/call.rs
+++ module-system/module-implementations/sov-evm/src/call.rs
@@ -30,7 +30,7 @@ impl<C: sov_modules_api::Context> Evm<C> {
         let evm_db: EvmDb<'_, C> = self.get_db(working_set);
 
         // It is ok to use the unwrap here because the error type is `Infallible`.
-        let _res = executor::execute_tx(evm_db, block_env, tx, cfg_env).unwrap();
+        executor::execute_tx(evm_db, block_env, tx, cfg_env).unwrap();
         Ok(CallResponse::default())
     }
 }
