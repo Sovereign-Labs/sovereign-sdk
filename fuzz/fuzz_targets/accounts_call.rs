f4d015b78f9fab26991564cda216e85400578cac
Filippo Costa <filippo@sovlabs.io>
2023-07-14 10:38:46 +0200

    wip

---
+++ fuzz/fuzz_targets/accounts_call.rs
@@ -0,0 +1,30 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+use sov_accounts::call::CallMessage;
+use sov_accounts::Accounts;
+use sov_modules_api::default_context::DefaultContext;
+use sov_modules_api::Module;
+use sov_state::{ProverStorage, WorkingSet};
+
+type C = DefaultContext;
+
+fuzz_target!(|input: &[u8]| {
+    if input.len() < 32 {
+        return;
+    }
+    let (sender_bytes, data) = input.split_at(32);
+    let sender: [u8; 32] = sender_bytes.try_into().unwrap();
+    if let Ok(msgs) = serde_json::from_slice::<Vec<CallMessage<C>>>(data) {
+        let tmpdir = tempfile::tempdir().unwrap();
+        let ctx = C {
+            sender: sender.into(),
+        };
+        let mut working_set = WorkingSet::new(ProverStorage::with_path(tmpdir.path()).unwrap());
+
+        let accounts = Accounts::default();
+        for msg in msgs {
+            accounts.call(msg, &ctx, &mut working_set).ok();
+        }
+    }
+});
---
+++ fuzz/fuzz_targets/bank_call.rs
@@ -0,0 +1,26 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+use sov_bank::call::CallMessage;
+use sov_bank::Bank;
+use sov_modules_api::default_context::DefaultContext;
+use sov_modules_api::Module;
+use sov_state::{ProverStorage, WorkingSet};
+
+type C = DefaultContext;
+
+fuzz_target!(|input: (&[u8], [u8; 32])| {
+    let (data, sender) = input;
+    if let Ok(msgs) = serde_json::from_slice::<Vec<CallMessage<C>>>(data) {
+        let tmpdir = tempfile::tempdir().unwrap();
+        let mut working_set = WorkingSet::new(ProverStorage::with_path(tmpdir.path()).unwrap());
+        let ctx = C {
+            sender: sender.into(),
+        };
+
+        let bank = Bank::default();
+        for msg in msgs {
+            bank.call(msg, &ctx, &mut working_set).ok();
+        }
+    }
+});
