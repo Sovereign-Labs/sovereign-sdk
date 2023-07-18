1aaeb3d0aa84fd0a0a92d66a35e923e3aa245060
Nikolai Golub <nikolay.v.golub@gmail.com>
2023-07-18 15:29:25 +0200

    More tests + readme

--- module-system/module-implementations/sov-blob-storage/src/README.md
+++ module-system/module-implementations/sov-blob-storage/src/README.md
@@ -1,1 +1,5 @@
-# Blob Storage
+# sov-blob-storage
+
+Blob storage module allows to store arbitrary blobs in state of the module 
+
+Caller is responsible for correct match between block number and actual blob, as well as for order of blobs.
--- module-system/module-implementations/sov-blob-storage/tests/blob_storage_tests.rs
+++ module-system/module-implementations/sov-blob-storage/tests/blob_storage_tests.rs
@@ -4,7 +4,6 @@ use sov_modules_api::Genesis;
 use sov_state::{ProverStorage, WorkingSet};
 
 type C = DefaultContext;
-// type Storage = ProverStorage<DefaultStorageSpec>;
 
 #[test]
 fn empty_test() {
@@ -26,7 +25,7 @@ fn empty_test() {
 }
 
 #[test]
-fn store_and_iterate_standard() {
+fn store_and_retrieve_standard() {
     let tmpdir = tempfile::tempdir().unwrap();
     let mut working_set = WorkingSet::new(ProverStorage::with_path(tmpdir.path()).unwrap());
     let blob_storage = BlobStorage::<C>::default();
@@ -40,8 +39,8 @@ fn store_and_iterate_standard() {
     let blob_1 = vec![1, 2, 3];
     let blob_2 = vec![3, 4, 5];
     let blob_3 = vec![6, 7, 8];
-    let blob_4 = vec![6, 7, 8];
-    let blob_5 = vec![6, 7, 8];
+    let blob_4 = vec![9, 9, 9];
+    let blob_5 = vec![0, 1, 0];
 
     blob_storage
         .store_blob(2, blob_1.clone(), &mut working_set)
@@ -72,4 +71,63 @@ fn store_and_iterate_standard() {
         .unwrap();
 
     assert_eq!(vec![blob_1, blob_2, blob_3], blobs);
+    assert_eq!(
+        Some(3),
+        blob_storage.earliest_stored_block_number(&mut working_set)
+    );
+}
+
+#[test]
+fn store_and_retrieve_out_of_order() {
+    let tmpdir = tempfile::tempdir().unwrap();
+    let mut working_set = WorkingSet::new(ProverStorage::with_path(tmpdir.path()).unwrap());
+    let blob_storage = BlobStorage::<C>::default();
+
+    blob_storage.genesis(&(), &mut working_set).unwrap();
+
+    let blob_1 = vec![1, 2, 3];
+    let blob_2 = vec![3, 4, 5];
+    let blob_3 = vec![6, 7, 8];
+    let blob_4 = vec![9, 9, 9];
+    let blob_5 = vec![0, 1, 0];
+
+    blob_storage
+        .store_blob(2, blob_1.clone(), &mut working_set)
+        .unwrap();
+    blob_storage
+        .store_blob(1, blob_2.clone(), &mut working_set)
+        .unwrap();
+    assert_eq!(
+        Some(2),
+        blob_storage.earliest_stored_block_number(&mut working_set)
+    );
+    blob_storage
+        .store_blob(2, blob_3.clone(), &mut working_set)
+        .unwrap();
+    blob_storage
+        .store_blob(1, blob_4.clone(), &mut working_set)
+        .unwrap();
+    blob_storage
+        .store_blob(2, blob_5.clone(), &mut working_set)
+        .unwrap();
+    assert_eq!(
+        Some(2),
+        blob_storage.earliest_stored_block_number(&mut working_set)
+    );
+
+    let blobs_1 = blob_storage
+        .get_blobs_for_block_number(1, &mut working_set)
+        .unwrap();
+    assert_eq!(
+        Some(2),
+        blob_storage.earliest_stored_block_number(&mut working_set)
+    );
+    assert_eq!(vec![blob_2, blob_4], blobs_1);
+    let blobs_2 = blob_storage
+        .get_blobs_for_block_number(2, &mut working_set)
+        .unwrap();
+    assert_eq!(vec![blob_1, blob_3, blob_5], blobs_2);
+    assert!(blob_storage
+        .earliest_stored_block_number(&mut working_set)
+        .is_none());
 }
