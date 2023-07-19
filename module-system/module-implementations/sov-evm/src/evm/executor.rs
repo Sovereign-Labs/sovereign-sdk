3b77d2c1e3f847e62caa7269f365a3f59ab43aba
Blazej Kolad <blazejkolad@gmail.com>
2023-07-19 09:00:55 +0200

    EVM: `get_transaction_receipt & eth_call` endpoints implementation.  (#513)

--- module-system/module-implementations/sov-evm/src/evm/executor.rs
+++ module-system/module-implementations/sov-evm/src/evm/executor.rs
@@ -1,6 +1,7 @@
 use std::convert::Infallible;
 
-use revm::primitives::{CfgEnv, EVMError, Env, ExecutionResult};
+use anvil::eth::backend::mem::inspector::Inspector;
+use revm::primitives::{CfgEnv, EVMError, Env, ExecutionResult, ResultAndState};
 use revm::{self, Database, DatabaseCommit};
 
 use super::transaction::{BlockEnv, EvmTransaction};
@@ -22,4 +23,25 @@ pub(crate) fn execute_tx<DB: Database<Error = Infallible> + DatabaseCommit>(
     evm.env = env;
     evm.database(db);
     evm.transact_commit()
+}
+
+pub(crate) fn inspect<DB: Database<Error = Infallible> + DatabaseCommit>(
+    db: DB,
+    block_env: BlockEnv,
+    tx: EvmTransaction,
+    config_env: CfgEnv,
+) -> Result<ResultAndState, EVMError<Infallible>> {
+    let mut evm = revm::new();
+
+    let env = Env {
+        cfg: config_env,
+        block: block_env.into(),
+        tx: tx.into(),
+    };
+
+    evm.env = env;
+    evm.database(db);
+
+    let mut inspector = Inspector::default();
+    evm.inspect(&mut inspector)
 }
