41b114ecc9ada3273a1a41b40fa01b2bcc5ff8be
bkolad <blazejkolad@gmail.com>
2023-08-01 13:32:12 +0200

    formatting

--- module-system/sov-modules-api/src/utils.rs
+++ module-system/sov-modules-api/src/utils.rs
@@ -1,5 +1,6 @@
 use jsonrpsee::types::ErrorObjectOwned;
 
+///
 pub fn to_jsonrpsee_error_object(err: impl ToString, message: &str) -> ErrorObjectOwned {
     ErrorObjectOwned::owned(
         jsonrpsee::types::error::UNKNOWN_ERROR_CODE,
