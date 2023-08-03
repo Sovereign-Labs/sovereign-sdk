26aa6827148ea01236b3387db68a904d12d579ad
dubbelosix <dub@006.com>
2023-08-04 01:30:10 +0530

    simplify the macro for generating the wrapped function

--- zk-cycle-utils/src/lib.rs
+++ zk-cycle-utils/src/lib.rs
@@ -3,9 +3,7 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::ItemFn;
-use syn::FnArg;
 use syn::parse_macro_input;
-
 
 #[proc_macro_attribute]
 pub fn cycle_tracker(_attr: TokenStream, item: TokenStream) -> TokenStream {
@@ -28,53 +26,29 @@ fn wrap_function(input: ItemFn) -> Result<TokenStream, syn::Error> {
     let risc0_zkvm = syn::Ident::new("risc0_zkvm", proc_macro2::Span::call_site());
     let risc0_zkvm_platform = syn::Ident::new("risc0_zkvm_platform", proc_macro2::Span::call_site());
 
-    if let Some(self_param) = inputs.first() {
-        if matches!(self_param, FnArg::Receiver(_)) {
-            let result = quote! {
-                #visibility fn #name #generics (#inputs) #output #where_clause {
-                    let before = #risc0_zkvm::guest::env::get_cycle_count();
-                    let result = (|| #block)();
-                    let after = #risc0_zkvm::guest::env::get_cycle_count();
+    let result = quote! {
+        #visibility fn #name #generics (#inputs) #output #where_clause {
+            let before = #risc0_zkvm::guest::env::get_cycle_count();
+            let result = (|| #block)();
+            let after = #risc0_zkvm::guest::env::get_cycle_count();
 
-                    // simple serialization to avoid pulling in bincode or other libs
-                    let tuple = (stringify!(#name).to_string(), (after - before) as u64);
-                    let mut serialized = Vec::new();
-                    serialized.extend(tuple.0.as_bytes());
-                    serialized.push(0);
-                    let size_bytes = tuple.1.to_ne_bytes();
-                    serialized.extend(&size_bytes);
+            // simple serialization to avoid pulling in bincode or other libs
+            let tuple = (stringify!(#name).to_string(), (after - before) as u64);
+            let mut serialized = Vec::new();
+            serialized.extend(tuple.0.as_bytes());
+            serialized.push(0);
+            let size_bytes = tuple.1.to_ne_bytes();
+            serialized.extend(&size_bytes);
 
-                    // calculate the syscall name.
-                    /// TODO: figure out how to do once. doesn't need to do it everytime.
-                    let cycle_string = String::from("cycle_metrics\0");
-                    let metrics_syscall_name = unsafe {
-                        #risc0_zkvm_platform::syscall::SyscallName::from_bytes_with_nul(cycle_string.as_ptr())
-                    };
-
-                    #risc0_zkvm::guest::env::send_recv_slice::<u8,u8>(metrics_syscall_name, &serialized);
-                    result
-                }
+            // calculate the syscall name.
+            let cycle_string = String::from("cycle_metrics\0");
+            let metrics_syscall_name = unsafe {
+                #risc0_zkvm_platform::syscall::SyscallName::from_bytes_with_nul(cycle_string.as_ptr())
             };
-            Ok(result.into())
-        } else {
-            // function
-            let result = quote! {
-                #visibility fn #name #generics (#inputs) #output {
-                    let result = (|| #block)();
-                    result
-                }
-            };
-            Ok(result.into())
+
+            #risc0_zkvm::guest::env::send_recv_slice::<u8,u8>(metrics_syscall_name, &serialized);
+            result
         }
-    } else {
-        // function without arguments
-        let result = quote! {
-            #visibility fn #name #generics (#inputs) #output {
-                let result = (|| #block)();
-                result
-            }
-        };
-        Ok(result.into())
-    }
+    };
+    Ok(result.into())
 }
-
