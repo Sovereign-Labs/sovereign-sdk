b9a06adee3039769ee939b2f3a160d7705ed95fc
bkolad <blazejkolad@gmail.com>
2023-08-03 11:08:00 +0200

    implement StateProvider

--- module-system/module-implementations/sov-evm/src/call.rs
+++ module-system/module-implementations/sov-evm/src/call.rs
@@ -1,6 +1,16 @@
+use std::sync::Arc;
+
 use anyhow::Result;
-use revm::primitives::CfgEnv;
-use sov_modules_api::CallResponse;
+use reth_primitives::{
+    Address, BlockNumber, Bytes, ChainSpec, ChainSpecBuilder, SealedBlock, SealedBlockWithSenders,
+    SealedHeader, H256, U256,
+};
+use reth_provider::{AccountReader, BlockHashReader, PostState, StateProvider, StateRootProvider};
+use reth_revm::database::{State, SubState};
+use reth_revm::executor::Executor;
+use revm::primitives::{Account, Bytecode, CfgEnv};
+use sov_modules_api::{CallResponse, Context};
+use sov_state::storage::StorageKey;
 use sov_state::WorkingSet;
 
 use crate::evm::contract_address;
@@ -20,6 +30,49 @@ pub struct CallMessage {
     pub tx: Vec<EvmTransaction>,
 }
 
+impl<'a, C: Context> StateRootProvider for EvmDb<'a, C> {
+    fn state_root(&self, post_state: PostState) -> Result<H256> {
+        todo!()
+    }
+}
+
+impl<'a, C: Context> AccountReader for EvmDb<'a, C> {
+    fn basic_account(&self, address: Address) -> Result<Option<Account>> {
+        todo!()
+    }
+}
+
+impl<'a, C: Context> BlockHashReader for EvmDb<'a, C> {
+    #[doc = " Get the hash of the block with the given number. Returns `None` if no block with this number"]
+    #[doc = " exists."]
+    fn block_hash(&self, number: BlockNumber) -> Result<Option<H256>> {
+        todo!()
+    }
+
+    #[doc = " Get headers in range of block hashes or numbers"]
+    fn canonical_hashes_range(&self, start: BlockNumber, end: BlockNumber) -> Result<Vec<H256>> {
+        todo!()
+    }
+}
+
+impl<'a, C: Context> StateProvider for EvmDb<'a, C> {
+    fn storage(&self, account: Address, storage_key: StorageKey) -> Result<Option<StorageValue>> {
+        todo!()
+    }
+
+    fn bytecode_by_hash(&self, code_hash: H256) -> Result<Option<Bytecode>> {
+        todo!()
+    }
+
+    fn proof(
+        &self,
+        address: Address,
+        keys: &[H256],
+    ) -> Result<(Vec<Bytes>, H256, Vec<Vec<Bytes>>)> {
+        todo!()
+    }
+}
+
 impl<C: sov_modules_api::Context> Evm<C> {
     pub(crate) fn execute_call(
         &self,
@@ -32,10 +85,35 @@ impl<C: sov_modules_api::Context> Evm<C> {
         let cfg_env = CfgEnv::default();
         let block_env = self.block_env.get(working_set).unwrap_or_default();
 
+        let evm_db: EvmDb<'_, C> = self.get_db(working_set);
+
+        let chain_spec = Arc::new(ChainSpecBuilder::mainnet().berlin_activated().build());
+
+        let db = SubState::new(State::new(evm_db));
+
+        let mut executor = Executor::new(chain_spec, db);
+
+        let block: SealedBlockWithSenders = SealedBlockWithSenders {
+            block: SealedBlock {
+                header: SealedHeader {
+                    header: todo!(),
+                    hash: todo!(),
+                },
+                body: vec![],
+                ommers: todo!(),
+                withdrawals: todo!(),
+            },
+            senders: vec![],
+        };
+        let (block, senders) = block.into_components();
+        let block = block.unseal();
+
+        let post_state = executor.execute_and_verify_receipt(&block, U256::MAX, Some(senders))?;
+
+        //todo!
+
         for tx in txs {
             self.transactions.set(&tx.hash, &tx, working_set);
-
-            let evm_db: EvmDb<'_, C> = self.get_db(working_set);
 
             // TODO https://github.com/Sovereign-Labs/sovereign-sdk/issues/505
             let result =
