603a1fd03299970d81a6dec7bd05690d1bb67dca
theochap <80177219+theochap@users.noreply.github.com>
2023-08-10 14:54:53 +0200

    Update adapters/risc0/src/guest.rs
    Co-authored-by: Nikolai Golub <nikolai@sovlabs.io>

--- adapters/risc0/src/guest.rs
+++ adapters/risc0/src/guest.rs
@@ -39,7 +39,7 @@ impl Zkvm for Risc0Guest {
         _code_commitment: &Self::CodeCommitment,
     ) -> Result<&'a [u8], Self::Error> {
         // Implement this method once risc0 supports recursion: issue #633
-        todo!("Implement once risc0 supports recursion")
+        todo!("Implement once risc0 supports recursion: https://github.com/Sovereign-Labs/sovereign-sdk/issues/633")
     }
 
     fn verify_and_extract_output<
