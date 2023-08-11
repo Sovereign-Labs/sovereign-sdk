3cc6083f9edc8692f6a4c64246555b6ca0461295
Nikolai Golub <nikolai@sovlabs.io>
2023-08-11 11:50:08 +0200

    Refactor `CelestiaAddress` (#551)
    * Refactor CelestiaAddress and its internal representation

--- examples/const-rollup-config/src/lib.rs
+++ examples/const-rollup-config/src/lib.rs
@@ -5,4 +5,4 @@ pub const ROLLUP_NAMESPACE_RAW: [u8; 8] = [115, 111, 118, 45, 116, 101, 115, 116
 
 /// The DA address of the sequencer (for now we use a centralized sequencer) in the tests.
 /// Here this is the address of the sequencer on the celestia blockchain.
-pub const SEQUENCER_DA_ADDRESS: [u8; 47] = *b"celestia1w7wcupk5gswj25c0khnkey5fwmlndx6t5aarmk";
+pub const SEQUENCER_DA_ADDRESS: &str = "celestia1w7wcupk5gswj25c0khnkey5fwmlndx6t5aarmk";
--- examples/demo-rollup/benches/rollup_bench.rs
+++ examples/demo-rollup/benches/rollup_bench.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::path::PathBuf;
+use std::str::FromStr;
 use std::sync::Arc;
 use std::time::Duration;
 
@@ -50,10 +51,11 @@ fn rollup_bench(_bench: &mut Criterion) {
 
     let mut demo = demo_runner.stf;
     let sequencer_private_key = DefaultPrivateKey::generate();
+    let sequencer_da_address = CelestiaAddress::from_str(SEQUENCER_DA_ADDRESS).unwrap();
     let demo_genesis_config = create_demo_genesis_config(
         100000000,
         sequencer_private_key.default_address(),
-        SEQUENCER_DA_ADDRESS.to_vec(),
+        sequencer_da_address.as_ref().to_vec(),
         &sequencer_private_key,
         &sequencer_private_key,
     );
