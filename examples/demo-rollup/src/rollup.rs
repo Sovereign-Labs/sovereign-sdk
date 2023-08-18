dcdc9b29c80c061d8bb6b1ef93a27e891f014459
bkolad <blazejkolad@gmail.com>
2023-08-18 15:24:53 +0200

    Add doc

--- examples/demo-rollup/src/rollup.rs
+++ examples/demo-rollup/src/rollup.rs
@@ -6,6 +6,7 @@ use demo_stf::runtime::get_rpc_methods;
 use risc0_adapter::host::Risc0Verifier;
 use sov_db::ledger_db::LedgerDB;
 use sov_rollup_interface::services::da::DaService;
+use sov_rollup_interface::zk::Zkvm;
 use sov_state::storage::Storage;
 use sov_stf_runner::{from_toml_path, RollupConfig, RunnerConfig, StateTransitionRunner};
 use tracing::{debug, Level};
@@ -15,18 +16,18 @@ use crate::register_rpc::register_ethereum;
 use crate::register_rpc::{register_ledger, register_sequencer};
 use crate::{get_genesis_config, initialize_ledger, ROLLUP_NAMESPACE};
 
-/// TODO
-pub struct Rollup<DA: DaService> {
-    app: App<Risc0Verifier, DA::Spec>,
+/// Dependencies needed to run the rollup.
+pub struct Rollup<Vm: Zkvm, DA: DaService> {
+    app: App<Vm, DA::Spec>,
     da_service: DA,
     ledger_db: LedgerDB,
     runner_config: RunnerConfig,
 }
 
-///
+/// Creates celestia based rollup.
 pub async fn new_rollup_with_celestia_da(
     rollup_config_path: &str,
-) -> Result<Rollup<CelestiaService>, anyhow::Error> {
+) -> Result<Rollup<Risc0Verifier, CelestiaService>, anyhow::Error> {
     debug!("Starting demo rollup with config {}", rollup_config_path);
     let rollup_config: RollupConfig<celestia::DaServiceConfig> =
         from_toml_path(rollup_config_path).context("Failed to read rollup configuration")?;
@@ -59,8 +60,8 @@ pub async fn new_rollup_with_celestia_da(
     })
 }
 
-impl<DA: DaService<Error = anyhow::Error>> Rollup<DA> {
-    /// TODO
+impl<Vm: Zkvm, DA: DaService<Error = anyhow::Error>> Rollup<Vm, DA> {
+    /// Runs the rollup.
     pub async fn run(mut self) -> Result<(), anyhow::Error> {
         let storage = self.app.get_storage();
         let mut methods = get_rpc_methods::<DefaultContext>(storage);
