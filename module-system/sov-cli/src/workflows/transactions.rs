e458a23b90df279d26072a9e6b2c416a5ba307c0
Preston Evans <preston.b.evans@gmail.com>
2023-08-21 16:43:06 -0500

    Cleanup diff

--- module-system/sov-cli/src/workflows/transactions.rs
+++ module-system/sov-cli/src/workflows/transactions.rs
@@ -64,95 +64,7 @@ impl<File: Subcommand, Json: Subcommand> TransactionWorkflow<File, Json> {
             }
         }
     }
-
-    // /// Run the transaction workflow
-    // pub fn run<RT: CliWallet, C: sov_modules_api::Context, U, E1, E2>(
-    //     self,
-    //     wallet_state: &mut WalletState<RT::Decodable, C>,
-    //     _app_dir: impl AsRef<Path>,
-    // ) -> Result<(), anyhow::Error>
-    // where
-    //     T: CliFrontEnd<RT>,
-    //     T: TryInto<RT::CliStringRepr<U>, Error = E1>,
-    //     RT::CliStringRepr<U>: TryInto<RT::Decodable, Error = E2>,
-    //     RT::Decodable: Serialize,
-    //     E1: Into<anyhow::Error> + Send + Sync,
-    //     E2: Into<anyhow::Error> + Send + Sync,
-    // {
-    //     match self {
-    //         TransactionWorkflow::Import(cli_version) => {
-    //             let intermediate_state: RT::CliStringRepr<U> = cli_version
-    //                 .try_into()
-    //                 .map_err(Into::<anyhow::Error>::into)?;
-    //             let tx = intermediate_state
-    //                 .try_into()
-    //                 .map_err(Into::<anyhow::Error>::into)?;
-    //             println!("Adding the following transaction to batch:");
-    //             println!("{}", serde_json::to_string_pretty(&tx)?);
-    //             wallet_state.unsent_transactions.push(tx);
-    //         }
-    //     }
-    //     Ok(())
-    // }
 }
-
-// #[derive(clap::Parser)]
-// /// Generate, sign, and send transactions
-// pub enum TransactionWorkflow<T>
-// where
-//     T: clap::Subcommand + Send + Sync,
-// {
-//     /// Import a transaction  as a JSON string
-//     #[clap(subcommand)]
-//     Import(ImportTransaction<T>),
-//     /// List the current batch of transactions
-//     List,
-//     // TODO: Add `send` and `generate_schema` subcommands/
-//     // TODO: design and implement batch management (remove tx, drop batch, etc.)
-// }
-
-// impl TransactionWorkflow<T> {
-//     /// Run the transaction workflow
-//     pub fn run<E1, E2, C: sov_modules_api::Context, RT: CliWallet>(
-//         self,
-//         wallet_state: &mut WalletState<RT::Decodable, C>,
-//         _app_dir: impl AsRef<Path>,
-//     ) -> Result<(), anyhow::Error>
-//     where
-//         RT::Decodable: Serialize + DeserializeOwned,
-//         RT::CliStringRepr: TryInto<RT::Decodable, Error = E1>,
-//         T: TryInto<RT::CliStringRepr, Error = E2>,
-//         E1: Into<anyhow::Error> + Send + Sync,
-//         E2: Into<anyhow::Error> + Send + Sync,
-//     {
-//         match self {
-//             TransactionWorkflow::Import(method) => {
-//                 match method {
-//                     ImportTransaction::FromFile(path_to_json) => {
-//                         std::fs::read(&path_to_json.as_ref()).with_context(|| {
-//                             format!("Could not open file at {}", path_to_json.as_ref())
-//                         })?;
-//                     }
-//                     ImportTransaction::FromString(json) => json,
-//                 };
-//                 let tx = json.try_into().map_err(Into::into)?;
-//                 println!("Adding the following transaction to batch:");
-//                 println!("{}", serde_json::to_string_pretty(&tx)?);
-//                 wallet_state.unsent_transactions.push(tx);
-//             }
-//             TransactionWorkflow::List => {
-//                 println!("Current batch:");
-//                 println!(
-//                     "{}",
-//                     serde_json::to_string_pretty(&wallet_state.unsent_transactions)?
-//                 );
-//             }
-//         }
-
-//         Ok(())
-//     }
-// }
-
 /// An argument passed as path to a file
 #[derive(clap::Parser)]
 pub struct FileArg {
@@ -215,10 +127,3 @@ where
         Ok(())
     }
 }
-
-// /// The optional arguments for the transaction workflow
-// #[derive(Debug, Args)]
-// pub struct OptionalArgs {
-//     #[clap(short, long, global = true, default_value_t = false)]
-//     send_transactions: bool,
-// }
