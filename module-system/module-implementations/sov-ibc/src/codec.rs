a6951b181d0f3b04beb679c28802df3fbb03af70
Philippe Laferriere <plafer@protonmail.com>
2023-08-21 13:08:14 -0400

    Make ProtobufCodec work on Any only

--- module-system/module-implementations/sov-ibc/src/codec.rs
+++ module-system/module-implementations/sov-ibc/src/codec.rs
@@ -1,37 +1,15 @@
 use core::fmt::Display;
-use core::marker::PhantomData;
 
+use ibc::Any;
 use ibc_proto::protobuf::{Error, Protobuf};
-use sov_state::codec::{StateKeyCodec, StateValueCodec};
+use sov_state::codec::StateValueCodec;
 
-// TODO:
-// We're currently blocked on using this, because the `StateMap` prevents us from using a different codec for key and value. Our keys are never protobuf-encodable (e.g. ClientId); so we'd like to borsh-encode the key, and protobuf-encode the value.
-pub struct ProtobufCodec<Raw> {
-    _raw: PhantomData<Raw>,
-}
+pub struct ProtobufCodec;
 
-impl<K, Raw> StateKeyCodec<K> for ProtobufCodec<Raw>
+impl<K> StateValueCodec<K> for ProtobufCodec
 where
-    K: Protobuf<Raw>,
+    K: Protobuf<Any>,
     K::Error: Display,
-    Raw: prost::Message + Default,
-{
-    type KeyError = Error;
-
-    fn encode_key(&self, key: &K) -> Vec<u8> {
-        key.encode_vec()
-    }
-
-    fn try_decode_key(&self, bytes: &[u8]) -> Result<K, Self::KeyError> {
-        Protobuf::decode_vec(bytes)
-    }
-}
-
-impl<K, Raw> StateValueCodec<K> for ProtobufCodec<Raw>
-where
-    K: Protobuf<Raw>,
-    K::Error: Display,
-    Raw: prost::Message + Default,
 {
     type ValueError = Error;
 
