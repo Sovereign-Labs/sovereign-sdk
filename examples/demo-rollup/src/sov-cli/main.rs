4b963bf9bd1ac29f4c4f0af21c26ede2c5e2ad9a
Nikolai Golub <nikolai@sovlabs.io>
2023-09-01 16:08:29 +0100

    Clean up sov-cli usage

--- examples/demo-rollup/src/sov-cli/main.rs
+++ examples/demo-rollup/src/sov-cli/main.rs
@@ -1,10 +1,7 @@
-#[cfg(feature = "native")]
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    demo_stf::cli::run::<celestia::CelestiaService::Spec>().await
-}
-
-#[cfg(not(feature = "native"))]
-fn main() -> Result<(), anyhow::Error> {
-    Err(anyhow::format_err!("CLI support is only available when the app is compiled with the 'native' flag. You can recompile with 'cargo build --features=native' to use the CLI"))
+    demo_stf::cli::run::<
+        <celestia::CelestiaService as sov_rollup_interface::services::da::DaService>::Spec,
+    >()
+    .await
 }
