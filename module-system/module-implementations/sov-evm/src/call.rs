5de65c0c51712c193fd982f2a9d43428a4b8a411
bkolad <blazejkolad@gmail.com>
2023-09-05 14:22:40 +0200

    fix tests

--- module-system/module-implementations/sov-evm/src/call.rs
+++ module-system/module-implementations/sov-evm/src/call.rs
@@ -42,7 +42,14 @@ impl<C: sov_modules_api::Context> Evm<C> {
         // TODO https://github.com/Sovereign-Labs/sovereign-sdk/issues/505
         let result = executor::execute_tx(evm_db, block_env, &evm_tx_recovered, cfg_env).unwrap();
 
-        let transaction = reth_rpc_types::Transaction::from_recovered(evm_tx_recovered.tx.clone());
+        // TODO fill the missing values.
+        let transaction = reth_rpc_types::Transaction::from_recovered_with_block_context(
+            evm_tx_recovered.tx.clone(),
+            reth_primitives::H256::default(),
+            0,
+            None,
+            reth_primitives::U256::from(1),
+        );
 
         println!("Hash {}", hash);
 
