a94c33307c0216bfebaa5eef72c4414ffd3cb0b3
Filippo Neysofu Costa <filippo@sovlabs.io>
2023-09-15 13:52:30 +0200

    Add missing docs to `sov-state` (#841)
    * #![deny(missing_docs)]
    
    * More docs
    
    Signed-off-by: Filippo Costa <filippo@sovlabs.io>
    
    * Fix doc link
    
    Signed-off-by: Filippo Costa <filippo@sovlabs.io>
    
    * Fix docs
    
    Signed-off-by: Filippo Costa <filippo@sovlabs.io>
    
    * Fix doc
    
    Signed-off-by: Filippo Costa <filippo@sovlabs.io>
    
    * Fix doc
    
    Signed-off-by: Filippo Costa <filippo@sovlabs.io>
    
    ---------
    
    Signed-off-by: Filippo Costa <filippo@sovlabs.io>

--- module-system/sov-state/src/witness.rs
+++ module-system/sov-state/src/witness.rs
@@ -6,18 +6,37 @@ use jmt::storage::TreeReader;
 use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
 
+/// A witness is a value produced during native execution that is then used by
+/// the zkVM circuit to produce proofs.
+///
+/// Witnesses are typically used to abstract away storage access from inside the
+/// zkVM. For every read operation performed by the native code, a hint can be
+/// added and the zkVM circuit can then read the same hint. Hints are replayed
+/// to [`Witness::get_hint`] in the same order
+/// they were added via [`Witness::add_hint`].
 // TODO: Refactor witness trait so it only require Serialize / Deserialize
 //   https://github.com/Sovereign-Labs/sovereign-sdk/issues/263
 pub trait Witness: Default + Serialize + DeserializeOwned {
+    /// Adds a serializable "hint" to the witness value, which can be later
+    /// read by the zkVM circuit.
+    ///
+    /// This method **SHOULD** only be called from the native execution
+    /// environment.
     fn add_hint<T: BorshSerialize>(&self, hint: T);
+
+    /// Retrieves a "hint" from the witness value.
     fn get_hint<T: BorshDeserialize>(&self) -> T;
+
+    /// Adds all hints from `rhs` to `self`.
     fn merge(&self, rhs: &Self);
 }
 
+/// A wrapper around a [`Witness`] that implements [`TreeReader`].
 #[derive(Debug)]
 pub struct TreeWitnessReader<'a, T: Witness>(&'a T);
 
 impl<'a, T: Witness> TreeWitnessReader<'a, T> {
+    /// Wraps the given witness.
     pub fn new(witness: &'a T) -> Self {
         Self(witness)
     }
@@ -50,6 +69,21 @@ impl<'a, T: Witness> TreeReader for TreeWitnessReader<'a, T> {
     }
 }
 
+/// A [`Vec`]-based implementation of [`Witness`] with no special logic.
+///
+/// # Example
+///
+/// ```
+/// use sov_state::{ArrayWitness, Witness};
+///
+/// let witness = ArrayWitness::default();
+///
+/// witness.add_hint(1u64);
+/// witness.add_hint(2u64);
+///
+/// assert_eq!(witness.get_hint::<u64>(), 1u64);
+/// assert_eq!(witness.get_hint::<u64>(), 2u64);
+/// ```
 #[derive(Default, Debug, Serialize, Deserialize)]
 pub struct ArrayWitness {
     next_idx: AtomicUsize,
