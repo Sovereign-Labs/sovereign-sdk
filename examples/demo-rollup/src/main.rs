169cfe616036ed1b8af4ac8891ce805f3b5a87ff
bkolad <blazejkolad@gmail.com>
2023-09-19 17:16:34 +0200

    MVP

--- examples/demo-rollup/src/main.rs
+++ examples/demo-rollup/src/main.rs
@@ -25,6 +25,7 @@ struct Args {
     rollup_config_path: String,
 }
 
+/*
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
     // Initializing logging
@@ -48,4 +49,69 @@ async fn main() -> Result<(), anyhow::Error> {
         }
         da => panic!("DA Layer not supported: {}", da),
     }
+}*/
+
+pub mod sov_api {
+
+    pub trait GasUnit {
+        fn from_str(s: &str) -> Self;
+        fn value(price: Self) -> u64;
+    }
+
+    pub struct TupleGasUnit {
+        zk_gas: u64,
+        native_gas: u64,
+    }
+
+    impl GasUnit for TupleGasUnit {
+        fn from_str(s: &str) -> Self {
+            todo!()
+        }
+
+        fn value(price: Self) -> u64 {
+            todo!()
+        }
+    }
+
+    pub trait Context {
+        type GasUnit: GasUnit;
+    }
+
+    pub struct DefaultContext;
+
+    impl Context for DefaultContext {
+        type GasUnit = TupleGasUnit;
+    }
+
+    pub fn parse_toml_file_to_lines(s: &str) -> Vec<String> {
+        todo!()
+    }
+}
+
+mod some_sov_module {
+    use super::sov_api::{parse_toml_file_to_lines, Context, GasUnit};
+
+    pub struct GasConfig<GU: GasUnit> {
+        pub complex_math_operation: GU,
+        pub some_other_operation: GU,
+    }
+
+    // This would be macro generates
+    pub fn from_toml<C: Context>(t: &str) -> GasConfig<C::GasUnit> {
+        let strs = parse_toml_file_to_lines(t);
+        let complex_math_operation = C::GasUnit::from_str(&strs[0]);
+        let some_other_operation = C::GasUnit::from_str(&strs[1]);
+
+        GasConfig {
+            complex_math_operation,
+            some_other_operation,
+        }
+    }
+}
+
+mod runtime {}
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    todo!();
 }
