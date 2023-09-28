00c1935dffe447f859768bba4a70b25d459215cf
bkolad <blazejkolad@gmail.com>
2023-09-28 16:56:10 +0200

    Add tests

--- module-system/sov-modules-api/src/pub_key_hex.rs
+++ module-system/sov-modules-api/src/pub_key_hex.rs
@@ -1,8 +1,7 @@
-use crate::default_signature::DefaultPublicKey;
 use derive_more::Display;
-
 use ed25519_dalek::{VerifyingKey as DalekPublicKey, PUBLIC_KEY_LENGTH};
-use thiserror::Error;
+
+use crate::default_signature::DefaultPublicKey;
 #[derive(
     serde::Serialize,
     serde::Deserialize,
@@ -20,16 +19,8 @@ pub struct PublicKeyHex {
     hex: String,
 }
 
-#[derive(Error, Debug)]
-pub enum HexConversionError {
-    #[error("todo")]
-    OddLength,
-    #[error("todo")]
-    InvalidHexCharacter { c: char, index: usize },
-}
-
 impl TryFrom<&str> for PublicKeyHex {
-    type Error = HexConversionError;
+    type Error = anyhow::Error;
 
     fn try_from(hex: &str) -> Result<Self, Self::Error> {
         Self::try_from(hex.to_owned())
@@ -37,18 +28,21 @@ impl TryFrom<&str> for PublicKeyHex {
 }
 
 impl TryFrom<String> for PublicKeyHex {
-    type Error = HexConversionError;
+    type Error = anyhow::Error;
 
     fn try_from(hex: String) -> Result<Self, Self::Error> {
         if hex.len() & 1 != 0 {
-            return Err(HexConversionError::OddLength);
+            anyhow::bail!("Bad hex conversion: odd input length")
         }
 
         if let Some((index, c)) = hex.chars().enumerate().find(|(_, c)| {
-            !matches!(c, '0'..='9' | 'a'..='f')
-            //Case::Upper => !matches!(c, '0'..='9' | 'A'..='F'),
+            !(matches!(c, '0'..='9' | 'a'..='f') || matches!(c, '0'..='9' | 'A'..='F'))
         }) {
-            return Err(HexConversionError::InvalidHexCharacter { c, index });
+            anyhow::bail!(
+                "Bad hex conversion: wrong character `{}` at index {}",
+                c,
+                index
+            )
         }
 
         Ok(Self { hex })
@@ -88,9 +82,46 @@ impl TryFrom<PublicKeyHex> for DefaultPublicKey {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::default_signature::private_key::DefaultPrivateKey;
+    use crate::PrivateKey;
 
     #[test]
     fn test_pub_key_hex() {
-        let pk_hex = PublicKeyHex::try_from("z").unwrap();
+        let pub_key = DefaultPrivateKey::generate().pub_key();
+        let pub_key_hex = PublicKeyHex::try_from(pub_key.clone()).unwrap();
+
+        println!("{}", pub_key_hex);
+
+        let converted_pub_key = DefaultPublicKey::try_from(pub_key_hex).unwrap();
+
+        assert_eq!(pub_key, converted_pub_key);
+    }
+
+    #[test]
+    fn test_pub_key_hex_str() {
+        let key = "022e229198d957bf0c0a504e7d7bcec99a1d62cccc7861ed2452676ad0323ad8";
+        let pub_key_hex_lower: PublicKeyHex = key.try_into().unwrap();
+        let pub_key_hex_upper: PublicKeyHex = key.to_uppercase().try_into().unwrap();
+
+        let pub_key_lower = DefaultPublicKey::try_from(pub_key_hex_lower).unwrap();
+        let pub_key_upper = DefaultPublicKey::try_from(pub_key_hex_upper).unwrap();
+
+        assert_eq!(pub_key_lower, pub_key_upper)
+    }
+
+    #[test]
+    fn test_bad_pub_key_hex_str() {
+        let key = "022e229198d957Zf0c0a504e7d7bcec99a1d62cccc7861ed2452676ad0323ad8";
+        let err = PublicKeyHex::try_from(key).unwrap_err();
+
+        assert_eq!(
+            err.to_string(),
+            "Bad hex conversion: wrong character `Z` at index 14"
+        );
+
+        let key = "022";
+        let err = PublicKeyHex::try_from(key).unwrap_err();
+
+        assert_eq!(err.to_string(), "Bad hex conversion: odd input length")
     }
 }
