37c794f578fd7bc8ba15faed5ef9f4e78d2e74c1
bkolad <blazejkolad@gmail.com>
2023-09-28 17:03:55 +0200

    Add doc

--- module-system/sov-modules-api/src/pub_key_hex.rs
+++ module-system/sov-modules-api/src/pub_key_hex.rs
@@ -1,6 +1,7 @@
 use derive_more::Display;
 use ed25519_dalek::{VerifyingKey as DalekPublicKey, PUBLIC_KEY_LENGTH};
 
+/// A hexadecimal representation of a PublicKey.
 use crate::default_signature::DefaultPublicKey;
 #[derive(
     serde::Serialize,
