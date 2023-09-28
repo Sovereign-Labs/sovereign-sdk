661ce22a84f066ab2f8888ede161bb7480a60998
bkolad <blazejkolad@gmail.com>
2023-09-28 16:58:09 +0200

    Remove println

--- module-system/sov-modules-api/src/pub_key_hex.rs
+++ module-system/sov-modules-api/src/pub_key_hex.rs
@@ -89,11 +89,7 @@ mod tests {
     fn test_pub_key_hex() {
         let pub_key = DefaultPrivateKey::generate().pub_key();
         let pub_key_hex = PublicKeyHex::try_from(pub_key.clone()).unwrap();
-
-        println!("{}", pub_key_hex);
-
         let converted_pub_key = DefaultPublicKey::try_from(pub_key_hex).unwrap();
-
         assert_eq!(pub_key, converted_pub_key);
     }
 
