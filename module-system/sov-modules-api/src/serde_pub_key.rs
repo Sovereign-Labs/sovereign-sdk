55286e229463ef3df17235aab071dc91a343541d
bkolad <blazejkolad@gmail.com>
2023-09-30 23:59:43 +0200

    add test

--- module-system/sov-modules-api/src/serde_pub_key.rs
+++ module-system/sov-modules-api/src/serde_pub_key.rs
@@ -34,8 +34,24 @@ impl<'de> serde::Deserialize<'de> for DefaultPublicKey {
 
 #[cfg(test)]
 mod test {
-    
+    use super::*;
 
     #[test]
-    fn test_pub_key() {}
+    fn test_pub_key_json() {
+        let pub_key_hex: PublicKeyHex =
+            "022e229198d957bf0c0a504e7d7bcec99a1d62cccc7861ed2452676ad0323ad8"
+                .try_into()
+                .unwrap();
+
+        let pub_key = DefaultPublicKey::try_from(&pub_key_hex).unwrap();
+        let pub_key_str: String = serde_json::to_string(&pub_key).unwrap();
+
+        assert_eq!(
+            pub_key_str,
+            r#""022e229198d957bf0c0a504e7d7bcec99a1d62cccc7861ed2452676ad0323ad8""#
+        );
+
+        let deserialized: DefaultPublicKey = serde_json::from_str(&pub_key_str).unwrap();
+        assert_eq!(deserialized, pub_key);
+    }
 }
