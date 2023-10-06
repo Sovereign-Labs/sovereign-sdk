d9d7bca14a458f3e47e5db5a2eef7f62101e97bf
dubbelosix <rohan@sovlabs.io>
2023-10-06 14:42:00 +0530

    TEMP

--- adapters/solana/solana_client/src/main.rs
+++ adapters/solana/solana_client/src/main.rs
@@ -1,23 +1,57 @@
-use {
-    anchor_client::{
-        Client,
-        Cluster,
-    },
-    solana_sdk::{
-        signer::keypair::read_keypair_file,
-        pubkey::Pubkey,
-    },
-    anchor_lang::prelude::*,
-};
+use anchor_client::{Client, Cluster};
+use solana_sdk::{signer::keypair::read_keypair_file,pubkey::Pubkey,
+                 signature::{Keypair, Signer},
+                 system_instruction,
+                 system_program,
+                 transaction::Transaction,};
+use anchor_lang::prelude::*;
+use anchor_lang::solana_program::sysvar::clock::Clock;
 
 use blockroot::accounts as blockroot_accounts;
 use blockroot::instruction as blockroot_instruction;
+
+use solana_rpc_client::rpc_client::RpcClient;
+use solana_sdk::{
+
+};
+
 
 use clap::Parser;
 use alloc::rc::Rc;
 extern crate alloc;
 
 const DEFAULT_URL: &str = "http://127.0.0.1:8899";
+
+const CLOCK_PROGRAM_ID: &str = "SysvarC1ock11111111111111111111111111111111";
+const BLOCKROOT_PROGRAM_ID: &str = "6YQGvP866CHpLTdHwmLqj2Vh5q7T1GF4Kk9gS9MCta8E";
+
+fn create_account(
+    client: &RpcClient,
+    payer: &Keypair,
+    new_account: &Keypair,
+    space: u64,
+) -> anyhow::Result<()> {
+    let rent = client.get_minimum_balance_for_rent_exemption(space.try_into()?)?;
+    let instr = system_instruction::create_account(
+        &payer.pubkey(),
+        &new_account.pubkey(),
+        rent,
+        space,
+        &system_program::ID,
+    );
+
+    let blockhash = client.get_latest_blockhash()?;
+    let tx = Transaction::new_signed_with_payer(
+        &[instr],
+        Some(&payer.pubkey()),
+        &[payer, new_account],
+        blockhash,
+    );
+
+    let _sig = client.send_and_confirm_transaction(&tx)?;
+
+    Ok(())
+}
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -31,6 +65,7 @@ struct Args {
     url: String,
 }
 
+
 fn main() {
     let args = Args::parse();
     let signer = args.signer;
@@ -38,11 +73,14 @@ fn main() {
     let kp = read_keypair_file(signer).unwrap();
     let c = Client::new(Cluster::Localnet, Rc::new(kp));
     let program_id: Pubkey = Pubkey::new(
-        &bs58::decode("6YQGvP866CHpLTdHwmLqj2Vh5q7T1GF4Kk9gS9MCta8E").into_vec().unwrap());
+        &bs58::decode(BLOCKROOT_PROGRAM_ID).into_vec().unwrap());
     let prog = c.program(program_id).unwrap();
-    let signature = prog.request()
-        .accounts(blockroot_accounts::Initialize {})
-        .args(blockroot_instruction::Initialize {})
-        .send();
-    println!("{:?}",signature);
+    //
+    // let clock_pubkey = Pubkey::new(&bs58::decode(CLOCK_PROGRAM_ID).into_vec().unwrap());
+    // let signature = prog.request()
+    //     .accounts(blockroot_accounts::Initialize {
+    //         clock: clock_pubkey
+    //     })
+    //     .args(blockroot_instruction::Initialize {})
+    //     .send();
 }
--- adapters/solana/solana_da_programs/programs/blockroot/src/lib.rs
+++ adapters/solana/solana_da_programs/programs/blockroot/src/lib.rs
@@ -1,7 +1,10 @@
 mod merkle;
 
+use std::collections::BTreeMap;
 use anchor_lang::prelude::*;
 use merkle::compute_merkle_root;
+use anchor_lang::solana_program::sysvar::clock::Clock;
+use anchor_lang::solana_program::keccak::hashv;
 
 declare_id!("6YQGvP866CHpLTdHwmLqj2Vh5q7T1GF4Kk9gS9MCta8E");
 
@@ -9,15 +12,87 @@ declare_id!("6YQGvP866CHpLTdHwmLqj2Vh5q7T1GF4Kk9gS9MCta8E");
 pub mod blockroot {
     use super::*;
 
-    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
-        let data1= &[1u8; 10000];
-        let data2 = &[2u8; 10000];
-        let data_vec = vec![&data1[..],&data2[..]];
-        let root = compute_merkle_root(data_vec);
-        msg!("{:?}",root);
+    pub fn initialize<'info>(ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub creator: Signer<'info>,
+    #[account(init_if_needed,payer=creator, space=10000000)]
+    pub chunk_accumulator: Account<'info, ChunkAccumulator>,
+    pub clock: Sysvar<'info, Clock>,
+    pub system_program: Program<'info, System>,
+}
+
+
+#[account]
+#[derive(Default, Debug)]
+pub struct Chunk {
+    pub digest: [u8; 32],
+    pub num_chunks: u64,
+    pub chunk_num: u64,
+    pub chunk_body: Vec<u8>,
+}
+
+#[account]
+#[derive(Default, Debug)]
+pub struct ChunkAccumulator {
+    chunks: BTreeMap<[u8; 32], Vec<Vec<Option<[u8; 32]>>>>,
+    chunk_age: BTreeMap<[u8;32], u64>
+}
+
+impl ChunkAccumulator {
+    pub fn new() -> Self {
+        ChunkAccumulator {
+            chunks: BTreeMap::new(),
+            chunk_age: BTreeMap::new()
+        }
+    }
+
+    pub fn accumulate(&mut self, chunk: Chunk) {
+        let Chunk {
+            digest,
+            num_chunks,
+            chunk_num,
+            chunk_body,
+        } = chunk;
+
+        let levels = self.chunks.entry(digest).or_insert_with(|| {
+            let mut vec = Vec::new();
+            let mut num = num_chunks as usize;
+            while num > 0 {
+                vec.push(vec![None; num]);
+                num = (num + 1) / 2;
+            }
+            vec
+        });
+
+        // Store the hash of the chunk body
+        let chunk_hash = hashv(&[&chunk_body]).to_bytes();
+        levels[0][chunk_num as usize] = Some(chunk_hash);
+
+        // Merge and promote as necessary
+        for i in 0..levels.len() - 1 {
+            for j in (0..levels[i].len()).step_by(2) {
+                if j + 1 < levels[i].len() && levels[i][j].is_some() && levels[i][j + 1].is_some() {
+                    let merged = hashv(&[levels[i][j].as_ref().unwrap(), levels[i][j + 1].as_ref().unwrap()]).to_bytes();
+                    levels[i + 1][j / 2] = Some(merged);
+                }
+            }
+        }
+    }
+
+    // If the digest's tree is complete, this will return the Merkle root.
+    pub fn get_merkle_root(&self, digest: [u8; 32]) -> Option<[u8; 32]> {
+        self.chunks.get(&digest).and_then(|levels| levels.last()?.first().cloned()).flatten()
+    }
+
+    // Check if the Merkle tree for a specific digest is complete
+    pub fn is_complete(&self, digest: [u8; 32]) -> bool {
+        self.get_merkle_root(digest).is_some()
+    }
+}
+
