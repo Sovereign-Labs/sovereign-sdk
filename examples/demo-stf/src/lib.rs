64d8e33140c0ee5b9f350f957279b840edfd3f21
bkolad <blazejkolad@gmail.com>
2023-10-09 10:23:28 +0200

    fix expose_rpc macro

--- examples/demo-stf/src/lib.rs
+++ examples/demo-stf/src/lib.rs
@@ -1,5 +1,5 @@
-//#![deny(missing_docs)]
-//#![doc = include_str!("../README.md")]
+// #![deny(missing_docs)]
+// #![doc = include_str!("../README.md")]
 
 #[cfg(feature = "native")]
 pub mod cli;
--- examples/demo-stf/src/runtime.rs
+++ examples/demo-stf/src/runtime.rs
@@ -1,4 +1,35 @@
-//!TODO
+//! The Rollup entrypoint.
+//!
+//! On a high level, the rollup node receives serialized call messages from the DA layer and executes them as atomic transactions.
+//! Upon reception, the message has to be deserialized and forwarded to an appropriate module.
+//!
+//! The module-specific logic is implemented by module creators, but all the glue code responsible for message
+//! deserialization/forwarding is handled by a rollup `runtime`.
+//!
+//! In order to define the runtime we need to specify all the modules supported by our rollup (see the `Runtime` struct bellow)
+//!
+//! The `Runtime` together with associated interfaces (`Genesis`, `DispatchCall`, `MessageCodec`)
+//! and derive macros defines:
+//! - how the rollup modules are wired up together.
+//! - how the state of the rollup is initialized.
+//! - how messages are dispatched to appropriate modules.
+//!
+//! Runtime lifecycle:
+//!
+//! 1. Initialization:
+//!     When a rollup is deployed for the first time, it needs to set its genesis state.
+//!     The `#[derive(Genesis)` macro will generate `Runtime::genesis(config)` method which returns
+//!     `Storage` with the initialized state.
+//!
+//! 2. Calls:      
+//!     The `Module` interface defines a `call` method which accepts a module-defined type and triggers the specific `module logic.`
+//!     In general, the point of a call is to change the module state, but if the call throws an error,
+//!     no state is updated (the transaction is reverted).
+//!
+//! `#[derive(MessageCodec)` adds deserialization capabilities to the `Runtime` (implements `decode_call` method).
+//! `Runtime::decode_call` accepts serialized call message and returns a type that implements the `DispatchCall` trait.
+//!  The `DispatchCall` implementation (derived by a macro) forwards the message to the appropriate module and executes its `call` method.
+
 
 #[cfg(feature = "native")]
 use sov_accounts::{AccountsRpcImpl, AccountsRpcServer};
@@ -26,42 +57,7 @@ use sov_sequencer_registry::{SequencerRegistryRpcImpl, SequencerRegistryRpcServe
 #[cfg(feature = "native")]
 use sov_value_setter::{ValueSetterRpcImpl, ValueSetterRpcServer};
 
-/// The Rollup entrypoint.
-///
-/// On a high level, the rollup node receives serialized call messages from the DA layer and executes them as atomic transactions.
-/// Upon reception, the message has to be deserialized and forwarded to an appropriate module.
-///
-/// The module-specific logic is implemented by module creators, but all the glue code responsible for message
-/// deserialization/forwarding is handled by a rollup `runtime`.
-///
-/// In order to define the runtime we need to specify all the modules supported by our rollup (see the `Runtime` struct bellow)
-///
-/// The `Runtime` together with associated interfaces (`Genesis`, `DispatchCall`, `MessageCodec`)
-/// and derive macros defines:
-/// - how the rollup modules are wired up together.
-/// - how the state of the rollup is initialized.
-/// - how messages are dispatched to appropriate modules.
-///
-/// Runtime lifecycle:
-///
-/// 1. Initialization:
-///     When a rollup is deployed for the first time, it needs to set its genesis state.
-///     The `#[derive(Genesis)` macro will generate `Runtime::genesis(config)` method which returns
-///     `Storage` with the initialized state.
-///
-/// 2. Calls:      
-///     The `Module` interface defines a `call` method which accepts a module-defined type and triggers the specific `module logic.`
-///     In general, the point of a call is to change the module state, but if the call throws an error,
-///     no state is updated (the transaction is reverted).
-///
-/// `#[derive(MessageCodec)` adds deserialization capabilities to the `Runtime` (implements `decode_call` method).
-/// `Runtime::decode_call` accepts serialized call message and returns a type that implements the `DispatchCall` trait.
-///  The `DispatchCall` implementation (derived by a macro) forwards the message to the appropriate module and executes its `call` method.
-///
-/// Similar mechanism works for queries with the difference that queries are submitted by users directly to the rollup node
-/// instead of going through the DA layer.
-
-
+/// The `demo-stf runtime`.
 #[cfg_attr(feature = "native", derive(CliWallet), expose_rpc)]
 #[derive(Genesis, DispatchCall, MessageCodec, DefaultRuntime)]
 #[serialization(borsh::BorshDeserialize, borsh::BorshSerialize)]
@@ -70,18 +66,25 @@ use sov_value_setter::{ValueSetterRpcImpl, ValueSetterRpcServer};
     serialization(serde::Serialize, serde::Deserialize)
 )]
 pub struct Runtime<C: Context, Da: DaSpec> {
+    /// The Bank module.
     pub bank: sov_bank::Bank<C>,
+    /// The Sequencer Registry module.
     pub sequencer_registry: sov_sequencer_registry::SequencerRegistry<C, Da>,
     #[cfg_attr(feature = "native", cli_skip)]
+    /// The Blob Storage module.
     pub blob_storage: sov_blob_storage::BlobStorage<C, Da>,
     #[cfg_attr(feature = "native", cli_skip)]
+    /// The Chain State module.
     pub chain_state: sov_chain_state::ChainState<C, Da>,
+    /// The Value Setter module.
     pub value_setter: sov_value_setter::ValueSetter<C>,
+    /// The Accounts module.
     pub accounts: sov_accounts::Accounts<C>,
-    #[cfg_attr(feature = "native", cli_skip)]
-    #[cfg(feature = "experimental")]
-    pub evm: sov_evm::Evm<C>,
+    /// The NFT module.
     pub nft: sov_nft_module::NonFungibleToken<C>,
+    #[cfg(feature = "experimental")]
+    #[cfg_attr(feature = "native", cli_skip)]
+    pub evm: sov_evm::Evm<C>,
 }
 
 impl<C, Da> sov_modules_stf_template::Runtime<C, Da> for Runtime<C, Da>
