32a5cfb2d5b9ec80970dce8751aee707da40526e
bkolad <blazejkolad@gmail.com>
2023-10-09 21:57:53 +0200

    Update genesis

--- module-system/module-implementations/sov-evm/src/genesis.rs
+++ module-system/module-implementations/sov-evm/src/genesis.rs
@@ -8,6 +8,77 @@ use crate::evm::db_init::InitEvmDb;
 use crate::evm::primitive_types::Block;
 use crate::evm::{AccountInfo, EvmChainConfig};
 use crate::Evm;
+use reth_primitives::Address;
+use std::collections::HashMap;
+
+/// Evm account.
+#[derive(Clone, Debug, serde::Serialize, serde::Deserialize, Eq, PartialEq)]
+pub struct AccountData {
+    /// Account address.
+    pub address: Address,
+    /// Account balance.
+    pub balance: U256,
+    /// Code hash.
+    pub code_hash: H256,
+    /// Smart contract code.
+    pub code: Bytes,
+    /// Account nonce.
+    pub nonce: u64,
+}
+
+impl AccountData {
+    /// Empty code hash.
+    pub fn empty_code() -> H256 {
+        KECCAK_EMPTY
+    }
+
+    /// Account balance.
+    pub fn balance(balance: u64) -> U256 {
+        U256::from(balance)
+    }
+}
+
+/// Genesis configuration.
+#[derive(Clone, Debug, serde::Serialize, serde::Deserialize, Eq, PartialEq)]
+pub struct EvmConfig {
+    /// Genesis accounts.
+    pub data: Vec<AccountData>,
+    /// Chain id.
+    pub chain_id: u64,
+    /// Limits size of contract code size.
+    pub limit_contract_code_size: Option<usize>,
+    /// List of EVM hardforks by block number
+    pub spec: HashMap<u64, SpecId>,
+    /// Coinbase where all the fees go
+    pub coinbase: Address,
+    /// Starting base fee.
+    pub starting_base_fee: u64,
+    /// Gas limit for single block
+    pub block_gas_limit: u64,
+    /// Genesis timestamp.
+    pub genesis_timestamp: u64,
+    /// Delta to add to parent block timestamp,
+    pub block_timestamp_delta: u64,
+    /// Base fee params.
+    pub base_fee_params: reth_primitives::BaseFeeParams,
+}
+
+impl Default for EvmConfig {
+    fn default() -> Self {
+        Self {
+            data: vec![],
+            chain_id: 1,
+            limit_contract_code_size: None,
+            spec: vec![(0, SpecId::SHANGHAI)].into_iter().collect(),
+            coinbase: Address::zero(),
+            starting_base_fee: reth_primitives::constants::MIN_PROTOCOL_BASE_FEE,
+            block_gas_limit: reth_primitives::constants::ETHEREUM_BLOCK_GAS_LIMIT,
+            block_timestamp_delta: reth_primitives::constants::SLOT_DURATION.as_secs(),
+            genesis_timestamp: 0,
+            base_fee_params: reth_primitives::BaseFeeParams::ethereum(),
+        }
+    }
+}
 
 impl<C: sov_modules_api::Context> Evm<C> {
     pub(crate) fn init_module(
