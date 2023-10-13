7c529f3f8d2158c930b6e8c0cc8a97b290ccdb9a
bkolad <blazejkolad@gmail.com>
2023-10-13 11:22:33 +0200

    Add provers

---
+++ templates/rollup-template/provers/risc0/guest-mock/src/bin/mock_da.rs
@@ -0,0 +1,12 @@
+#![no_main]
+//! This binary implements the verification logic for the rollup. This is the code that runs inside
+//! of the zkvm in order to generate proofs for the rollup.
+use template_stf::{zk_stf, RollupVerifier, new_da_verifier, ZkvmGuest};
+
+pub fn main() {
+    let guest = ZkvmGuest::new();
+    let mut stf_verifier = RollupVerifier::new(zk_stf(), new_da_verifier());
+    stf_verifier
+        .run_block(guest)
+        .expect("Prover must be honest");
+}
