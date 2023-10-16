ea3dfa0b000ca101b9e83db9c26f9b3f1bb03170
Preston Evans <preston.b.evans@gmail.com>
2023-10-16 15:29:19 -0500

    remove dead runtime machinery

--- module-system/module-implementations/sov-blob-storage/tests/capability_tests.rs
+++ module-system/module-implementations/sov-blob-storage/tests/capability_tests.rs
@@ -16,7 +16,7 @@ use sov_rollup_interface::mocks::{
     MockAddress, MockBlob, MockBlock, MockBlockHeader, MockDaSpec, MockValidityCond, MockZkvm,
 };
 use sov_sequencer_registry::{SequencerConfig, SequencerRegistry};
-use sov_state::{ProverStorage, Storage};
+use sov_state::{DefaultStorageSpec, ProverStorage, Storage};
 
 type C = DefaultContext;
 type B = MockBlob;
@@ -85,8 +85,8 @@ fn make_blobs_by_slot(
 
 #[test]
 fn priority_sequencer_flow_general() {
-    let (app, runtime, genesis_root) = TestRuntime::pre_initialized(true);
-    let mut working_set = WorkingSet::new(app.current_storage.clone());
+    let (current_storage, runtime, genesis_root) = TestRuntime::pre_initialized(true);
+    let mut working_set = WorkingSet::new(current_storage.clone());
 
     let register_message = sov_sequencer_registry::CallMessage::Register {
         da_address: REGULAR_SEQUENCER_DA.as_ref().to_vec(),
@@ -148,8 +148,8 @@ fn priority_sequencer_flow_general() {
 
 #[test]
 fn test_blobs_from_non_registered_sequencers_are_not_saved() {
-    let (app, runtime, genesis_root) = TestRuntime::pre_initialized(true);
-    let mut working_set = WorkingSet::new(app.current_storage.clone());
+    let (current_storage, runtime, genesis_root) = TestRuntime::pre_initialized(true);
+    let mut working_set = WorkingSet::new(current_storage.clone());
 
     let unregistered_sequencer = MockAddress::new([7; 32]);
     let register_message = sov_sequencer_registry::CallMessage::Register {
@@ -221,8 +221,8 @@ fn test_blobs_from_non_registered_sequencers_are_not_saved() {
 
 #[test]
 fn test_blobs_no_deferred_without_preferred_sequencer() {
-    let (app, runtime, genesis_root) = TestRuntime::pre_initialized(false);
-    let mut working_set = WorkingSet::new(app.current_storage.clone());
+    let (current_storage, runtime, genesis_root) = TestRuntime::pre_initialized(false);
+    let mut working_set = WorkingSet::new(current_storage.clone());
 
     let register_message = sov_sequencer_registry::CallMessage::Register {
         da_address: REGULAR_SEQUENCER_DA.as_ref().to_vec(),
@@ -296,8 +296,8 @@ fn test_blobs_no_deferred_without_preferred_sequencer() {
 
 #[test]
 fn deferred_blobs_are_first_after_preferred_sequencer_exit() {
-    let (app, runtime, genesis_root) = TestRuntime::pre_initialized(true);
-    let mut working_set = WorkingSet::new(app.current_storage.clone());
+    let (current_storage, runtime, genesis_root) = TestRuntime::pre_initialized(true);
+    let mut working_set = WorkingSet::new(current_storage.clone());
 
     let register_message = sov_sequencer_registry::CallMessage::Register {
         da_address: REGULAR_SEQUENCER_DA.as_ref().to_vec(),
@@ -483,138 +483,30 @@ fn test_blob_priority_sorting() {
 #[serialization(borsh::BorshDeserialize, borsh::BorshSerialize)]
 struct TestRuntime<C: Context, Da: DaSpec> {
     pub bank: sov_bank::Bank<C>,
-    pub accounts: sov_accounts::Accounts<C>,
     pub sequencer_registry: sov_sequencer_registry::SequencerRegistry<C, Da>,
     pub chain_state: sov_chain_state::ChainState<C, Da>,
     pub blob_storage: sov_blob_storage::BlobStorage<C, Da>,
 }
 
-impl<C: Context, Da: DaSpec> Runtime<C, Da> for TestRuntime<C, Da> {}
-
-impl<C: Context, Da: DaSpec> BlobSelector<Da> for TestRuntime<C, Da> {
-    type Context = C;
-
-    fn get_blobs_for_this_slot<'a, I>(
-        &self,
-        _current_blobs: I,
-        _working_set: &mut WorkingSet<Self::Context>,
-    ) -> anyhow::Result<Vec<BlobRefOrOwned<'a, <Da as DaSpec>::BlobTransaction>>>
-    where
-        I: IntoIterator<Item = &'a mut <Da as DaSpec>::BlobTransaction>,
-    {
-        todo!()
-    }
-}
-
-impl<C: Context, Da: DaSpec> TxHooks for TestRuntime<C, Da> {
-    type Context = C;
-
-    fn pre_dispatch_tx_hook(
-        &self,
-        tx: &Transaction<Self::Context>,
-        working_set: &mut WorkingSet<C>,
-    ) -> anyhow::Result<<Self::Context as Spec>::Address> {
-        // Before executing a transaction, retrieve the sender's address from the accounts module
-        // and check the nonce
-        self.accounts.pre_dispatch_tx_hook(tx, working_set)
-    }
-
-    fn post_dispatch_tx_hook(
-        &self,
-        tx: &Transaction<Self::Context>,
-        working_set: &mut WorkingSet<C>,
-    ) -> anyhow::Result<()> {
-        // After executing each transaction, update the nonce
-        self.accounts.post_dispatch_tx_hook(tx, working_set)
-    }
-}
-
-impl<C: Context, Da: DaSpec> ApplyBlobHooks<Da::BlobTransaction> for TestRuntime<C, Da> {
-    type Context = C;
-    type BlobResult =
-        SequencerOutcome<<<Da as DaSpec>::BlobTransaction as BlobReaderTrait>::Address>;
-
-    fn begin_blob_hook(
-        &self,
-        blob: &mut Da::BlobTransaction,
-        working_set: &mut WorkingSet<C>,
-    ) -> anyhow::Result<()> {
-        // Before executing each batch, check that the sender is regsitered as a sequencer
-        self.sequencer_registry.begin_blob_hook(blob, working_set)
-    }
-
-    fn end_blob_hook(
-        &self,
-        result: Self::BlobResult,
-        working_set: &mut WorkingSet<C>,
-    ) -> anyhow::Result<()> {
-        // After processing each blob, reward or slash the sequencer if appropriate
-        match result {
-            SequencerOutcome::Rewarded(_reward) => {
-                // TODO: Process reward here or above.
-                <SequencerRegistry<C, Da> as ApplyBlobHooks<Da::BlobTransaction>>::end_blob_hook(
-                    &self.sequencer_registry,
-                    sov_sequencer_registry::SequencerOutcome::Completed,
-                    working_set,
-                )
-            }
-            SequencerOutcome::Ignored => Ok(()),
-            SequencerOutcome::Slashed {
-                reason,
-                sequencer_da_address,
-            } => <SequencerRegistry<C, Da> as ApplyBlobHooks<Da::BlobTransaction>>::end_blob_hook(
-                &self.sequencer_registry,
-                sov_sequencer_registry::SequencerOutcome::Slashed {
-                    sequencer: sequencer_da_address,
-                },
-                working_set,
-            ),
-        }
-    }
-}
-
-impl<C: Context, Da: DaSpec> SlotHooks<Da> for TestRuntime<C, Da> {
-    type Context = C;
-
-    fn begin_slot_hook(
-        &self,
-        _slot_header: &Da::BlockHeader,
-        _validity_condition: &Da::ValidityCondition,
-        _pre_state_root: &<<Self::Context as Spec>::Storage as Storage>::Root,
-        _working_set: &mut sov_modules_api::WorkingSet<C>,
-    ) {
-    }
-
-    fn end_slot_hook(&self, _working_set: &mut sov_modules_api::WorkingSet<C>) {}
-}
-
-impl<C: Context, Da: sov_modules_api::DaSpec> FinalizeHook<Da> for TestRuntime<C, Da> {
-    type Context = C;
-
-    fn finalize_hook(
-        &self,
-        _root_hash: &<<Self::Context as Spec>::Storage as Storage>::Root,
-        _accessory_working_set: &mut AccessoryWorkingSet<C>,
-    ) {
-    }
-}
-
 impl TestRuntime<DefaultContext, MockDaSpec> {
     pub fn pre_initialized(
         with_preferred_sequencer: bool,
-    ) -> (
-        AppTemplate<DefaultContext, MockDaSpec, MockZkvm, Self>,
-        Self,
-        jmt::RootHash,
-    ) {
-        use sov_rollup_interface::stf::StateTransitionFunction;
+    ) -> (ProverStorage<DefaultStorageSpec>, Self, jmt::RootHash) {
+        use sov_modules_api::Genesis;
         let tmpdir = tempfile::tempdir().unwrap();
         let storage = ProverStorage::with_path(tmpdir.path()).unwrap();
 
         let genesis_config = Self::build_genesis_config(with_preferred_sequencer);
-        let mut app = AppTemplate::new(storage, Self::default());
-        let root = app.init_chain(genesis_config);
-        (app, Default::default(), root)
+        let runtime: Self = Default::default();
+
+        let mut working_set = WorkingSet::new(storage.clone());
+        runtime.genesis(&genesis_config, &mut working_set).unwrap();
+
+        let (reads_writes, witness) = working_set.checkpoint().freeze();
+        let genesis_root = storage.validate_and_commit(reads_writes, &witness).unwrap();
+
+        // let root = storage.validate_and_commit()
+        (storage, runtime, genesis_root)
     }
 
     fn build_genesis_config(
@@ -643,11 +535,8 @@ impl TestRuntime<DefaultContext, MockDaSpec> {
             current_time: Default::default(),
         };
 
-        let accounts_config = AccountConfig { pub_keys: vec![] };
-
         GenesisConfig {
             bank: bank_config,
-            accounts: accounts_config,
             sequencer_registry: sequencer_registry_config,
             chain_state: chain_state_config,
             blob_storage: (),
