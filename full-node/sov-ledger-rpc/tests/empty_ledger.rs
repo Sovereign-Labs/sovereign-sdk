42b45d005feed070d246653bce421e5500146a3f
Filippo Costa <filippo@sovlabs.io>
2023-10-17 20:19:23 +0200

    Implement ledger_getEvents
    Signed-off-by: Filippo Costa <filippo@sovlabs.io>

--- full-node/sov-ledger-rpc/tests/empty_ledger.rs
+++ full-node/sov-ledger-rpc/tests/empty_ledger.rs
@@ -7,7 +7,11 @@ use sov_db::ledger_db::LedgerDB;
 use sov_ledger_rpc::client::RpcClient;
 use sov_ledger_rpc::server::rpc_module;
 use sov_ledger_rpc::HexHash;
-use sov_rollup_interface::rpc::{BatchResponse, QueryMode, SlotResponse, TxResponse};
+use sov_modules_api::Event;
+use sov_rollup_interface::rpc::{
+    BatchResponse, EventIdentifier, QueryMode, SlotResponse, TxIdAndOffset, TxIdentifier,
+    TxResponse,
+};
 use tempfile::tempdir;
 
 async fn rpc_server() -> (jsonrpsee::server::ServerHandle, SocketAddr) {
@@ -58,6 +62,7 @@ async fn getters_succeed() {
         .get_transactions(vec![], QueryMode::Compact)
         .await
         .unwrap();
+    rpc_client.get_events(vec![]).await.unwrap();
 
     let hash = HexHash([0; 32]);
     rpc_client
@@ -129,4 +134,42 @@ async fn get_head_with_optional_query_mode() {
             .await
             .unwrap();
     }
+}
+
+/// `ledger_getEvents` supports several parameter types, because of a
+/// `jsonrpsee` limitation. See:
+/// - https://github.com/Sovereign-Labs/sovereign-sdk/pull/1058
+/// - https://github.com/Sovereign-Labs/sovereign-sdk/issues/1037
+///
+/// While `jsonrpsee` macro-generated clients can only generate nested array
+/// types as parameters (e.g. `"params": [[1, 2, 3]]`), we want to test that
+/// non-nested array types are also supported (e.g. `"params": [1, 2, 3]` and
+/// `"params": [{"txId": 1, "offset": 2}]`).
+#[tokio::test]
+async fn get_events_patterns() {
+    let (_server_handle, addr) = rpc_server().await;
+    let rpc_client = rpc_client(addr).await;
+
+    rpc_client
+        .get_events(vec![EventIdentifier::Number(2)])
+        .await
+        .unwrap();
+    rpc_client
+        .request::<Vec<Option<Event>>, _>("ledger_getEvents", vec![vec![2]])
+        .await
+        .unwrap();
+    rpc_client
+        .request::<Vec<Option<Event>>, _>("ledger_getEvents", vec![2])
+        .await
+        .unwrap();
+    rpc_client
+        .request::<Vec<Option<Event>>, _>(
+            "ledger_getEvents",
+            vec![EventIdentifier::TxIdAndOffset(TxIdAndOffset {
+                tx_id: TxIdentifier::Number(1),
+                offset: 2,
+            })],
+        )
+        .await
+        .unwrap();
 }
