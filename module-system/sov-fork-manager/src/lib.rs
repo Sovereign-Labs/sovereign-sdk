a596724f224357a7e17425151a5bcdd03b9905cf
Nikolai Golub <nikolai@sovlabs.io>
2023-10-17 16:56:43 +0200

    [no ci] simplify fork manager from creating its own storage

--- module-system/sov-fork-manager/src/lib.rs
+++ module-system/sov-fork-manager/src/lib.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 use std::hash::Hash;
 
+use jmt::storage::TreeWriter;
 // use std::sync::{Arc, RwLock};
 use sov_rollup_interface::da::{BlockHeaderTrait, DaSpec};
 use sov_state::storage::{QuerySnapshotLayers, Snapshot, SnapshotId, StorageKey, StorageValue};
@@ -65,10 +66,7 @@ where
     Da: DaSpec,
     Da::SlotHash: Hash,
 {
-    pub fn new_locked(
-        db: sov_db::state_db::StateDB,
-        native_db: sov_db::native_db::NativeDB,
-    ) -> Self {
+    pub fn new(db: sov_db::state_db::StateDB, native_db: sov_db::native_db::NativeDB) -> Self {
         Self {
             db,
             native_db,
@@ -119,7 +117,14 @@ where
             .unwrap();
         self.snapshots.insert(snapshot_block_hash.clone(), snapshot);
     }
+}
 
+impl<S, Da> ForkManager<S, Da>
+where
+    S: Snapshot + Into<(jmt::storage::NodeBatch, sov_state::OrderedReadsAndWrites)>,
+    Da: DaSpec,
+    Da::SlotHash: Hash,
+{
     fn remove_snapshot(&mut self, block_hash: &Da::SlotHash) -> S {
         let snapshot = self
             .snapshots
@@ -133,13 +138,32 @@ where
         snapshot
     }
 
+    fn commit_snapshot(&self, snapshot: S) {
+        let (node_batch, accessory_writes) = snapshot.into();
+        {
+            self.db
+                .write_node_batch(&node_batch)
+                .expect("db write must succeed");
+
+            self.native_db
+                .set_values(
+                    accessory_writes
+                        .ordered_writes
+                        .iter()
+                        .map(|(k, v_opt)| {
+                            (k.key.to_vec(), v_opt.as_ref().map(|v| v.value.to_vec()))
+                        })
+                        .collect(),
+                )
+                .expect("native db write must succeed");
+
+            self.db.inc_next_version();
+        }
+    }
+
     pub fn finalize_snapshot(&mut self, block_hash: &Da::SlotHash) {
-        let _snapshot = self.remove_snapshot(block_hash);
-        // let payload = snapshot.into();
-        // {
-        //     let mut db = self.db.lock().unwrap();
-        //     db.commit(payload);
-        // }
+        let snapshot = self.remove_snapshot(block_hash);
+        self.commit_snapshot(snapshot);
 
         if let Some(parent_block_hash) = self.blocks_to_parent.remove(block_hash) {
             let mut to_discard: Vec<_> = self
@@ -210,140 +234,31 @@ mod tests {
 
         let db = sov_db::state_db::StateDB::with_path(tmpdir.path()).unwrap();
         let native_db = sov_db::native_db::NativeDB::with_path(tmpdir.path()).unwrap();
-        let fork_manager = ForkManager::<MockSnapshot, Da>::new_locked(db, native_db);
+        let fork_manager = ForkManager::<MockSnapshot, Da>::new(db, native_db);
         assert!(fork_manager.is_empty());
     }
 
-    // use crate::db::Database;
-    // use crate::state::{StateCheckpoint, DB};
-    // use crate::BlockHash;
-    //
-    // fn write_values(
-    //     db: DB,
-    //     snapshot_ref: TreeQuery<Database, BlockStateManager<Database, FrozenSnapshot, BlockHash>>,
-    //     values: &[(&str, &str)],
-    // ) -> FrozenSnapshot {
-    //     let checkpoint = StateCheckpoint::new(snapshot_ref);
-    //     let mut working_set = checkpoint.into_revertable();
-    //     for (key, value) in values {
-    //         let key = Key::from(key.to_string());
-    //         let value = Value::from(value.to_string());
-    //         working_set.set(&key, value);
-    //     }
-    //     let checkpoint = working_set.commit();
-    //     let (_witness, snapshot) = checkpoint.freeze();
-    //     snapshot
-    // }
-    //
-    // mod fork_tree_manager {
-    //     use super::*;
-    //
-    //     #[test]
-    //     fn new() {
-    //         let db = DB::default();
-    //         let state_manager =
-    //             BlockStateManager::<Database, FrozenSnapshot, BlockHash>::new_locked(db.clone());
-    //         let state_manager = state_manager.write().unwrap();
-    //         assert!(state_manager.self_ref.is_some());
-    //         {
-    //             let db = db.lock().unwrap();
-    //             assert!(db.data.is_empty());
-    //         }
-    //         assert!(state_manager.is_empty());
-    //     }
-    //
-    //     #[test]
-    //     fn linear_progression_with_2_blocks_delay() {
-    //         let db = DB::default();
-    //         let state_manager = BlockStateManager::new_locked(db.clone());
-    //         let mut state_manager = state_manager.write().unwrap();
-    //         assert!(state_manager.is_empty());
-    //         let genesis_block = "genesis".to_string();
-    //         let block_a = "a".to_string();
-    //         let block_b = "b".to_string();
-    //         let block_c = "c".to_string();
-    //
-    //         // Block A
-    //         let block_a_values = vec![("x", "1"), ("y", "2")];
-    //         let snapshot_ref = state_manager.get_new_ref(&genesis_block, &block_a);
-    //
-    //         assert!(!state_manager.is_empty());
-    //         let snapshot = write_values(db.clone(), snapshot_ref, &block_a_values);
-    //         state_manager.add_snapshot(snapshot);
-    //         assert!(!state_manager.is_empty());
-    //         {
-    //             assert!(db.lock().unwrap().data.is_empty());
-    //         }
-    //
-    //         // Block B
-    //         let block_b_values = vec![("x", "3"), ("z", "4")];
-    //         let snapshot_ref = state_manager.get_new_ref(&block_a, &block_b);
-    //         let snapshot = write_values(db.clone(), snapshot_ref, &block_b_values);
-    //         let snapshot_id_b = snapshot.get_id().clone();
-    //         state_manager.add_snapshot(snapshot);
-    //         assert_eq!(
-    //             Some(CacheValue::from(Value::from("1".to_string()))),
-    //             state_manager.get_value_recursively(
-    //                 &snapshot_id_b,
-    //                 &CacheKey::from(Key::from("x".to_string()))
-    //             )
-    //         );
-    //         {
-    //             assert!(db.lock().unwrap().data.is_empty());
-    //         }
-    //         println!("AFTER B: {:?}", state_manager);
-    //         // Finalizing A
-    //         state_manager.finalize_snapshot(&block_a);
-    //         {
-    //             let db = db.lock().unwrap();
-    //             assert!(!db.data.is_empty());
-    //             assert_eq!(Some("1".to_string()), db.get("x"));
-    //             assert_eq!(Some("2".to_string()), db.get("y"));
-    //             assert_eq!(None, db.get("z"));
-    //         }
-    //         println!("AFTER FINALIZING A: {:?}", state_manager);
-    //
-    //         // Block C
-    //         let block_c_values = vec![("x", "5"), ("z", "6")];
-    //         let snapshot_ref = state_manager.get_new_ref(&block_b, &block_c);
-    //         let snapshot = write_values(db.clone(), snapshot_ref, &block_c_values);
-    //         state_manager.add_snapshot(snapshot);
-    //         println!("AFTER C: {:?}", state_manager);
-    //         // Finalizing B
-    //         state_manager.finalize_snapshot(&block_b);
-    //         assert!(!state_manager.is_empty());
-    //         {
-    //             let db = db.lock().unwrap();
-    //             assert!(!db.data.is_empty());
-    //             assert_eq!(Some("3".to_string()), db.get("x"));
-    //             assert_eq!(Some("2".to_string()), db.get("y"));
-    //             assert_eq!(Some("4".to_string()), db.get("z"));
-    //         }
-    //
-    //         state_manager.finalize_snapshot(&block_c);
-    //         // TODO: Finalize everything, it should be clean
-    //         println!("AFTER FINALIZING C: {:?}", state_manager);
-    //         assert!(state_manager.is_empty());
-    //     }
-    //
-    //     #[test]
-    //     #[ignore = "TBD"]
-    //     fn fork_added() {}
-    //
-    //     #[test]
-    //     #[ignore = "TBD"]
-    //     fn adding_alien_snapshot() {}
-    //
-    //     #[test]
-    //     #[ignore = "TBD"]
-    //     fn finalizing_alien_block() {}
-    //
-    //     #[test]
-    //     #[ignore = "TBD"]
-    //     fn finalizing_same_block_hash_twice() {}
-    //
-    //     #[test]
-    //     #[ignore = "TBD"]
-    //     fn requesting_ref_from_same_block_twice() {}
-    // }
+    #[test]
+    #[ignore = "TBD"]
+    fn linear_progression_with_2_blocks_delay() {}
+
+    #[test]
+    #[ignore = "TBD"]
+    fn fork_added() {}
+
+    #[test]
+    #[ignore = "TBD"]
+    fn adding_alien_snapshot() {}
+
+    #[test]
+    #[ignore = "TBD"]
+    fn finalizing_alien_block() {}
+
+    #[test]
+    #[ignore = "TBD"]
+    fn finalizing_same_block_hash_twice() {}
+
+    #[test]
+    #[ignore = "TBD"]
+    fn requesting_ref_from_same_block_twice() {}
 }
