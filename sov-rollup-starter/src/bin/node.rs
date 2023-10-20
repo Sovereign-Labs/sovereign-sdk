b9f320845d39a02966631521ec6690c2053dc698
bkolad <blazejkolad@gmail.com>
2023-10-20 14:10:56 +0200

    change logging level

--- sov-rollup-starter/src/bin/node.rs
+++ sov-rollup-starter/src/bin/node.rs
@@ -27,7 +27,7 @@ async fn main() -> Result<(), anyhow::Error> {
     // Initializing logging
     tracing_subscriber::registry()
         .with(fmt::layer())
-        .with(EnvFilter::from_str("debug,hyper=info").unwrap())
+        .with(EnvFilter::from_default_env())
         .init();
 
     let args = Args::parse();
