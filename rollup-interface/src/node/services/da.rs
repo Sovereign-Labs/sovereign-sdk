344b0740a0d6fc2444046981ee918ee039ed9b0c
Blazej Kolad <blazejkolad@gmail.com>
2023-12-13 14:52:15 +0100

    Update rollup-interface/src/node/services/da.rs
    Co-authored-by: Nikolai Golub <nikolai@sovlabs.io>

--- rollup-interface/src/node/services/da.rs
+++ rollup-interface/src/node/services/da.rs
@@ -116,7 +116,7 @@ pub trait DaService: Send + Sync + 'static {
         aggregated_proof_data: &[u8],
     ) -> Result<u64, Self::Error>;
 
-    /// Fetches all aggregated zk proofs at a specified block heigh.
+    /// Fetches all aggregated ZK proofs at a specified block height.
     async fn get_aggregated_proofs_at(&self, height: u64) -> Result<Vec<Vec<u8>>, Self::Error>;
 }
 
