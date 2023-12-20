93cdb85ce734306dd3892ef15c4e574c9ab5891c
bkolad <blazejkolad@gmail.com>
2023-12-20 14:13:12 +0100

    Fix lint

--- examples/demo-rollup/tests/test_helpers.rs
+++ examples/demo-rollup/tests/test_helpers.rs
@@ -4,8 +4,9 @@ use demo_stf::genesis_config::GenesisPaths;
 use sov_demo_rollup::MockDemoRollup;
 use sov_mock_da::{MockAddress, MockDaConfig};
 use sov_modules_rollup_blueprint::RollupBlueprint;
-use sov_stf_runner::ProverServiceConfig;
-use sov_stf_runner::{RollupConfig, RollupProverConfig, RpcConfig, RunnerConfig, StorageConfig};
+use sov_stf_runner::{
+    ProverServiceConfig, RollupConfig, RollupProverConfig, RpcConfig, RunnerConfig, StorageConfig,
+};
 use tokio::sync::oneshot;
 
 pub async fn start_rollup(
