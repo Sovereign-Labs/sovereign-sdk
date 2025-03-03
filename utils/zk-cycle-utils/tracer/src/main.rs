0950bf5150846dd4370b1862b19c7edb5673e231
leopardracer <136604165+leopardracer@users.noreply.github.com>
2025-03-04 00:48:48 +0200

    Update main.rs

--- utils/zk-cycle-utils/tracer/src/main.rs
+++ utils/zk-cycle-utils/tracer/src/main.rs
@@ -74,7 +74,7 @@ fn get_cycle_count(insn: &str) -> Result<usize, &'static str> {
     }
 }
 
-fn print_intruction_counts(first_header: &str,
+fn print_instruction_counts(first_header: &str,
                            count_vec: Vec<(String, usize)>,
                            top_n: usize,
                            strip_hashes: bool,
@@ -328,7 +328,7 @@ fn main() -> std::io::Result<()> {
     println!("\n\nTotal instructions in trace: {}", total_lines);
     if !no_stack_counts {
         println!("\n\n Instruction counts considering call graph");
-        print_intruction_counts("Function Name", raw_counts, top_n, strip_hashes,Some(&exclude_view));
+        print_instruction_counts("Function Name", raw_counts, top_n, strip_hashes,Some(&exclude_view));
     }
 
     let mut raw_counts: Vec<(String, usize)> = counts_without_callgraph
@@ -338,7 +338,7 @@ fn main() -> std::io::Result<()> {
     raw_counts.sort_by(|a, b| b.1.cmp(&a.1));
     if !no_raw_counts {
         println!("\n\n Instruction counts ignoring call graph");
-        print_intruction_counts("Function Name",raw_counts, top_n, strip_hashes,Some(&exclude_view));
+        print_instruction_counts("Function Name",raw_counts, top_n, strip_hashes,Some(&exclude_view));
     }
 
     let mut raw_counts: Vec<(String, usize)> = filtered_stack_counts
@@ -361,7 +361,7 @@ fn main() -> std::io::Result<()> {
     raw_counts.sort_by(|a, b| b.1.cmp(&a.1));
     if let Some(f) = function_name {
         println!("\n\n Stack patterns for function '{f}' ");
-        print_intruction_counts("Function Stack",raw_counts, top_n, strip_hashes,None);
+        print_instruction_counts("Function Stack",raw_counts, top_n, strip_hashes,None);
     }
     Ok(())
 }
