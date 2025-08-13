2ced90d416594232ca5e7c6649a7e45d898d1631
Nikolai Golub <nikolai@sovlabs.io>
2025-08-13 16:34:37 +0200

    Nonces start from 0

--- crates/module-system/module-implementations/sov-uniqueness/src/lib.rs
+++ crates/module-system/module-implementations/sov-uniqueness/src/lib.rs
@@ -88,12 +88,14 @@ impl<S: Spec> Uniqueness<S> {
         credential_id: &CredentialId,
         state: &mut Reader,
     ) -> Result<u64, anyhow::Error> {
-        self.nonces.get(credential_id, state).map(|maybe_nonce| {
-            maybe_nonce
-                .unwrap_or_default()
-                .checked_add(1)
-                .ok_or(anyhow::anyhow!("Maximum nonce value reached"))
-        })?
+        self.nonces
+            .get(credential_id, state)
+            .map(|maybe_nonce| match maybe_nonce {
+                None => Ok(0),
+                Some(existing) => existing
+                    .checked_add(1)
+                    .ok_or(anyhow::anyhow!("Maximum nonce value reached")),
+            })?
     }
 }
 
