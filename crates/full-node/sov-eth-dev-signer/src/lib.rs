6ed0341c536ecff1981f4043452e8110989bce90
Leonid Logvinov <logvinov.leon@gmail.com>
2025-08-14 15:45:17 +0200

    Hide transaction generator behind the arbitrary feature

--- crates/full-node/sov-eth-dev-signer/src/lib.rs
+++ crates/full-node/sov-eth-dev-signer/src/lib.rs
@@ -1,9 +1,11 @@
 #![deny(missing_docs)]
 #![doc = include_str!("../README.md")]
 
+#[cfg(feature = "arbitrary")]
 mod randomness;
 mod signer;
 mod transaction;
+#[cfg(feature = "arbitrary")]
 pub mod transfer_generator;
 
 pub use signer::{Error, Signer, Signers};
