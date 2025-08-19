1819d0532a131a40656e5db366947be52a69226d
Leonid Logvinov <logvinov.leon@gmail.com>
2025-08-19 15:48:10 +0200

    Serialize Headers in a bincode-compatible way

--- crates/module-system/module-implementations/sov-evm/src/evm/primitive_types.rs
+++ crates/module-system/module-implementations/sov-evm/src/evm/primitive_types.rs
@@ -1,9 +1,10 @@
 use std::ops::Range;
 
-use alloy_consensus::Header;
-use alloy_primitives::{Address, Sealable, Sealed};
+use alloy_consensus::{serde_bincode_compat, Header};
+use alloy_primitives::{Address, Sealable, Sealed, B256};
 use reth_primitives::{TransactionSigned, TransactionSignedEcRecovered};
 use revm::primitives::EVMError;
+use serde_with::serde_as;
 use sov_modules_api::macros::UniversalWallet;
 
 /// RLP encoded evm transaction.
@@ -41,9 +42,11 @@ impl TransactionSignedAndRecovered {
     }
 }
 
+#[serde_as]
 #[derive(Debug, PartialEq, Clone, serde::Serialize, serde::Deserialize)]
 pub struct Block {
     /// Block header.
+    #[serde_as(as = "serde_bincode_compat::Header")]
     pub(crate) header: Header,
 
     /// Transactions in this block.
@@ -59,7 +62,7 @@ impl Block {
     }
 }
 
-#[derive(Debug, PartialEq, Clone, serde::Serialize, serde::Deserialize)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct SealedBlock {
     /// Block header.
     pub(crate) header: Sealed<Header>,
@@ -77,6 +80,51 @@ impl SealedBlock {
     /// Returns the block transactions.
     pub fn transactions(&self) -> &Range<u64> {
         &self.transactions
+    }
+}
+
+impl serde::Serialize for SealedBlock {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        use serde::ser::SerializeStruct;
+
+        let mut s = serializer.serialize_struct("SealedBlock", 3)?;
+        // serialize inner Header using bincode-compat wrapper
+        s.serialize_field(
+            "header",
+            &serde_bincode_compat::Header::from(self.header.inner()),
+        )?;
+        s.serialize_field("seal", &self.header.seal())?;
+        s.serialize_field("transactions", &self.transactions)?;
+        s.end()
+    }
+}
+
+impl<'de> serde::Deserialize<'de> for SealedBlock {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        #[serde_as]
+        #[derive(serde::Deserialize)]
+        struct Raw {
+            #[serde_as(as = "serde_bincode_compat::Header")]
+            header: Header,
+            seal: B256,
+            transactions: Range<u64>,
+        }
+
+        let Raw {
+            header,
+            seal,
+            transactions,
+        } = Raw::deserialize(deserializer)?;
+        Ok(SealedBlock {
+            header: Sealed::new_unchecked(header, seal),
+            transactions,
+        })
     }
 }
 
