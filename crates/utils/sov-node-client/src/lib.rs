39d186a921b023d4364edaddc457b4db859ff3fb
bkolad <blazejkolad@gmail.com>
2025-08-25 09:50:02 +0200

    Better comment

--- crates/utils/sov-node-client/src/lib.rs
+++ crates/utils/sov-node-client/src/lib.rs
@@ -202,7 +202,7 @@ impl NodeClient {
         Ok(amount)
     }
 
-    /// Get balance of the user.
+    /// Get balance of the hoder.
     pub async fn get_balance_for_holder<S: sov_modules_api::Spec>(
         &self,
         holder: &str,
