706d24694f05a6724eaddd16893b352822b97d27
Preston Evans <preston.b.evans@gmail.com>
2025-08-27 11:20:37 +0200

    Fix non-native feature

--- crates/full-node/sov-metrics/src/lib.rs
+++ crates/full-node/sov-metrics/src/lib.rs
@@ -71,6 +71,11 @@ impl MaybeTimer {
     pub fn elapsed(&self) -> std::time::Duration {
         std::time::Duration::from_secs(0)
     }
+
+    /// Returns the elapsed time since the timer if the `native` feature is enabled. Otherwise does nothing.
+    pub fn stop_and_get_elapsed(&mut self) -> std::time::Duration {
+        std::time::Duration::from_secs(0)
+    }
 }
 
 #[cfg(feature = "native")]
