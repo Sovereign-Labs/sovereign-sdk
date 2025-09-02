8f4b3e6d8e2d5a80f392afa3af5ce78cc8b27a9e
Leonid Logvinov <logvinov.leon@gmail.com>
2025-09-02 14:25:24 +0200

    Simplify the inspector

--- crates/module-system/module-implementations/sov-evm/src/sov_evm/inspector.rs
+++ crates/module-system/module-implementations/sov-evm/src/sov_evm/inspector.rs
@@ -8,8 +8,8 @@ use revm::{
 /// An Inspector that erases the costs of storage access
 #[derive(Clone, Debug, Default)]
 pub struct UnmeteredStorageAccessInspector {
-    /// Keep track of the last opcode executed and the remaining gas
-    last_opcode_gas_remaining: Option<(OpCode, u64)>,
+    /// Keep track of the remaining gas before opcode execution
+    last_gas_remaining: Option<u64>,
 }
 
 impl UnmeteredStorageAccessInspector {
@@ -23,20 +23,20 @@ where
     CTX: ContextTr,
 {
     fn step(&mut self, interp: &mut Interpreter, _: &mut CTX) {
-        if let Some(opcode) = OpCode::new(interp.bytecode.opcode()) {
-            // keep track of the last opcode executed
-            self.last_opcode_gas_remaining = Some((opcode, interp.gas.remaining()));
-        }
+        self.last_gas_remaining = Some(interp.gas.remaining());
     }
 
     fn step_end(&mut self, interp: &mut Interpreter, _: &mut CTX) {
-        if let Some((opcode, gas_remaining)) = self.last_opcode_gas_remaining.take() {
-            // compute gas usage for the last opcode
+        let gas_remaining = self
+            .last_gas_remaining
+            .take()
+            .expect("step() is always called before step_end()");
+        // if storage access - revert gas changes
+        let opcode = OpCode::new(interp.bytecode.opcode());
+        if opcode == Some(OpCode::SSTORE) || opcode == Some(OpCode::SLOAD) {
+            // compute gas usage for the opcode
             let gas_cost = gas_remaining.saturating_sub(interp.gas.remaining());
-            // if storage access - revert gas changes
-            if opcode == OpCode::SSTORE || opcode == OpCode::SLOAD {
-                interp.gas.erase_cost(gas_cost);
-            }
+            interp.gas.erase_cost(gas_cost);
         }
     }
 }
