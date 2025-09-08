a3b10f56be6676917cd22c329db7ae4a6a92f541
bkolad <blazejkolad@gmail.com>
2025-09-08 08:44:18 +0200

    Evm logs

--- crates/module-system/sov-test-utils/src/evm/test-data/Store.sol
+++ crates/module-system/sov-test-utils/src/evm/test-data/Store.sol
@@ -4,6 +4,16 @@
 pragma solidity ^0.8.0;
 contract SimpleStorage {
     uint256 public num;
+
+    event SimpleLog1(
+        address indexed addr,    
+        string indexed note,
+    );
+
+     event SimpleLog2(
+        address indexed addr,    
+        uint256 indexed value,
+    );
     
     function set(uint256 _num) public {
         num = _num;
@@ -15,5 +25,13 @@ contract SimpleStorage {
 
     function alwaysRevert() external pure {
         revert("This function always reverts!");
+    }
+
+    function emitLog1(string calldata _note) external pure {
+        emit SimpleLog(msg.sender, _note);
+    }
+
+    function emitLog2() external pure {
+        emit SimpleLo2(msg.sender, _num);
     }
 }
