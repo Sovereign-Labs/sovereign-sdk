7c14e1050afc37950deb48995a302285975b180e
bkolad <blazejkolad@gmail.com>
2025-09-08 14:50:27 +0200

    tests

--- crates/module-system/sov-test-utils/src/evm/test-data/Store.sol
+++ crates/module-system/sov-test-utils/src/evm/test-data/Store.sol
@@ -5,14 +5,9 @@ pragma solidity ^0.8.0;
 contract SimpleStorage {
     uint256 public num;
 
-    event SimpleLog1(
+     event SimpleLog(
         address indexed addr,    
-        string indexed note,
-    );
-
-     event SimpleLog2(
-        address indexed addr,    
-        uint256 indexed value,
+        uint256 value
     );
     
     function set(uint256 _num) public {
@@ -27,11 +22,13 @@ contract SimpleStorage {
         revert("This function always reverts!");
     }
 
-    function emitLog1(string calldata _note) external pure {
-        emit SimpleLog(msg.sender, _note);
+
+    function emitOneLog() public {
+        emit SimpleLog(msg.sender, num);
     }
 
-    function emitLog2() external pure {
-        emit SimpleLo2(msg.sender, _num);
+    function emitTwoLog() public {
+        emit SimpleLog(msg.sender, num);
+        emit SimpleLog(msg.sender, num + 1);
     }
 }
