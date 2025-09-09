4529fd924f1925e98d1c24a98da854cc6ae668bc
bkolad <blazejkolad@gmail.com>
2025-09-09 08:13:56 +0200

    Put cached

--- crates/module-system/module-implementations/sov-evm/src/db/mod.rs
+++ crates/module-system/module-implementations/sov-evm/src/db/mod.rs
@@ -9,6 +9,7 @@ use sov_address::{EthereumAddress, FromVmAddress};
 use sov_modules_api::BorshSerializedSize;
 use sov_modules_api::{Spec, StateAccessor, StateMap, StateReader};
 use sov_state::codec::BcsCodec;
+use sov_state::SlotKey;
 use sov_state::User;
 use std::fmt::{self, Debug};
 
@@ -81,12 +82,25 @@ where
     }
 
     fn code_by_hash(&mut self, code_hash: B256) -> Result<Bytecode, Self::Error> {
+        let key = SlotKey::from(code_hash.to_vec());
+
+        if let Some(code) = self.state.get_cached::<CachedByteCode>(key.clone()) {
+            return Ok(code.code.clone());
+        }
+
         // TODO move to new_raw_with_hash for better performance
         let bytecode = self
             .code
             .get(&code_hash, self.state)
             .map_err(Error)?
             .unwrap_or_default();
+
+        self.state.put_cached::<CachedByteCode>(
+            key,
+            CachedByteCode {
+                code: bytecode.clone(),
+            },
+        );
 
         Ok(bytecode)
     }
