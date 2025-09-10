349c9652b55b05bbc4fa7594b8bb153c035e71c8
Theodore Bugnet <theodore@sovlabs.io>
2025-09-10 11:00:28 +0100

    Try to de-flaky tests

--- crates/full-node/sov-sequencer/tests/integration/preferred_end_to_end.rs
+++ crates/full-node/sov-sequencer/tests/integration/preferred_end_to_end.rs
@@ -661,6 +661,7 @@ async fn flaky_seq_behind_deferred_slots_count_simple_lagging() {
         let _ = da_layer.produce_block().await;
         sleep(Duration::from_millis(50)).await; // Notifications don't work during recovery.
     }
+    sleep(Duration::from_millis(100)).await;
     test_rollup.wait_for_sequencer_ready().await.unwrap();
 
     // Submit the same transaction to the now-working sequencer
@@ -804,10 +805,11 @@ async fn seq_behind_deferred_slots_count_with_shutdown() {
 
     // Give time for the sequencer to catch up its visible state number
     tracing::info!("Producing DA blocks to let the sequencer resync.");
-    for _ in 0..10 {
+    for _ in 0..20 {
         test_rollup.da_service.produce_block_now().await.unwrap();
         sleep(Duration::from_millis(50)).await;
     }
+    tokio::time::sleep(Duration::from_millis(100)).await;
     test_rollup.wait_for_sequencer_ready().await.unwrap();
 
     // Submit the same transaction to the now-working sequencer
