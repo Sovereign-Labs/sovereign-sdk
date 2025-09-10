76fe21318c41ebb4d32efae7289525de85f89230
bkolad <blazejkolad@gmail.com>
2025-09-10 12:37:56 +0200

    test logs

--- examples/demo-rollup/tests/evm/evm_logs.rs
+++ examples/demo-rollup/tests/evm/evm_logs.rs
@@ -1,5 +1,3 @@
-use std::hash::Hash;
-
 use super::evm_test_helper;
 use crate::evm::evm_test_helper::setup;
 
@@ -12,15 +10,17 @@ async fn evm_test_logs() {
 
     test_rollup.wait_for_next_blocks(1).await;
 
-    let pending_log = evm_client.emit_one_log(contract_address).await;
-    let tx_hash_1 = pending_log.tx_hash();
+    let set_arg = 1;
+    let pending_log = evm_client.set_value(contract_address, set_arg).await;
+    let tx_hash = pending_log.tx_hash();
+
     test_rollup.wait_for_next_blocks(1).await;
-    let rec = evm_client.receipt(tx_hash_1).await.unwrap();
+    let rec = evm_client.receipt(tx_hash).await.unwrap();
     let log = rec.logs.first().unwrap();
 
+    assert_eq!(log.transaction_hash.unwrap(), tx_hash);
     assert_eq!(log.address, contract_address);
-    assert_eq!(log.transaction_hash, tx_hash_1);
-    //log.data
 
-    //println!("logs {:?}", rec.logs);
+    let value_from_logs = log.data.0.slice(28..32);
+    assert_eq!(*value_from_logs, set_arg.to_be_bytes());
 }
