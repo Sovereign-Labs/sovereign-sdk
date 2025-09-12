77d2d1ef33a1d53c4b7209036eda9eaf77b733fd
bkolad <blazejkolad@gmail.com>
2025-09-12 13:11:28 +0200

    fix

--- examples/demo-rollup/tests/evm/evm_logs.rs
+++ examples/demo-rollup/tests/evm/evm_logs.rs
@@ -17,28 +17,30 @@ struct SimpleLog {
 async fn evm_test_logs() {
     let (test_rollup, evm_client, _) = setup(0).await;
 
-    let contract_address = evm_test_helper::deploy_contract_check(&evm_client)
-        .await
-        .unwrap();
+    /*  let contract_address = evm_test_helper::deploy_contract_check(&evm_client)
+            .await
+            .unwrap();
 
-    test_rollup.wait_for_next_blocks(1).await;
+        test_rollup.wait_for_next_blocks(1).await;
 
-    let set_arg = 1;
-    let pending_log = evm_client.set_value(contract_address, set_arg).await;
-    let tx_hash = pending_log.tx_hash();
+        let set_arg = 1;
+        let pending_log = evm_client.set_value(contract_address, set_arg).await;
+        let tx_hash = pending_log.tx_hash();
 
-    test_rollup.wait_for_next_blocks(1).await;
-    let rec = evm_client.receipt(tx_hash).await.unwrap();
-    let log = rec.logs.first().unwrap();
+        test_rollup.wait_for_next_blocks(1).await;
+        let rec = evm_client.receipt(tx_hash).await.unwrap();
+        let log = rec.logs.first().unwrap();
 
-    let contract_log = SimpleStorageContract::parse_simple_log(log.clone());
+        let contract_log = SimpleStorageContract::parse_simple_log(log.clone());
 
-    assert_eq!(contract_log.original.transaction_hash.unwrap(), tx_hash);
-    assert_eq!(contract_log.original.address, contract_address);
+        assert_eq!(contract_log.original.transaction_hash.unwrap(), tx_hash);
+        assert_eq!(contract_log.original.address, contract_address);
 
-    assert_eq!(contract_log.paresed.value, set_arg.into());
-
-    //let mut sub = evm_client.subscribe_logs().await;
-    //let log = sub.recv().await.unwrap();
-    //println!("{:?}", log);
+        assert_eq!(contract_log.paresed.value, set_arg.into());
+    */
+    let mut sub = evm_client.subscribe_logs().await;
+    loop {
+        let log = sub.recv().await.unwrap();
+        println!("{:?}", log);
+    }
 }
