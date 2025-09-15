88a6121ea92a3ca8a8e15b964c7f7e1ad0b70b43
Leonid Logvinov <logvinov.leon@gmail.com>
2025-09-15 12:04:45 +0200

    Revert "Upgrade RETH to 1.7.0 (#1668)"
    This reverts commit 675f7bbe798c66da383d3dac0ed8837de5e70b89.

--- crates/module-system/module-implementations/sov-evm/src/rpc/mod.rs
+++ crates/module-system/module-implementations/sov-evm/src/rpc/mod.rs
@@ -154,7 +154,7 @@ where
         let balance = self
             .get_db(state)
             .basic(address)
-            .map_err(|e| eth_api_into_rpc_error(EthApiError::from(e)))?
+            .map_err(EthApiError::from)?
             .map(|account| account.balance)
             .unwrap_or_default();
 
@@ -615,7 +615,12 @@ fn eth_from_evm_error<Ws: StateAccessor>(err: EVMError<crate::db::Error<Ws>>) ->
 
 impl<Ws: StateAccessor> From<crate::db::Error<Ws>> for EthApiError {
     fn from(err: crate::db::Error<Ws>) -> Self {
-        EthApiError::EvmCustom(format!("Database error: {err}"))
+        RpcInvalidTransactionError::other(ErrorObject::owned(
+            -32603,
+            format!("Database error: {err}"),
+            None::<()>,
+        ))
+        .into()
     }
 }
 
