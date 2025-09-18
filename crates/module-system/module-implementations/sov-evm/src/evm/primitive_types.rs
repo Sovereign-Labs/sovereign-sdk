9b6ecd4f82993a7eec0f7dc482e04127b08bf5d1
bkolad <blazejkolad@gmail.com>
2025-09-18 14:51:37 +0200

    fix

--- crates/module-system/module-implementations/sov-evm/src/evm/primitive_types.rs
+++ crates/module-system/module-implementations/sov-evm/src/evm/primitive_types.rs
@@ -158,15 +158,15 @@ impl<'de> serde::Deserialize<'de> for SealedBlock {
 #[cfg(feature = "native")]
 /// Sealed or pending block.
 pub enum MaybeSealedBlock {
-    /// TODO
+    /// SealedBlock
     Sealed(SealedBlock),
-    /// TODO
+    /// Pending
     Pending(crate::Block),
 }
 
 #[cfg(feature = "native")]
 impl MaybeSealedBlock {
-    /// TODO
+    /// Hash of the block.
     pub fn hash(&self) -> Option<B256> {
         match self {
             Self::Sealed(block) => Some(block.header.hash()),
@@ -174,7 +174,7 @@ impl MaybeSealedBlock {
         }
     }
 
-    /// TODO
+    /// The block number.
     pub fn number(&self) -> u64 {
         match self {
             Self::Sealed(block) => block.header.number,
@@ -182,7 +182,7 @@ impl MaybeSealedBlock {
         }
     }
 
-    /// TODO
+    /// Index of the first transaction in the block.
     pub fn transactions_start(&self) -> u64 {
         match self {
             Self::Sealed(block) => block.transactions.start,
@@ -190,7 +190,7 @@ impl MaybeSealedBlock {
         }
     }
 
-    /// TODO
+    /// Index of the last transaction in the block.
     pub fn transactions_end(&self) -> u64 {
         match self {
             Self::Sealed(block) => block.transactions.end,
@@ -198,7 +198,7 @@ impl MaybeSealedBlock {
         }
     }
 
-    /// TODO
+    /// The block timestamp.
     pub fn timestamp(&self) -> u64 {
         match self {
             Self::Sealed(block) => block.header.timestamp,
