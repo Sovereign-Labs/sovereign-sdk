86acabc6761ba16406f4b807b2327170e6e56702
bkolad <blazejkolad@gmail.com>
2025-09-18 14:48:45 +0200

    fix

--- examples/demo-rollup/tests/evm/evm_logs.rs
+++ examples/demo-rollup/tests/evm/evm_logs.rs
@@ -1,6 +1,7 @@
-use alloy_rpc_types_eth::{BlockNumberOrTag, Filter};
-
 use crate::evm::evm_test_helper::setup;
+use alloy_primitives::B256;
+use alloy_primitives::U256;
+use alloy_rpc_types_eth::{BlockNumberOrTag, Filter};
 
 #[tokio::test(flavor = "multi_thread")]
 async fn evm_test_get_logs() {
@@ -29,19 +30,29 @@ async fn evm_test_get_logs() {
     let rec = evm_client.alloy_receipt(tx_hash).await.unwrap();
     let block_hash = rec.block_hash.unwrap();
 
-    let filter = Filter::new().at_block_hash(block_hash);
-    let logs = evm_client.get_logs(&filter).await;
+    {
+        let filter = Filter::new().at_block_hash(block_hash);
+        let logs = evm_client.get_logs(&filter).await;
+        assert_eq!(logs.len() as u32, nb_of_txs * nb_of_logs_per_tx);
 
-    assert_eq!(logs.len() as u32, nb_of_txs * nb_of_logs_per_tx);
+        for (index, log) in logs.into_iter().enumerate() {
+            let index = index as u64;
+            assert!(filter.matches(log.inner.as_ref()));
+            assert_eq!(log.log_index.unwrap(), index);
+            assert_eq!(
+                log.transaction_index.unwrap(),
+                (index / nb_of_logs_per_tx as u64)
+            );
+        }
+    }
 
-    for (index, log) in logs.into_iter().enumerate() {
-        let index = index as u64;
-        assert!(filter.matches(log.inner.as_ref()));
-        assert_eq!(log.log_index.unwrap(), index);
-        assert_eq!(
-            log.transaction_index.unwrap(),
-            (index / nb_of_logs_per_tx as u64)
-        );
+    // topic3 seolects one log from each tx
+    {
+        let topic: B256 = U256::from(3).into();
+        let filter = Filter::new().topic3(topic);
+
+        let logs = evm_client.get_logs(&filter).await;
+        check_logs(&filter, logs, nb_of_txs);
     }
 }
 
@@ -71,13 +82,31 @@ async fn evm_test_get_logs_range() {
     }
     test_rollup.wait_for_next_blocks(1).await;
 
-    let filter = Filter::new()
-        .from_block(start_block)
-        .to_block(BlockNumberOrTag::Latest);
+    // Check logs from all txs.
+    {
+        let filter = Filter::new()
+            .from_block(start_block)
+            .to_block(BlockNumberOrTag::Latest);
 
-    let logs = evm_client.get_logs(&filter).await;
-    assert_eq!(logs.len() as u32, nb_of_txs * nb_of_logs_per_tx);
+        let logs = evm_client.get_logs(&filter).await;
+        check_logs(&filter, logs, nb_of_txs * nb_of_logs_per_tx);
+    }
 
+    // topic3 seolects one log from each tx
+    {
+        let topic: B256 = U256::from(3).into();
+        let filter = Filter::new()
+            .from_block(start_block)
+            .to_block(BlockNumberOrTag::Latest)
+            .topic3(topic);
+
+        let logs = evm_client.get_logs(&filter).await;
+        check_logs(&filter, logs, nb_of_txs);
+    }
+}
+
+fn check_logs(filter: &Filter, logs: Vec<alloy_rpc_types_eth::Log>, expected_nb_of_logs: u32) {
+    assert_eq!(logs.len() as u32, expected_nb_of_logs);
     for log in logs {
         assert!(filter.matches(log.inner.as_ref()));
     }
