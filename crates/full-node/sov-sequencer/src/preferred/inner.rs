f3e22488dbf98629c2df0fa9c253803f7a9733f7
bkolad <blazejkolad@gmail.com>
2025-09-23 09:41:26 +0200

    Add warning

--- crates/full-node/sov-sequencer/src/preferred/inner.rs
+++ crates/full-node/sov-sequencer/src/preferred/inner.rs
@@ -287,6 +287,10 @@ where
 
         let state_roots = self.executor.state_roots.clone();
 
+        if state_roots.len() > 50 {
+            tracing::warn!("Execitor: The computed state roots map is large, and cloning it can be costly in terms of time. state_roots len: {}", state_roots.len());
+        }
+
         let notification = StartBlockNotification {
             state_roots,
             data: start_block_data,
