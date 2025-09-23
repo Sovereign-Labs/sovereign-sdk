25dec8a9c6a6e38865487aabba8a5d8ddfe7916f
Theodore Bugnet <theodore@sovlabs.io>
2025-09-23 19:12:54 +0100

    Add standard authentication to Eip712Authenticator

--- crates/module-system/module-implementations/sov-evm/src/lib.rs
+++ crates/module-system/module-implementations/sov-evm/src/lib.rs
@@ -33,8 +33,9 @@ mod helpers;
 use alloy_primitives::U256;
 use alloy_primitives::{Address, B256};
 pub use authenticate::{
-    authenticate, decode_evm_tx, Eip712Authenticator, EthereumAuthenticator, EvmAuthenticator,
-    EvmAuthenticatorInput, SchemaProvider,
+    authenticate, decode_evm_tx, Eip712Authenticator, Eip712AuthenticatorInput,
+    Eip712AuthenticatorTrait, EthereumAuthenticator, EvmAuthenticator, EvmAuthenticatorInput,
+    SchemaProvider,
 };
 pub use revm::primitives::hardfork::SpecId;
 use sov_address::{EthereumAddress, FromVmAddress};
