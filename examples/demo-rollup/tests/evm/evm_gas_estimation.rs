2871d53851e80b935d76fdfcac8a404015bea6dd
Leonid Logvinov <logvinov.leon@gmail.com>
2025-09-26 13:01:18 +0200

    Post-merge fixes

--- examples/demo-rollup/tests/evm/evm_gas_estimation.rs
+++ examples/demo-rollup/tests/evm/evm_gas_estimation.rs
@@ -1,11 +1,12 @@
 use crate::evm::evm_test_helper::setup;
-use ethers_core::abi::Address;
+use crate::evm::evm_test_helper::EVM_EXTENSION;
+use ethers::abi::Address;
 
 #[tokio::test(flavor = "multi_thread")]
 async fn simple_transfer() -> anyhow::Result<()> {
-    let (_, test_client, _, _) = setup(0).await;
+    let (_, test_client, _) = setup(0, EVM_EXTENSION).await;
 
-    let simple_transfer = test_client.make_eip1559_tx(0, Some(Address::zero()), None);
+    let simple_transfer = test_client.make_tx(Some(Address::zero()), None);
     let gas_estimation = test_client.eth_estimate_gas(simple_transfer).await;
     assert_eq!(gas_estimation, 100_000); // Simple transfer consumes 0 EVM gas so we only see the ABSOLUTE_MARGIN
     Ok(())
@@ -13,9 +14,9 @@ async fn simple_transfer() -> anyhow::Result<()> {
 
 #[tokio::test(flavor = "multi_thread")]
 async fn contract_deploy() -> anyhow::Result<()> {
-    let (_, test_client, _, _) = setup(0).await;
+    let (_, test_client, _) = setup(0, EVM_EXTENSION).await;
 
-    let deploy_tx = test_client.make_eip1559_tx(0, None, Some(test_client.contract.byte_code()));
+    let deploy_tx = test_client.make_tx(None, Some(test_client.contract.byte_code()));
     let gas_estimation = test_client.eth_estimate_gas(deploy_tx).await;
     assert_eq!(gas_estimation, (208_049 / 2) * 3 + 100_000);
     Ok(())
