f690f9b8c1c34bd567743416196923e6efe9a000
Leonid Logvinov <logvinov.leon@gmail.com>
2025-09-30 11:49:05 +0200

    Reorder functions by importance
    - Move run() function to top as main entry point
    - Follow with execute_swap() as primary business logic
    - Then add_initial_liquidity() as setup function
    - Deploy functions at bottom as utility/infrastructure

--- crates/utils/sov-evm-soak-testing/src/uniswap.rs
+++ crates/utils/sov-evm-soak-testing/src/uniswap.rs
@@ -15,6 +15,74 @@ where
     _pair: Pair::PairInstance<&'a P, N>,
 }
 
+pub async fn run(client: TestClient) -> Result<()> {
+    let signer = Address::from_slice(client.address().as_bytes());
+    let client = &client.alloy_client;
+    
+    let contracts = deploy_uniswap_contracts(client).await?;
+    
+    let weth_liquidity = parse_ether("1000")?;
+    let usdc_liquidity = parse_ether("2000")?;
+    add_initial_liquidity(&contracts, signer, weth_liquidity, usdc_liquidity).await?;
+    
+    let swap_amount = parse_ether("10")?;
+    let swap_path = vec![*contracts.usdc.address(), *contracts.weth.address()];
+    execute_swap(&contracts, signer, swap_amount, swap_path).await?;
+    
+    Ok(())
+}
+
+async fn execute_swap<P, N>(
+    contracts: &Contracts<'_, P, N>,
+    signer: Address,
+    amount_in: U256,
+    path: Vec<Address>,
+) -> Result<()>
+where
+    P: Provider<N> + Clone + Send + Sync,
+    N: Network + Send + Sync,
+{
+    let expected_out = contracts.router.getAmountsOut(amount_in, path.clone()).call().await?[1];
+    println!("Swapping {} -> {} ETH", format_ether(amount_in), format_ether(expected_out));
+
+    let from_token = if path[0] == *contracts.usdc.address() {
+        &contracts.usdc
+    } else {
+        &contracts.weth
+    };
+
+    mint_and_approve(from_token, signer, *contracts.router.address(), amount_in).await?;
+    contracts.router.swapExactTokensForTokens(amount_in, expected_out, path, signer).submit().await?;
+    
+    Ok(())
+}
+
+async fn add_initial_liquidity<P, N>(
+    contracts: &Contracts<'_, P, N>,
+    signer: Address,
+    weth_amount: U256,
+    usdc_amount: U256,
+) -> Result<()>
+where
+    P: Provider<N> + Clone + Send + Sync,
+    N: Network + Send + Sync,
+{
+    mint_and_approve(&contracts.weth, signer, *contracts.router.address(), weth_amount).await?;
+    mint_and_approve(&contracts.usdc, signer, *contracts.router.address(), usdc_amount).await?;
+
+    contracts.router
+        .addLiquidity(
+            *contracts.weth.address(),
+            *contracts.usdc.address(),
+            weth_amount,
+            usdc_amount,
+        )
+        .submit()
+        .await?;
+    
+    Ok(())
+}
+
 async fn deploy_uniswap_contracts<P, N>(
     client: &P,
 ) -> Result<Contracts<'_, P, N>>
@@ -45,33 +113,6 @@ where
     })
 }
 
-async fn add_initial_liquidity<P, N>(
-    contracts: &Contracts<'_, P, N>,
-    signer: Address,
-    weth_amount: U256,
-    usdc_amount: U256,
-) -> Result<()>
-where
-    P: Provider<N> + Clone + Send + Sync,
-    N: Network + Send + Sync,
-{
-    mint_and_approve(&contracts.weth, signer, *contracts.router.address(), weth_amount).await?;
-    mint_and_approve(&contracts.usdc, signer, *contracts.router.address(), usdc_amount).await?;
-
-    contracts.router
-        .addLiquidity(
-            *contracts.weth.address(),
-            *contracts.usdc.address(),
-            weth_amount,
-            usdc_amount,
-        )
-        .submit()
-        .await?;
-    
-    Ok(())
-}
-
-
 async fn mint_and_approve<P, N>(
     token: &Erc20::Erc20Instance<P, N>, 
     to: Address, 
@@ -84,47 +125,5 @@ where
 {
     token.mint(to, amount).submit().await?;
     token.approve(spender, amount).submit().await?;
-    Ok(())
-}
-
-async fn execute_swap<P, N>(
-    contracts: &Contracts<'_, P, N>,
-    signer: Address,
-    amount_in: U256,
-    path: Vec<Address>,
-) -> Result<()>
-where
-    P: Provider<N> + Clone + Send + Sync,
-    N: Network + Send + Sync,
-{
-    let expected_out = contracts.router.getAmountsOut(amount_in, path.clone()).call().await?[1];
-    println!("Swapping {} -> {} ETH", format_ether(amount_in), format_ether(expected_out));
-
-    let from_token = if path[0] == *contracts.usdc.address() {
-        &contracts.usdc
-    } else {
-        &contracts.weth
-    };
-
-    mint_and_approve(from_token, signer, *contracts.router.address(), amount_in).await?;
-    contracts.router.swapExactTokensForTokens(amount_in, expected_out, path, signer).submit().await?;
-    
-    Ok(())
-}
-
-pub async fn run(client: TestClient) -> Result<()> {
-    let signer = Address::from_slice(client.address().as_bytes());
-    let client = &client.alloy_client;
-    
-    let contracts = deploy_uniswap_contracts(client).await?;
-    
-    let weth_liquidity = parse_ether("1000")?;
-    let usdc_liquidity = parse_ether("2000")?;
-    add_initial_liquidity(&contracts, signer, weth_liquidity, usdc_liquidity).await?;
-    
-    let swap_amount = parse_ether("10")?;
-    let swap_path = vec![*contracts.usdc.address(), *contracts.weth.address()];
-    execute_swap(&contracts, signer, swap_amount, swap_path).await?;
-    
     Ok(())
 }
