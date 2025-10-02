02b706a516ddb3251a37047f653f587b44a171fe
Preston Evans <preston.b.evans@gmail.com>
2025-10-02 17:51:12 +0200

    Allow multithreading uniswap test

--- crates/utils/sov-evm-soak-testing/src/main.rs
+++ crates/utils/sov-evm-soak-testing/src/main.rs
@@ -1,3 +1,4 @@
+use alloy::hex;
 use alloy_primitives::Address;
 use anyhow::Result;
 use clap::Parser;
@@ -28,6 +29,9 @@ struct Args {
 
     #[command(subcommand)]
     test: TestType,
+
+    #[arg(short, long, default_value = "1")]
+    num_workers: usize,
 }
 
 #[derive(clap::Subcommand, Clone, Debug)]
@@ -46,18 +50,44 @@ enum TestType {
 async fn main() -> Result<()> {
     let args = Args::parse();
 
+    let mut handles: Vec<tokio::task::JoinHandle<anyhow::Result<()>>> =
+        Vec::with_capacity(args.num_workers);
     match args.test {
         TestType::Uniswap { count } => {
-            let client = RpcClient::new(&args.private_key, args.rpc_addr).await;
-            let signer = Address::from_slice(client.address().as_bytes());
-            let test = UniSoakTest::new(client.alloy_client, signer).await?;
-            test.run(count).await?;
+            for i in 0..args.num_workers {
+                // Tweak the private key to avoid conflicts between workers
+                let mut key_bytes: [u8; 32] =
+                    hex::decode(&args.private_key).unwrap().try_into().unwrap();
+                key_bytes[0] = key_bytes[0].wrapping_add(i as u8);
+                let key = hex::encode(key_bytes);
+                // Clone the RPC address to avoid lifetime issues
+                let addr = args.rpc_addr.clone();
+                // Spawn a new task for each worker
+                handles.push(tokio::spawn(async move {
+                    let client = RpcClient::new(&key, addr).await;
+                    let signer = Address::from_slice(&client.address().0);
+                    match UniSoakTest::new(client.alloy_client, signer).await {
+                        Ok(test) => {
+                            if let Err(e) = test.run(count).await {
+                                println!("Worker {i} error during run: {e:?}");
+                            }
+                        }
+                        Err(e) => {
+                            println!("Worker {i} failed to deploy contracts: {e:?}");
+                        }
+                    }
+                    Ok(())
+                }));
+            }
         }
         TestType::SimpleStorage => {
             let contract = SimpleStorage::default();
             let client = SimpleStorageClient::new(&args.private_key, contract, args.rpc_addr).await;
             simple_storage::run(client).await?;
         }
+    }
+    for handle in handles {
+        handle.await??;
     }
 
     Ok(())
