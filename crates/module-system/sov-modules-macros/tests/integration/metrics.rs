2f1def1825135478d8e4a25aea3ed815629a611e
Leonid Logvinov <logvinov.leon@gmail.com>
2025-10-06 19:27:52 +0200

    Fix metrics test to use &'static str for gas constant names

--- crates/module-system/sov-modules-macros/tests/integration/metrics.rs
+++ crates/module-system/sov-modules-macros/tests/integration/metrics.rs
@@ -19,7 +19,7 @@ fn test_metrics(input: &mut u64) {
 
     *input *= 10;
 
-    let constant = <S as Spec>::Gas::from([1, 1]).with_name("test".to_string());
+    let constant = <S as Spec>::Gas::from([1, 1]).with_name("test");
 
     meter.charge_gas(&constant).unwrap();
 
@@ -81,7 +81,7 @@ fn test_metrics_without_input() {
         S::initial_base_fee_per_gas(),
     );
 
-    let constant = <S as Spec>::Gas::from([1, 1]).with_name("test".to_string());
+    let constant = <S as Spec>::Gas::from([1, 1]).with_name("test");
 
     meter.charge_gas(&constant).unwrap();
 
