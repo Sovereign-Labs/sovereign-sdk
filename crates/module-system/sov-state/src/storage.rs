1950a03f11ae99601791fcb841b8bb0447da5acd
Preston Evans <preston.b.evans@gmail.com>
2025-10-07 23:39:26 +0200

    Fix test

--- crates/module-system/sov-state/src/storage.rs
+++ crates/module-system/sov-state/src/storage.rs
@@ -201,8 +201,8 @@ mod private {
         assert_eq!(key.0[..9], [6, 0, 0, 1, 2, 3, 4, 5, 6]);
 
         assert!(key.try_extend(&[1; REMAINING_BYTES]).is_ok());
-        assert_eq!(key.len_excluding_prefix(), 79);
-        assert_eq!(key.0[..9], [79, 0, 0, 1, 2, 3, 4, 5, 6]);
+        assert_eq!(key.len_excluding_prefix(), MAX_INLINE_KEY_LEN);
+        assert_eq!(key.0[..9], [MAX_INLINE_KEY_LEN as u8, 0, 0, 1, 2, 3, 4, 5, 6]);
         assert_eq!(&key.without_prefix()[6..], &[1; REMAINING_BYTES]);
     }
 }
