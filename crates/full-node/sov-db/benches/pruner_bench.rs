1e055d9caf34e4946782872a79071d32f59ece51
Preston Evans <preston.b.evans@gmail.com>
2025-10-09 17:03:03 +0200

    Make pruner batch size configurable

--- crates/full-node/sov-db/benches/pruner_bench.rs
+++ crates/full-node/sov-db/benches/pruner_bench.rs
@@ -63,7 +63,7 @@ fn bench_pruner(c: &mut Criterion) {
                 (tempdir, rocksdb)
             },
             |(_tempdir, rocksdb)| {
-                let pruner = Pruner::new(rocksdb.clone());
+                let pruner = Pruner::new(rocksdb.clone(), None);
                 let output = pruner
                     .collect_pruning_batch_for_module_accessory_state(100)
                     .unwrap();
