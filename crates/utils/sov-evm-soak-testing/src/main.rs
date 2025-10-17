20cdac09909c1c5bddf260258dff6b3da9fde8f1
Leonid Logvinov <logvinov.leon@gmail.com>
2025-10-17 11:58:20 +0200

    Use try_join_all

--- crates/utils/sov-evm-soak-testing/src/main.rs
+++ crates/utils/sov-evm-soak-testing/src/main.rs
@@ -1,10 +1,13 @@
 use alloy::providers::{Provider, ProviderBuilder};
 use alloy::signers::local::PrivateKeySigner;
 use alloy::{hex, providers::DynProvider};
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use clap::Parser;
+use clap::Subcommand;
+use futures::future::try_join_all;
 use reqwest::Url;
 use std::net::SocketAddr;
+use tokio::task::JoinHandle;
 
 use crate::{logs::LogsSoakTest, uniswap::UniSoakTest};
 
@@ -32,7 +35,7 @@ struct Args {
     test: TestType,
 }
 
-#[derive(clap::Subcommand, Clone, Debug)]
+#[derive(Subcommand, Clone, Debug)]
 enum TestType {
     /// Run Uniswap soak test
     Uniswap {
@@ -57,16 +60,13 @@ enum TestType {
 }
 
 // Tweak the private key to avoid conflicts between workers
-fn derive_worker_key(root_key: &str, idx: usize) -> anyhow::Result<String> {
+fn derive_worker_key(root_key: &str, idx: usize) -> Result<String> {
     let mut key_bytes: [u8; 32] = hex::decode(root_key)?.try_into().unwrap();
     key_bytes[0] = key_bytes[0].wrapping_add(idx as u8);
     Ok(hex::encode(key_bytes))
 }
 
-pub(crate) fn alloy_client(
-    socket: SocketAddr,
-    signer: PrivateKeySigner,
-) -> anyhow::Result<DynProvider> {
+pub(crate) fn alloy_client(socket: SocketAddr, signer: PrivateKeySigner) -> Result<DynProvider> {
     let url = Url::parse(&format!("http://{socket}/rpc"))?;
     let client = ProviderBuilder::new()
         .wallet(signer)
@@ -82,16 +82,14 @@ async fn main() -> Result<()> {
     match args.test {
         TestType::Uniswap { count, num_workers } => {
             if num_workers > 255 {
-                return Err(anyhow::anyhow!("num_workers must be less than 256 because of our private key tweaking. This is an easy fix, but we haven't done it yet."));
+                return Err(anyhow!("num_workers must be less than 256 because of our private key tweaking. This is an easy fix, but we haven't done it yet."));
             }
-            let mut handles: Vec<tokio::task::JoinHandle<anyhow::Result<()>>> =
-                Vec::with_capacity(num_workers);
+            let mut handles: Vec<JoinHandle<Result<()>>> = Vec::with_capacity(num_workers);
             for i in 0..num_workers {
-                let private_key = derive_worker_key(&args.private_key, i)?;
+                let signer: PrivateKeySigner = derive_worker_key(&args.private_key, i)?.parse()?;
+                let client = alloy_client(args.rpc_addr, signer.clone())?;
                 // Spawn a new task for each worker
                 handles.push(tokio::spawn(async move {
-                    let signer: PrivateKeySigner = private_key.parse()?;
-                    let client = alloy_client(args.rpc_addr, signer.clone())?;
                     match UniSoakTest::new(client, signer.address()).await {
                         Ok(test) => {
                             if let Err(e) = test.run(count).await {
@@ -105,10 +103,7 @@ async fn main() -> Result<()> {
                     Ok(())
                 }));
             }
-
-            for handle in handles {
-                handle.await??;
-            }
+            try_join_all(handles).await?;
         }
         TestType::SimpleStorage => {
             let signer: PrivateKeySigner = args.private_key.parse()?;
