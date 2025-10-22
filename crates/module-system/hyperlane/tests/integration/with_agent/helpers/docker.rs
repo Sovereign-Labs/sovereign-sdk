9dac2d44548512282fc674c11afe7b6710bc7ff6
ross-weir <29697678+ross-weir@users.noreply.github.com>
2025-10-22 21:36:50 +1100

    update hyperlane

--- crates/module-system/hyperlane/tests/integration/with_agent/helpers/docker.rs
+++ crates/module-system/hyperlane/tests/integration/with_agent/helpers/docker.rs
@@ -23,25 +23,25 @@ where
 pub async fn print_logs_from_exec_result(name: &str, result: &mut ExecResult, timeout: Duration) {
     let _span = tracing::info_span!("docker_exec_log", name = name).entered();
     let exit_code = result.exit_code().await.unwrap();
-    tracing::info!("Exit code  {exit_code:?}");
+    println!("Exit code  {exit_code:?}");
     let _ = tokio::time::timeout(timeout, async {
-        tracing::info!("Printing stdout");
+        println!("Printing stdout");
         let mut stdout = result.stdout().lines();
         while let Some(line) = stdout.next_line().await.unwrap() {
-            tracing::info!("stdout: {line}");
+            println!("stdout: {line}");
         }
     })
     .await;
-    tracing::info!("---- Done printing stdout");
+    println!("---- Done printing stdout");
     let _ = tokio::time::timeout(timeout, async {
-        tracing::info!("Printing stderr");
+        println!("Printing stderr");
         let mut stderr = result.stderr().lines();
         while let Some(line) = stderr.next_line().await.unwrap() {
-            tracing::info!("stderr: {line}");
+            println!("stderr: {line}");
         }
     })
     .await;
-    tracing::info!("---- Done printing stderr");
+    println!("---- Done printing stderr");
 }
 
 #[cfg_attr(target_os = "macos", allow(dead_code))]
