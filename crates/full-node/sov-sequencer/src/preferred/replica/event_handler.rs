4450c36308341e004a13e2ec199e7c9523856855
Blazej Kolad <blazejkolad@gmail.com>
2025-10-23 08:21:09 +0200

    Implement `process_do_batch_start_replica` and `process_do_new_tx_replica methods` (#1948)

--- crates/full-node/sov-sequencer/src/preferred/replica/event_handler.rs
+++ crates/full-node/sov-sequencer/src/preferred/replica/event_handler.rs
@@ -2,10 +2,43 @@ use crate::preferred::inner::SequencerStateUpdator;
 use crate::preferred::replica::db_data::DbData;
 use crate::preferred::replica::replica_sync_task::DBDataRejected;
 use crate::preferred::replica::replica_sync_task::ReplicaEventHandler;
+use crate::preferred::BatchCreationError;
+use crate::preferred::DoNewTxError;
+use crate::preferred::SequencerStateUpdatorError;
+use crate::SequencerNotReadyDetails;
 use async_trait::async_trait;
 use sov_modules_api::Runtime;
 use sov_modules_api::Spec;
 use std::sync::Arc;
+
+#[derive(Debug, thiserror::Error)]
+pub(crate) enum ReplicaError<S: Spec> {
+    #[error("The replica rejected the db data. DbData: {0:?}")]
+    Rejected(DBDataRejected),
+
+    #[error("Replica is not ready. Details: {0:?}. DbData: {1:?}")]
+    NotReady(SequencerNotReadyDetails, DbData),
+
+    #[error("Failed to create a new batch on the replica.")]
+    Creation(#[from] BatchCreationError),
+
+    #[error("Failed to apply a new transaction on the replica.")]
+    NewTx(DoNewTxError<S>),
+
+    #[error("The replica is shutting down.")]
+    Shutdown,
+    #[error("The replica encountered an unexpected shutdown.")]
+    UnexpectedShutdown,
+}
+
+impl<S: Spec> From<SequencerStateUpdatorError> for ReplicaError<S> {
+    fn from(value: SequencerStateUpdatorError) -> Self {
+        match value {
+            SequencerStateUpdatorError::Shutdown => Self::Shutdown,
+            SequencerStateUpdatorError::Unexpected => Self::UnexpectedShutdown,
+        }
+    }
+}
 
 #[async_trait]
 impl<S, Rt> ReplicaEventHandler for Arc<SequencerStateUpdator<S, Rt>>
