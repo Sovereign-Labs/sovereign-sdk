b0cca06744b66a4db1a3558364a0b849501a6b20
Nikolai Golub <nikolai@sovlabs.io>
2025-10-29 17:25:24 +0100

    Remove derived Debug for `CelestiaConfig`
    And add option for signer from environment variable

--- crates/adapters/celestia/src/config.rs
+++ crates/adapters/celestia/src/config.rs
@@ -2,9 +2,10 @@
 use std::num::NonZero;
 
 use schemars::JsonSchema;
+use std::fmt;
 
 /// Runtime configuration for the [`sov_rollup_interface::node::da::DaService`] implementation.
-#[derive(Debug, Clone, PartialEq, serde::Deserialize, serde::Serialize, JsonSchema)]
+#[derive(Clone, PartialEq, serde::Deserialize, serde::Serialize, JsonSchema)]
 pub struct CelestiaConfig {
     /// The address of the Celestia RPC server
     /// For example: ws://localhost:26658
@@ -14,7 +15,7 @@ pub struct CelestiaConfig {
     /// If not specified in the config will be pulled from `SOV_CELESTIA_RPC_AUTH_TOKEN`
     /// environment variable.
     /// Optional.
-    #[serde(alias = "celestia_rpc_auth_token")]
+    #[serde(default = "default_rpc_auth_token", alias = "celestia_rpc_auth_token")]
     pub rpc_auth_token: Option<String>,
 
     /// The address of the Celestia gRPC server, for example, http://localhost:9090
@@ -25,11 +26,14 @@ pub struct CelestiaConfig {
     /// The token for accessing Celestia gRPC server.
     /// If not specified in the config, will be pulled from `SOV_CELESTIA_GRPC_AUTH_TOKEN`.
     /// Optional, used only if `grpc_url` is set.
+    #[serde(default = "default_grpc_auth_token")]
     pub grpc_auth_token: Option<String>,
 
     /// The private key in hex format of Celestia wallet that has enough TIA to publish blobs.
+    /// If not specified in the config, will be pulled from `SOV_CELESTIA_SIGNER_KEY`.
     /// Can be done, for example:
     /// `celestia-appd keys export --unsafe --unarmored-hex key-name --keyring-backend test`
+    #[serde(default = "default_signer_private_key")]
     pub signer_private_key: Option<String>,
     /// The timeout for a Celestia RPC request, in seconds.
     #[serde(
@@ -59,6 +63,34 @@ pub struct CelestiaConfig {
     /// See [`backon::ExponentialBuilder`] for more details
     #[serde(default = "default_factor")]
     pub backoff_factor: f32,
+}
+
+impl fmt::Debug for CelestiaConfig {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("CelestiaConfig")
+            .field("rpc_url", &self.rpc_url)
+            .field(
+                "rpc_auth_token",
+                &self.rpc_auth_token.as_ref().map(|_| "REDACTED"),
+            )
+            .field("grpc_url", &self.grpc_url)
+            .field(
+                "grpc_auth_token",
+                &self.grpc_auth_token.as_ref().map(|_| "REDACTED"),
+            )
+            .field(
+                "signer_private_key",
+                &self.signer_private_key.as_ref().map(|_| "REDACTED"),
+            )
+            .field("request_timeout_secs", &self.request_timeout_secs)
+            .field("safe_lead_time_ms", &self.safe_lead_time_ms)
+            .field("tx_priority", &self.tx_priority)
+            .field("backoff_min_delay_ms", &self.backoff_min_delay_ms)
+            .field("backoff_max_delay_ms", &self.backoff_max_delay_ms)
+            .field("backoff_max_times", &self.backoff_max_times)
+            .field("backoff_factor", &self.backoff_factor)
+            .finish()
+    }
 }
 
 /// Custom type matching [`celestia_rpc::TxPriority`] but with `JsonSchema` support.
@@ -91,34 +123,16 @@ impl CelestiaConfig {
         backoff_policy
     }
 
-    /// Gets auth token from parsed config or from `SOV_CELESTIA_GRPC_AUTH_TOKEN` environment variable.
-    /// Returns None if both are unset.
-    /// Config value has priority over environment variable.
-    fn grpc_auth_token(&self) -> Option<String> {
-        self.grpc_auth_token
-            .clone()
-            .or_else(|| std::env::var("SOV_CELESTIA_GRPC_AUTH_TOKEN").ok())
-    }
-
-    /// Gets auth token from parsed config or from `SOV_CELESTIA_RPC_AUTH_TOKEN` environment variable
-    /// Returns None if both are unset.
-    /// Config value has priority over environment variable.
-    fn rpc_auth_token(&self) -> Option<String> {
-        self.rpc_auth_token
-            .clone()
-            .or_else(|| std::env::var("SOV_CELESTIA_RPC_AUTH_TOKEN").ok())
-    }
-
     pub(crate) async fn build_client(&self) -> anyhow::Result<celestia_client::Client> {
         let mut builder = celestia_client::Client::builder().rpc_url(&self.rpc_url);
-        if let Some(rpc_auth_token) = self.rpc_auth_token() {
-            builder = builder.rpc_auth_token(&rpc_auth_token);
+        if let Some(rpc_auth_token) = &self.rpc_auth_token {
+            builder = builder.rpc_auth_token(rpc_auth_token);
         }
         // Submission section.
         if let Some(grpc_url) = &self.grpc_url {
             builder = builder.grpc_url(grpc_url);
-            if let Some(grpc_auth_token) = self.grpc_auth_token() {
-                builder = builder.grpc_metadata("x-token", &grpc_auth_token);
+            if let Some(grpc_auth_token) = &self.grpc_auth_token {
+                builder = builder.grpc_metadata("x-token", grpc_auth_token);
             }
             if let Some(signer_key_hex) = &self.signer_private_key {
                 builder = builder.private_key_hex(signer_key_hex);
@@ -134,6 +148,18 @@ pub(crate) fn default_safe_lead_time_ms() -> u64 {
 
 fn default_rpc_addr() -> String {
     "ws://localhost:26658".into()
+}
+
+fn default_rpc_auth_token() -> Option<String> {
+    std::env::var("SOV_CELESTIA_RPC_AUTH_TOKEN").ok()
+}
+
+fn default_grpc_auth_token() -> Option<String> {
+    std::env::var("SOV_CELESTIA_GRPC_AUTH_TOKEN").ok()
+}
+
+fn default_signer_private_key() -> Option<String> {
+    std::env::var("SOV_CELESTIA_SIGNER_KEY").ok()
 }
 
 // Exponential backoff defaults:
