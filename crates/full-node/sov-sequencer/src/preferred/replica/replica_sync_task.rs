26af49b8d6a1659ff7f73cd24b8ae7b89a1f48e0
bkolad <blazejkolad@gmail.com>
2025-11-03 15:21:37 +0100

    fix

--- crates/full-node/sov-sequencer/src/preferred/replica/replica_sync_task.rs
+++ crates/full-node/sov-sequencer/src/preferred/replica/replica_sync_task.rs
@@ -366,7 +366,7 @@ mod tests {
             .await;
 
         // Wait for sync_task.start to spawn the sync task
-        tokio::time::sleep(Duration::from_millis(10)).await;
+        tokio::time::sleep(Duration::from_millis(100)).await;
 
         execute(db, test_cases).await;
 
