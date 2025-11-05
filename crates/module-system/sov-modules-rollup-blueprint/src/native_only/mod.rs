a49708a3def62db087e0d796754997a2921d3177
Nikolai Golub <nikolai@sovlabs.io>
2025-11-05 15:07:39 +0100

    Fix forgotten placeholders

--- crates/module-system/sov-modules-rollup-blueprint/src/native_only/mod.rs
+++ crates/module-system/sov-modules-rollup-blueprint/src/native_only/mod.rs
@@ -230,7 +230,7 @@ pub trait FullNodeBlueprint<M: ExecutionMode>: RollupBlueprint<M> {
                     background_handles,
                     proof_sender: sequencer,
                     api_ledger_db: api_ledger_db.clone(),
-                    da_address: da_service.get_signer().await.context("XX")?,
+                    da_address: da_service.get_signer().await.context("Full node with standard sequencer require DaService with signer support")?,
                 })
             }
             SequencerKindConfig::Preferred(seq_config) => {
@@ -260,7 +260,7 @@ pub trait FullNodeBlueprint<M: ExecutionMode>: RollupBlueprint<M> {
                     background_handles,
                     proof_sender: sequencer,
                     api_ledger_db: api_ledger_db.clone(),
-                    da_address: da_service.get_signer().await.context("XX")?,
+                    da_address: da_service.get_signer().await.context("Full node with preferred sequencer require DaService with signer support")?,
                 })
             }
         }
