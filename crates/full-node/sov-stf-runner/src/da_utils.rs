7227cc126a0e9d104a004b109aa67c327fa65c27
Nikolai Golub <nikolai@sovlabs.io>
2025-11-10 10:03:02 +0100

    Fix fetch_block_reorg_aware for lower polling times
    Now DaService::get_block_at only be cancelled if head changes

--- crates/full-node/sov-stf-runner/src/da_utils.rs
+++ crates/full-node/sov-stf-runner/src/da_utils.rs
@@ -1,18 +1,31 @@
 //! Helper utilities for interacting with the DA layer.
+
 use std::sync::atomic::Ordering;
 use std::time::Duration;
 
-use sov_rollup_interface::da::BlockHeaderTrait;
-use sov_rollup_interface::node::da::{DaService, SlotData};
+use sov_rollup_interface::node::da::DaService;
 use sov_rollup_interface::node::DaSyncState;
 
 const MAX_GET_BLOCK_ATTEMPTS: u32 = 10;
 
+async fn get_new_head_height_if_roll_back(
+    sync_state: &DaSyncState,
+    requested_height: u64,
+    polling_interval: Duration,
+) -> u64 {
+    loop {
+        let target_height = sync_state.target_da_height.load(Ordering::Relaxed);
+        if target_height < requested_height {
+            return target_height;
+        }
+        tokio::time::sleep(polling_interval).await;
+    }
+}
+
 /// Tries to fetch block at given height.
 /// If `DaSyncState.target_height` becomes lower in the case of re-org, the function fetches a new head instead.
 /// Because DaSyncState is polling target height periodically,
-/// there is a possibility that this function won't notice change in target height if the polling interval of DaSyncState is too high.
-/// To mitigate this, it retries to call `get_block_at` several times before giving up and returning an error.
+/// there is a possibility that this function won't notice change in target height if the polling interval of DaSyncState is too high, but that's fine
 pub(crate) async fn fetch_block_reorg_aware<Da: DaService>(
     da_service: &Da,
     sync_state: &DaSyncState,
@@ -26,68 +39,44 @@ pub(crate) async fn fetch_block_reorg_aware<Da: DaService>(
         total_timeout = ?da_total_timeout,
         "Fetch polling for a block"
     );
+
     let mut requested_height = height;
-    let mut interval = tokio::time::interval(polling_interval);
 
-    let check_height = |h| -> u64 {
-        let target_height = sync_state.target_da_height.load(Ordering::Relaxed);
-        // Allow requesting height next after head, this is a normal operation.
-        let highest_allowed_to_request = target_height.saturating_add(1);
-
-        if highest_allowed_to_request < h {
-            tracing::info!(
-                new_head_height = target_height,
-                h,
-                "Head height decreased below currently requesting, re-requesting at new head"
-            );
-            target_height
-        } else {
-            h
-        }
-    };
-
-    let mut attempt = 0;
-    let sleep = tokio::time::sleep(da_total_timeout);
-    tokio::pin!(sleep);
-
-    loop {
+    // During each iteration of the loop, it will try to fetch da block at given height
+    // If head rolls back below requested,
+    // requested height will be changed to height and new iteration of the loop continues
+    // We allow chain to consecutively rewind of `MAX_GET_BLOCK_ATTEMPTS` after that it will error
+    for attempt in 1..=MAX_GET_BLOCK_ATTEMPTS {
+        // If the head rolled back below requested height, we will try to request the head instead.
+        let rolled_back_head_future =
+            get_new_head_height_if_roll_back(sync_state, height, polling_interval);
+        // DaService suppose to do all necessary retries on failures.
+        // Runner only limits the total time of this activity.
+        let get_block_future =
+            tokio::time::timeout(da_total_timeout, da_service.get_block_at(requested_height));
         tokio::select! {
-            result = da_service.get_block_at(requested_height) => {
-                tracing::trace!(
-                    requested_height,
-                    original_height = height,
-                    is_err = result.is_err(),
-                    attempt,
-                    "Received result from `get_block_at`");
-                match result {
-                    Ok(block) => {
-                        tracing::trace!(block_header = %block.header().display(), "Block fetched, returning");
-                        return Ok(block);
+            get_block_result = get_block_future => {
+                // Just flatten timeout, but return as is
+                match get_block_result {
+                    Ok(inner_result) => {
+                        return inner_result.map_err(|error| anyhow::anyhow!("Error from DaService: {error:?}"));
                     }
-                    Err(err) => {
-                        tracing::trace!(?err, requested_height, attempt, "Error fetching block");
-                        attempt += 1;
-                        let requestable_height = check_height(requested_height);
-                        if requestable_height != requested_height {
-                            tracing::info!(requestable_height, "Request able height has changed, trying again");
-                            requested_height = requestable_height;
-                            continue;
-                        } else if attempt >= MAX_GET_BLOCK_ATTEMPTS {
-                            anyhow::bail!("Failed to fetch block after {MAX_GET_BLOCK_ATTEMPTS} attempts. Last error: {:?}", err);
-                        } else {
-                            // What if the target height is not updated, and we've returning early.
-                            // Basically we should note that if the polling interval is more than (block_time * attempts) it will error in case of rewind.
-                            tracing::info!(requestable_height, attempt, "Height hasn't changed, retrying again.");
-                        }
+                    Err(_) => {
+                        anyhow::bail!("Timeout getting block from DaService after {da_total_timeout:?}");
                     }
                 }
             }
-            _ = interval.tick() => {
-                requested_height = check_height(requested_height);
-            }
-            _ = &mut sleep => {
-                anyhow::bail!("Total timeout after {:?} while trying fetching block at height {}", da_total_timeout, requested_height);
+            rolled_back_height = rolled_back_head_future => {
+                tracing::warn!(
+                    requested_height,
+                    new_da_head_height = rolled_back_height,
+                    attempt,
+                    ouf_of_attempts = MAX_GET_BLOCK_ATTEMPTS,
+                    "DA head has rolled back below requested height. Updating requested height and retrying");
+                requested_height = rolled_back_height;
             }
         }
     }
+
+    anyhow::bail!("Failed to fetch block after {MAX_GET_BLOCK_ATTEMPTS} of attempts");
 }
