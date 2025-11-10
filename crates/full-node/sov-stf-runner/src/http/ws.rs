a2e3537bda3f1fd77e41b7fa9f6863b651c28048
Leonid Logvinov <logvinov.leon@gmail.com>
2025-11-10 14:55:19 +0100

    Introduce consume_until_shutdown abstraction

--- crates/full-node/sov-stf-runner/src/http/ws.rs
+++ crates/full-node/sov-stf-runner/src/http/ws.rs
@@ -6,8 +6,9 @@ use futures_util::sink::SinkExt;
 use futures_util::stream::StreamExt;
 use jsonrpsee::core::JsonRawValue;
 use jsonrpsee::RpcModule;
+use sov_rollup_interface::consume_until_shutdown;
+use tokio::spawn;
 use tokio::sync::{mpsc, watch};
-use tokio::{select, spawn};
 use tracing::{debug, error, trace};
 
 /// Convert content to a WebSocket message, using binary or text encoding
@@ -67,44 +68,46 @@ async fn socket_reader_task(
     rpc_methods: RpcModule<()>,
     mut shutdown_receiver: watch::Receiver<()>,
 ) {
-    loop {
-        select! {
-            Some(Ok(msg)) = ws_reader.next() => {
-                trace!(message = ?msg, "Message received from websocket");
-                match msg {
-                    Message::Text(text) => {
-                        handle_rpc_message(&text, &msg_tx, &rpc_methods, false, &shutdown_receiver).await;
-                    }
-                    Message::Binary(data) => {
-                        // Parse binary frame as UTF-8 JSON-RPC request
-                        match std::str::from_utf8(&data) {
-                            Ok(text) => {
-                                handle_rpc_message(text, &msg_tx, &rpc_methods, true, &shutdown_receiver).await;
-                            }
-                            Err(error) => {
-                                error!(%error, "Invalid UTF-8 in binary WebSocket frame");
-                            }
+    consume_until_shutdown! {
+        "WebSocket reader",
+        ws_reader.next(),
+        shutdown_receiver,
+        msg => {
+            let msg = match msg {
+                Err(error) => {
+                    debug!(%error, "WebSocket closed, stopping reader task");
+                    break;
+                },
+                Ok(msg) => msg,
+            };
+            match msg {
+                Message::Text(text) => {
+                    handle_rpc_message(&text, &msg_tx, &rpc_methods, false, &shutdown_receiver).await;
+                }
+                Message::Binary(data) => {
+                    // Parse binary frame as UTF-8 JSON-RPC request
+                    match std::str::from_utf8(&data) {
+                        Ok(text) => {
+                            handle_rpc_message(text, &msg_tx, &rpc_methods, true, &shutdown_receiver).await;
+                        }
+                        Err(error) => {
+                            error!(%error, "Invalid UTF-8 in binary WebSocket frame");
                         }
                     }
-                    Message::Pong(_) => {}
-                    Message::Ping(ping) => {
-                        if msg_tx.send(Message::Pong(ping)).await.is_err() {
-                            error!("Websocket sender has been closed, aborting websocket");
-                            break;
-                        }
-                    }
-                    Message::Close(_) => {
+                }
+                Message::Pong(_) => {}
+                Message::Ping(ping) => {
+                    if msg_tx.send(Message::Pong(ping)).await.is_err() {
+                        error!("Websocket sender has been closed, aborting websocket");
                         break;
                     }
                 }
-            }
-            _ = shutdown_receiver.changed() => {
-                debug!("Shutdown signal received, stopping WebSocket read handler");
-                break;
+                Message::Close(_) => {
+                    break;
+                }
             }
         }
     }
-    trace!("WebSocket read handler finished");
 }
 
 async fn handle_rpc_message(
@@ -145,23 +148,18 @@ async fn subscription_forwarder_task(
     use_binary: bool,
     mut shutdown_receiver: watch::Receiver<()>,
 ) {
-    loop {
-        select! {
-            Some(message) = receiver.recv() => {
-                trace!("Subscription message received: {}", message);
-                let sub_message = to_ws_message(message, use_binary);
-                if let Err(error) = msg_tx.send(sub_message).await {
-                    debug!(%error, "WebSocket closed, stopping subscription forwarding");
-                    break;
-                }
-            }
-            _ = shutdown_receiver.changed() => {
-                debug!("Shutdown signal received, stopping subscription forwarder");
+    consume_until_shutdown! {
+        "Subscription forwarder",
+        receiver.recv(),
+        shutdown_receiver,
+        message => {
+            let sub_message = to_ws_message(message, use_binary);
+            if let Err(error) = msg_tx.send(sub_message).await {
+                debug!(%error, "WebSocket closed, stopping subscription forwarding");
                 break;
             }
         }
     }
-    trace!("Subscription forwarding task finished");
 }
 
 async fn socket_writer_task(
@@ -169,20 +167,16 @@ async fn socket_writer_task(
     mut ws_writer: SplitSink<WebSocket, Message>,
     mut shutdown_receiver: watch::Receiver<()>,
 ) {
-    loop {
-        select! {
-            Some(response) = msg_rx.recv() => {
-                if let Err(error) = ws_writer.send(response).await {
-                    debug!(%error, "WebSocket closed, stopping writer task");
-                    break;
-                }
-                trace!("Message sent to websocket");
-            }
-            _ = shutdown_receiver.changed() => {
-                debug!("Shutdown signal received, stopping WebSocket write handler");
+    consume_until_shutdown! {
+        "WebSocket writer",
+        msg_rx.recv(),
+        shutdown_receiver,
+        response => {
+            if let Err(error) = ws_writer.send(response).await {
+                debug!(%error, "WebSocket closed, stopping writer task");
                 break;
             }
+            trace!("Message sent to websocket");
         }
     }
-    trace!("WebSocket write handler finished");
 }
