7dea03b6073d57ecb634af59d18f3a941306f6eb
Nikolai Golub <nikolai@sovlabs.io>
2025-11-12 11:56:37 +0100

    Fix fmt

--- crates/adapters/celestia/src/da_service/tests.rs
+++ crates/adapters/celestia/src/da_service/tests.rs
@@ -28,7 +28,11 @@ async fn collect_all_blobs_between(
 ) -> anyhow::Result<(Vec<BlobWithSender>, Vec<BlobWithSender>)> {
     // Adding one more height to the current head to accommodate for blob inclusion.
     // Even by the time submiPayForBlob has returned it should be included, we observed flakyness.
-    let height_after = da_service.get_head_block_header().await?.height().saturating_add(1);
+    let height_after = da_service
+        .get_head_block_header()
+        .await?
+        .height()
+        .saturating_add(1);
     let mut collected_batch_blobs = Vec::new();
     let mut collected_proof_blobs = Vec::new();
 
