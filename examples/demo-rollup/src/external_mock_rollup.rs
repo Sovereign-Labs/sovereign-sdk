89c79dcdf594033c8befc86648af049dbfe35853
Theodore Bugnet <theodore@sovlabs.io>
2025-11-13 17:53:58 +0000

    Remove spawn from remaining api handlers, fix Arc usages

--- examples/demo-rollup/src/external_mock_rollup.rs
+++ examples/demo-rollup/src/external_mock_rollup.rs
@@ -100,7 +100,7 @@ impl FullNodeBlueprint<Native> for ExternalMockDemoRollup<Native> {
 
     async fn sequencer_additional_apis<Seq>(
         &self,
-        sequencer: Arc<Seq>,
+        sequencer: Seq,
         rollup_config: &RollupConfig<<Self::Spec as Spec>::Address, Self::DaService>,
         shutdown_receiver: tokio::sync::watch::Receiver<()>,
     ) -> anyhow::Result<NodeEndpoints>
