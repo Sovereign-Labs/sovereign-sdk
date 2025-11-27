3b0bd213d18223f3955a34ff761f20c83e437353
Theodore Bugnet <theodore@sovlabs.io>
2025-11-27 15:39:00 +0000

    Bump sequencer queue timeout, sendrawtransaction_sync timeout, and add a debug log to all ethereum transactions ingested

--- crates/full-node/full-node-configs/src/sequencer.rs
+++ crates/full-node/full-node-configs/src/sequencer.rs
@@ -207,7 +207,7 @@ pub const fn default_maximum_future_nonce_delta() -> u64 {
 }
 
 pub const fn default_future_nonce_transaction_timeout_millis() -> u64 {
-    2_000
+    20_000
 }
 
 pub const fn default_num_cache_warmup_workers() -> usize {
