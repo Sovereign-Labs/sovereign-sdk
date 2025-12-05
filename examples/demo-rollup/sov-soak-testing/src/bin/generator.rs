e8c156af1bbda5b555239695e24c0e0adb7d4867
Theodore Bugnet <theodore@sovlabs.io>
2025-12-05 15:54:51 +0000

    Add stats tracker for soak test workers rebased on dev

--- examples/demo-rollup/sov-soak-testing/src/bin/generator.rs
+++ examples/demo-rollup/sov-soak-testing/src/bin/generator.rs
@@ -5,8 +5,8 @@ use sov_bank::Bank;
 use sov_modules_api::prelude::tracing;
 use sov_modules_api::{EncodeCall, Runtime, Spec};
 use sov_soak_testing::{
-    CelestiaRollupSpec, DemoCelestiaRT, DemoMockRT, MockDemoRollupSpec, SoakTestRunner, TestRT,
-    ValidityProfile,
+    spawn_throughput_watcher, CelestiaRollupSpec, DemoCelestiaRT, DemoMockRT, MockDemoRollupSpec,
+    SoakTestRunner, TestRT, ValidityProfile, WorkerStats,
 };
 use sov_synthetic_load::SyntheticLoad;
 use sov_test_utils::TestSpec;
@@ -44,7 +44,7 @@ struct Args {
     /// transactions don't overlap with the previous run.
     salt: u32,
 
-    #[arg(short, long, default_value = "buzzy")]
+    #[arg(short, long, default_value = "clean")]
     /// The distribution of valid/invalid transactions to generate.
     validity_profile: ValidityProfile,
 
@@ -72,6 +72,8 @@ async fn run_soak_test_with_demo_runtime<R, S>(
     num_workers: u32,
     validity: Distribution<MessageValidity>,
     tx_type: TxType,
+    restart_after: Option<std::time::Duration>,
+    stats_tx: tokio::sync::mpsc::UnboundedSender<WorkerStats>,
 ) -> anyhow::Result<()>
 where
     R: Runtime<S> + EncodeCall<Bank<S>> + EncodeCall<SyntheticLoad<S>> + Clone,
@@ -86,7 +88,15 @@ where
     };
 
     runner
-        .run(client, rx, worker_id, num_workers, validity)
+        .run(
+            client,
+            rx,
+            worker_id,
+            num_workers,
+            validity,
+            restart_after,
+            stats_tx,
+        )
         .await
 }
 
@@ -98,6 +108,8 @@ async fn worker_task(
     runtime: SelectedRuntime,
     validity_profile: ValidityProfile,
     tx_type: TxType,
+    restart_after: Option<std::time::Duration>,
+    stats_tx: tokio::sync::mpsc::UnboundedSender<WorkerStats>,
 ) -> anyhow::Result<()> {
     let validity = validity_profile.get_validity();
 
@@ -110,6 +122,8 @@ async fn worker_task(
                 num_workers,
                 validity,
                 tx_type,
+                restart_after,
+                stats_tx,
             )
             .await
         }
@@ -121,6 +135,8 @@ async fn worker_task(
                 num_workers,
                 validity,
                 tx_type,
+                restart_after,
+                stats_tx,
             )
             .await
         }
@@ -132,6 +148,8 @@ async fn worker_task(
                 num_workers,
                 validity,
                 tx_type,
+                restart_after,
+                stats_tx,
             )
             .await
         }
@@ -157,7 +175,16 @@ async fn main() -> Result<(), anyhow::Error> {
         .build()?;
     let client = sov_api_spec::Client::new_with_client(&args.api_url, reqwest_client);
 
+    let restart_after = args
+        .restart_after_seconds
+        .map(std::time::Duration::from_secs);
+
+    // Create stats channels for each worker
+    let mut stats_receivers = Vec::new();
     for i in 0..args.num_workers {
+        let (stats_tx, stats_rx) = tokio::sync::mpsc::unbounded_channel();
+        stats_receivers.push(stats_rx);
+
         worker_set.spawn(worker_task(
             client.clone(),
             rx.clone(),
@@ -166,8 +193,13 @@ async fn main() -> Result<(), anyhow::Error> {
             args.runtime,
             args.validity_profile,
             args.tx_type,
+            restart_after,
+            stats_tx,
         ));
     }
+
+    // Spawn the throughput watcher task
+    let watcher_handle = tokio::spawn(spawn_throughput_watcher(stats_receivers, 5));
 
     let mut terminate = tokio::signal::unix::signal(SignalKind::terminate())
         .expect("Failed to set up SIGTERM handler");
@@ -181,6 +213,9 @@ async fn main() -> Result<(), anyhow::Error> {
 
     tx.send(true)?;
     _ = worker_set.join_all();
+
+    // Wait for watcher to finish processing remaining stats
+    _ = watcher_handle.await;
 
     Ok(())
 }
