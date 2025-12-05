8c28462303e901108dd0e1464e8a0d1c330f6055
Nikolai Golub <nikolai@sovlabs.io>
2025-12-05 15:01:14 +0100

    Stick to std::fmt::Debug only in `MaybeRetryable`

--- crates/rollup-interface/src/node/da.rs
+++ crates/rollup-interface/src/node/da.rs
@@ -69,7 +69,7 @@ pub enum MaybeRetryable<E> {
     Transient(E),
 }
 
-impl<E: std::fmt::Display> MaybeRetryable<E> {
+impl<E: std::fmt::Debug> MaybeRetryable<E> {
     fn is_retryable(&self) -> bool {
         matches!(self, Self::Transient(_))
     }
@@ -259,7 +259,7 @@ pub async fn run_maybe_retryable_async_fn_with_retries<F, Fut, T, E>(
 where
     F: Fn() -> Fut,
     Fut: std::future::Future<Output = Result<T, MaybeRetryable<E>>>,
-    E: std::fmt::Display + std::fmt::Debug,
+    E: std::fmt::Debug,
 {
     fxn.retry(backoff_policy)
         .notify(|err: &MaybeRetryable<E>, dur: Duration| {
