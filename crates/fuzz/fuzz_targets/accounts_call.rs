4ea35611fe832dd129bb4685638befbc651a953c
Leonid Logvinov <logvinov.leon@gmail.com>
2025-12-11 15:27:20 +0100

    Pass None to Context in some test

--- crates/fuzz/fuzz_targets/accounts_call.rs
+++ crates/fuzz/fuzz_targets/accounts_call.rs
@@ -87,7 +87,8 @@ fuzz_target!(
         for _ in 0..iterations {
             // we use slices for better select performance
             let sender = addresses.choose(rng).unwrap();
-            let context = Context::<S>::new(*sender, Default::default(), sequencer, sequencer_da);
+            let context =
+                Context::<S>::new(*sender, Default::default(), sequencer, sequencer_da, None);
 
             // clear previous state
             let previous = state.get(sender).unwrap().as_hex();
