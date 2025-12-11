a082f94892e1e7c976c807e0b706663c2e22ce75
Leonid Logvinov <logvinov.leon@gmail.com>
2025-12-11 12:44:49 +0100

    Implement SequencingDataTester::call

--- crates/module-system/module-implementations/sov-test-modules/src/sequencing_data.rs
+++ crates/module-system/module-implementations/sov-test-modules/src/sequencing_data.rs
@@ -1,5 +1,5 @@
-use borsh::{BorshDeserialize, BorshSerialize};
-use sov_modules_api::macros::UniversalWallet;
+use anyhow::{bail, ensure, Context as _};
+use chrono::{TimeZone, Utc};
 use sov_modules_api::{Context, DaSpec, GenesisState, Module, ModuleId, ModuleInfo, Spec, TxState};
 
 #[derive(Clone, ModuleInfo)]
@@ -10,27 +10,11 @@ pub struct SequencingDataTester<S: Spec> {
     _phantom: std::marker::PhantomData<S>,
 }
 
-#[derive(
-    Clone,
-    BorshSerialize,
-    BorshDeserialize,
-    PartialEq,
-    Eq,
-    Debug,
-    Hash,
-    schemars::JsonSchema,
-    UniversalWallet,
-)]
-pub enum CallMessage {
-    /// Captures the sequencing data from context and checks it's correctness
-    Main,
-}
-
 impl<S: Spec> Module for SequencingDataTester<S> {
     type Spec = S;
 
     type Config = ();
-    type CallMessage = CallMessage;
+    type CallMessage = ();
     type Event = ();
     type Error = anyhow::Error;
 
@@ -46,9 +30,35 @@ impl<S: Spec> Module for SequencingDataTester<S> {
     fn call(
         &mut self,
         _msg: Self::CallMessage,
-        _context: &Context<Self::Spec>,
+        context: &Context<Self::Spec>,
         _state: &mut impl TxState<S>,
     ) -> Result<(), Self::Error> {
+        let data = context
+            .sequencing_data()
+            .as_ref()
+            .context("No sequencing data in context")?;
+        let timestamp = parse_timestamp(data)?;
+        let reasonable_range = year_to_timestamp(2025)..year_to_timestamp(2100);
+        ensure!(
+            reasonable_range.contains(&timestamp),
+            "Timestamp {} is outside reasonable range {:?}",
+            timestamp,
+            reasonable_range
+        );
         Ok(())
     }
+}
+
+fn parse_timestamp(data: &[u8]) -> anyhow::Result<u128> {
+    let Ok(bytes) = data.try_into() else {
+        bail!("Failed to convert to [u8; 16]");
+    };
+    Ok(u128::from_le_bytes(bytes))
+}
+
+fn year_to_timestamp(year: i32) -> u128 {
+    Utc.with_ymd_and_hms(year, 1, 1, 0, 0, 0)
+        .unwrap()
+        .timestamp_nanos_opt()
+        .unwrap() as u128
 }
