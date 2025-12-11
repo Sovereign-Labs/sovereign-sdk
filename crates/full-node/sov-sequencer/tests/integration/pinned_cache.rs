4297b875f84cf693a73006286f166e89c1b09d10
Nikolai Golub <nikolai@sovlabs.io>
2025-12-11 13:15:10 +0100

    Fix lint

--- crates/full-node/sov-sequencer/tests/integration/pinned_cache.rs
+++ crates/full-node/sov-sequencer/tests/integration/pinned_cache.rs
@@ -19,7 +19,6 @@ use sov_state::DefaultStorageSpec;
 use sov_test_modules::pinned_cache::CallMessage as PinnedCacheCallMessage;
 use sov_test_modules::pinned_cache::PinnedCacheTester;
 use sov_test_modules::pinned_cache::ValueRange;
-use sov_test_utils::{default_test_signed_transaction, TEST_DEFAULT_MOCK_DA_BLOCK_TIME_MS};
 use sov_test_utils::generate_optimistic_runtime_with_kernel;
 use sov_test_utils::runtime::genesis::optimistic::HighLevelOptimisticGenesisConfig;
 use sov_test_utils::runtime::GenesisParams;
@@ -31,6 +30,7 @@ use sov_test_utils::MockDaSpec;
 use sov_test_utils::RtAgnosticBlueprint;
 use sov_test_utils::TestNomtSpec as TestSpec;
 use sov_test_utils::TestUser;
+use sov_test_utils::{default_test_signed_transaction, TEST_DEFAULT_MOCK_DA_BLOCK_TIME_MS};
 use tokio_stream::StreamExt;
 
 type TestNomtBlueprint = RtAgnosticBlueprint<
@@ -376,7 +376,8 @@ async fn test_pinning_after_recovery() {
     test_rollup.tenderly_produce_blocks(1).await.unwrap();
     let max_wait = 1000;
     let mut i = 0;
-    let reasonable_time_for_rollup = std::time::Duration::from_millis(TEST_DEFAULT_MOCK_DA_BLOCK_TIME_MS);
+    let reasonable_time_for_rollup =
+        std::time::Duration::from_millis(TEST_DEFAULT_MOCK_DA_BLOCK_TIME_MS);
     while test_rollup.is_sequencer_ready().await {
         // For some reason DA subscriptions are still broken at this point; if we use produce_and_wait_for_n_slots, the test will hang.
         // This is unrelated to the feature under test, so I've left it for now. Anyone reading this should feel free to change the test.
