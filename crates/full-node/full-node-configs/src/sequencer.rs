8566871b31b5d88b51dba6d972be2551727e3dad
Theodore Bugnet <theodore@sovlabs.io>
2025-12-12 14:08:47 +0000

    Fix spurious config changes

--- crates/full-node/full-node-configs/src/sequencer.rs
+++ crates/full-node/full-node-configs/src/sequencer.rs
@@ -216,7 +216,7 @@ pub const fn default_maximum_future_nonce_delta() -> u64 {
 }
 
 pub const fn default_future_nonce_transaction_timeout_millis() -> u64 {
-    20_000
+    2000
 }
 
 pub const fn default_num_cache_warmup_workers() -> usize {
