075941256156e14881f01d1496d5e3f3c2bddb63
Leonid Logvinov <logvinov.leon@gmail.com>
2025-12-15 13:44:20 +0100

    Run cargo fmt

--- crates/full-node/sov-db/src/schema/types.rs
+++ crates/full-node/sov-db/src/schema/types.rs
@@ -173,9 +173,11 @@ pub fn split_tx_for_storage<T: TxReceiptContents>(
     let tx_for_storage = StoredTransaction {
         hash: tx.tx_hash.into(),
         events: event_range,
-        body: tx
-            .body_to_save
-            .map(|ftx| borsh::to_vec(&ftx).expect("Serialization to vec is infallible").into()),
+        body: tx.body_to_save.map(|ftx| {
+            borsh::to_vec(&ftx)
+                .expect("Serialization to vec is infallible")
+                .into()
+        }),
         receipt: DbBytes::new(
             bincode::serialize(&tx.receipt).expect("Serialization to vec is infallible"),
         ),
