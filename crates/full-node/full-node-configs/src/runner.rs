cb73fe2775761d71e794ab8ed74e2b13be0315a8
bkolad <blazejkolad@gmail.com>
2025-12-16 19:36:16 +0100

    fix

--- crates/full-node/full-node-configs/src/runner.rs
+++ crates/full-node/full-node-configs/src/runner.rs
@@ -325,7 +325,7 @@ mod tests {
             [sequencer.preferred.rate_limiter]
             address_custom_limits = []
             max_requests_per_second = 1000000
-            ip_custom_limits = [["157.180.14.244", { batch_execution_time_limit_millis = 200, refill_rate = 2}]]
+            ip_custom_limits = [["157.180.14.244", { resources_per_bucket = 10, batch_execution_time_limit_millis = 200, refill_rate = 2}]]
             [sequencer.preferred.rate_limiter.default_limits]
             resources_per_bucket = 5
             refill_rate = 2
