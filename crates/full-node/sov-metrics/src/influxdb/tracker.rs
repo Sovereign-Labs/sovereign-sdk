35109a149543e23d473b50d6fc44e79b2d5f456e
bkolad <blazejkolad@gmail.com>
2025-12-18 09:34:24 +0100

    renaming

--- crates/full-node/sov-metrics/src/influxdb/tracker.rs
+++ crates/full-node/sov-metrics/src/influxdb/tracker.rs
@@ -829,10 +829,10 @@ impl Metric for RateLimiterMetrics {
     fn serialize_for_telegraf(&self, buffer: &mut Vec<u8>) -> std::io::Result<()> {
         write!(
             buffer,
-            "{},limiter_type={:?} value={}",
+            "{},limiter_type={:?} count={}",
             self.measurement_name(),
             self.limiter_type,
-            self.value,
+            self.count,
         )?;
 
         Ok(())
