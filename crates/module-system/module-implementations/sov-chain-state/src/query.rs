bd0fc571815f08b2d1b5f2c65c4559e03f66596b
Preston Evans <preston.b.evans@gmail.com>
2025-12-31 02:49:17 +0100

    Add endpoint to detect setup mode

--- crates/module-system/module-implementations/sov-chain-state/src/query.rs
+++ crates/module-system/module-implementations/sov-chain-state/src/query.rs
@@ -1,6 +1,15 @@
-#[cfg(feature = "native")]
-use sov_modules_api::prelude::UnwrapInfallible;
 use sov_modules_api::Spec;
+#[cfg(feature = "native")]
+use sov_modules_api::{
+    prelude::axum::{self, routing::get},
+    rest::HasCustomRestApi,
+};
+#[cfg(feature = "native")]
+use sov_modules_api::{
+    prelude::UnwrapInfallible,
+    rest::{utils::ApiResult, ApiState},
+    ApiStateAccessor,
+};
 #[cfg(feature = "native")]
 use sov_rollup_interface::common::VisibleSlotNumber;
 
@@ -18,5 +27,29 @@ impl<S: Spec> ChainState<S> {
             .get(accessor)
             .unwrap_infallible()
             .expect("The visible rollup height should always be set")
+    }
+
+    #[cfg(feature = "native")]
+    async fn is_in_setup_mode_handler(
+        state: ApiState<S, Self>,
+        mut accessor: ApiStateAccessor<S>,
+    ) -> ApiResult<bool> {
+        use sov_modules_api::VersionReader;
+
+        Ok(state
+            .is_setup_mode_active(accessor.rollup_height_to_access(), &mut accessor)
+            .unwrap_infallible()
+            .into())
+    }
+}
+
+#[cfg(feature = "native")]
+impl<S: Spec> HasCustomRestApi for ChainState<S> {
+    type Spec = S;
+
+    fn custom_rest_api(&self, state: ApiState<S>) -> axum::Router<()> {
+        axum::Router::new()
+            .route("/setup-mode-is-active", get(Self::is_in_setup_mode_handler))
+            .with_state(state.with(self.clone()))
     }
 }
