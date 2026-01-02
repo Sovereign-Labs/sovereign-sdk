9cf4290ada3f46beb5c7e24e07d1d510ab9b467e
Preston Evans <preston.b.evans@gmail.com>
2026-01-02 23:25:58 +0100

    Clippy

--- crates/module-system/module-implementations/sov-evm/src/authenticate.rs
+++ crates/module-system/module-implementations/sov-evm/src/authenticate.rs
@@ -182,7 +182,7 @@ pub fn decode_evm_tx(raw_tx: &[u8]) -> Result<(RlpEvmTransaction, TransactionSig
     }
 
     let type_tag = TransactionSigned::extract_type_byte(&mut &tx_data.rlp[..]).unwrap_or(0); // Reject as a legacy transaction by default
-    if !(type_tag == EIP1559_TX_TYPE_ID) {
+    if type_tag != EIP1559_TX_TYPE_ID {
         return Err(FatalError::DeserializationFailed(
             "Invalid transaction type: Only EIP1559 is currently supported. If you need to use EIP7702, please reach out to the SDK developers for support.".to_string(),
         ));
