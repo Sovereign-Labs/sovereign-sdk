3f02546781842641652cad17cadfc45b36b21f62
Preston Evans <preston.b.evans@gmail.com>
2026-01-02 23:18:07 +0100

    Add memory limit to cfg test

--- crates/module-system/module-implementations/sov-evm/src/evm/executor.rs
+++ crates/module-system/module-implementations/sov-evm/src/evm/executor.rs
@@ -133,6 +133,7 @@ mod tests {
         expected_cfg_env.disable_base_fee = true;
         expected_cfg_env.limit_contract_code_size = Some(100);
         expected_cfg_env.spec = SpecId::CANCUN;
+        expected_cfg_env.memory_limit = 50 * 1024 * 1024; // 50MB
 
         assert_eq!(expected_cfg_env, cfg_env);
     }
