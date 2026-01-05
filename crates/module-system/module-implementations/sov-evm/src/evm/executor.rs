c7b2290941423032b1b1bfb0e1d9683bfbc8a2f1
Preston Evans <32944016+preston-evans98@users.noreply.github.com>
2026-01-05 13:41:23 -0600

    Set memory limit on EVM as fallback if gas limit is raised (#2293)
    * Set memory limit on EVM as fallback if gas limit is raised
    
    * Add memory limit to cfg test
    
    * Fix merge conflict

--- crates/module-system/module-implementations/sov-evm/src/evm/executor.rs
+++ crates/module-system/module-implementations/sov-evm/src/evm/executor.rs
@@ -30,6 +30,7 @@ pub(crate) fn get_cfg_env(
 ) -> CfgEnv {
     let mut cfg_env = template_cfg.unwrap_or_default();
     cfg_env.chain_id = config_value!("CHAIN_ID");
+    cfg_env.memory_limit = 50 * 1024 * 1024; // 50MB
     cfg_env.limit_contract_code_size = Some(
         cfg.chain_spec
             .limit_contract_code_size
@@ -160,6 +161,7 @@ mod tests {
         expected_cfg_env.disable_base_fee = true;
         expected_cfg_env.limit_contract_code_size = Some(100);
         expected_cfg_env.spec = SpecId::CANCUN;
+        expected_cfg_env.memory_limit = 50 * 1024 * 1024; // 50MB
 
         assert_eq!(expected_cfg_env, cfg_env);
     }
