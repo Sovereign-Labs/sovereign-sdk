11d092fc8c3d08687b513889db8bc2af330734eb
bkolad <blazejkolad@gmail.com>
2026-01-13 18:56:27 +0100

    fix

--- examples/demo-rollup/tests/replica/start_stop.rs
+++ examples/demo-rollup/tests/replica/start_stop.rs
@@ -59,7 +59,7 @@ async fn test_replica_start_stop() {
         .await;
 
         wait_for_all_events_with_timeout(
-            Duration::from_millis(100),
+            Duration::from_millis(300),
             nb_of_txs,
             &mut event_subscription,
         )
