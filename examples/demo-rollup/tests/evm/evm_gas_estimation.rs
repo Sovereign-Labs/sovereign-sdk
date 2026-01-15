67837ec3843ce9e55ed85af658332e6a0804a78c
bkolad <blazejkolad@gmail.com>
2026-01-15 16:07:37 +0100

    fix tests

--- examples/demo-rollup/tests/evm/evm_gas_estimation.rs
+++ examples/demo-rollup/tests/evm/evm_gas_estimation.rs
@@ -6,26 +6,6 @@ use alloy_primitives::{Address, U256};
 use sov_evm_test_utils::SimpleStorage;
 
 #[tokio::test(flavor = "multi_thread")]
-async fn simple_transfer() -> anyhow::Result<()> {
-    let (_, test_client, _) = setup_with_simple_storage(0, EVM_EXTENSION).await;
-
-    let simple_transfer = test_client.make_tx(Some(Address::ZERO), None);
-    let gas_estimation = test_client.eth_estimate_gas(simple_transfer).await;
-    assert_eq!(gas_estimation, (14_810 / 2) * 3 + 100_000);
-    Ok(())
-}
-
-#[tokio::test(flavor = "multi_thread")]
-async fn contract_deploy() -> anyhow::Result<()> {
-    let (_, test_client, _) = setup_with_simple_storage(0, EVM_EXTENSION).await;
-
-    let deploy_tx = test_client.make_tx(None, Some(test_client.contract.byte_code()));
-    let gas_estimation = test_client.eth_estimate_gas(deploy_tx).await;
-    assert_eq!(gas_estimation, (248_136 / 2) * 3 + 100_000);
-    Ok(())
-}
-
-#[tokio::test(flavor = "multi_thread")]
 async fn big_accessory_state_writes() -> anyhow::Result<()> {
     let rollup = setup_test_rollup(0, EVM_EXTENSION).await;
     rollup.wait_for_next_blocks(1).await;
