7ca474dd2a1e217d75f2fe25a198295b42b78d22
bkolad <blazejkolad@gmail.com>
2026-01-16 14:59:56 +0100

    refactor

--- examples/demo-rollup/tests/replica/db_elected.rs
+++ examples/demo-rollup/tests/replica/db_elected.rs
@@ -3,52 +3,83 @@ use super::*;
 use sov_sequencer::SequencerRole;
 use tokio::time::Duration;
 
+type Rollup = ExternalMockDemoRollup<Native>;
+
+/// Test setup for DbElected tests with two nodes (leader and replica).
+struct DbElectedTestSetup {
+    leader: TestRollup<Rollup>,
+    replica: TestRollup<Rollup>,
+    da_shutdown: watch::Sender<()>,
+}
+
+impl DbElectedTestSetup {
+    /// Creates a new test setup with two DbElected nodes.
+    /// Returns None if Docker is not supported.
+    async fn new() -> Option<Self> {
+        let postgres = match PostgresData::create_postgres().await {
+            Ok(pg) => Some(pg),
+            Err(CreatePostgresError::DockerNotSupported) => return None,
+            Err(CreatePostgresError::DockerError(e)) => {
+                panic!("Failed to create Postgres container: {e}");
+            }
+        };
+
+        let (_, da_shutdown, addr) = create_da_service_periodic().await;
+
+        // Start both DbElected nodes
+        let node1 = postgres
+            .clone()
+            .map(|pg| (pg, "node1".into(), NodeRole::DbElected));
+        let rollup1 = start_rollup(addr, node1).await;
+
+        let node2 = postgres.map(|pg| (pg, "node2".into(), NodeRole::DbElected));
+        let rollup2 = start_rollup(addr, node2).await;
+
+        // Wait for both nodes to be ready
+        rollup1.wait_for_sequencer_ready().await.unwrap();
+        rollup2.wait_for_sequencer_ready().await.unwrap();
+
+        // Discover roles via the /sequencer/role endpoint
+        let role1 = rollup1.sequencer_role().await.unwrap();
+        let role2 = rollup2.sequencer_role().await.unwrap();
+
+        // Determine which node is the leader and which is the replica
+        let (leader, replica) = match (role1, role2) {
+            (SequencerRole::Leader, SequencerRole::Replica) => (rollup1, rollup2),
+            (SequencerRole::Replica, SequencerRole::Leader) => (rollup2, rollup1),
+            _ => panic!(
+                "Expected one Leader and one Replica, got {:?} and {:?}",
+                role1, role2
+            ),
+        };
+
+        Some(Self {
+            leader,
+            replica,
+            da_shutdown,
+        })
+    }
+
+    async fn shutdown(self) {
+        let _ = self.leader.shutdown().await;
+        let _ = self.replica.shutdown().await;
+        let _ = self.da_shutdown.send(());
+    }
+}
+
 /// Test that when two DbElected nodes start, one becomes leader and the other becomes replica.
 /// The leader can process transactions while the replica receives them via PostgreSQL sync.
 #[tokio::test(flavor = "multi_thread")]
 async fn test_db_elected_two_nodes_leader_and_replica() {
-    let postgres = PostgresData::create_postgres().await;
-
-    let postgres = match postgres {
-        Ok(pg) => Some(pg),
-        Err(CreatePostgresError::DockerNotSupported) => return,
-        Err(CreatePostgresError::DockerError(e)) => {
-            panic!("Failed to create Postgres container: {e}");
-        }
+    let Some(setup) = DbElectedTestSetup::new().await else {
+        return;
     };
 
     let key_and_address = read_private_key::<S>("tx_signer_private_key.json");
-    let (_, da_shutdown, addr) = create_da_service_periodic().await;
-
-    // Start both DbElected nodes
-    let node1 = postgres
-        .clone()
-        .map(|pg| (pg, "node1".into(), NodeRole::DbElected));
-    let rollup1 = start_rollup(addr, node1).await;
-
-    let node2 = postgres.map(|pg| (pg, "node2".into(), NodeRole::DbElected));
-    let rollup2 = start_rollup(addr, node2).await;
-
-    // Wait for both nodes to be ready
-    rollup1.wait_for_sequencer_ready().await.unwrap();
-    rollup2.wait_for_sequencer_ready().await.unwrap();
-
-    // Discover roles via the /sequencer/role endpoint
-    let role1 = rollup1.sequencer_role().await.unwrap();
-    let role2 = rollup2.sequencer_role().await.unwrap();
-
-    // Determine which node is the leader and which is the replica
-    let (leader_rollup, replica_rollup) = match (role1, role2) {
-        (SequencerRole::Leader, SequencerRole::Replica) => (&rollup1, &rollup2),
-        (SequencerRole::Replica, SequencerRole::Leader) => (&rollup2, &rollup1),
-        _ => panic!(
-            "Expected one Leader and one Replica, got {:?} and {:?}",
-            role1, role2
-        ),
-    };
 
     // Subscribe to events on the replica
-    let mut event_subscription = replica_rollup
+    let mut event_subscription = setup
+        .replica
         .api_client()
         .subscribe_to_events_with_filter("Bank/*")
         .await
@@ -66,65 +97,34 @@ async fn test_db_elected_two_nodes_leader_and_replica() {
         0,
     );
 
-    leader_rollup.send_tx_to_sequencer(&tx).await.unwrap();
+    setup.leader.send_tx_to_sequencer(&tx).await.unwrap();
 
     // Wait for the event on the replica (proves it received the tx via PostgreSQL sync)
     wait_for_all_events_with_timeout(Duration::from_millis(500), 1, &mut event_subscription).await;
 
-    let _ = rollup1.shutdown().await;
-    let _ = rollup2.shutdown().await;
-    let _ = da_shutdown.send(());
+    setup.shutdown().await;
 }
 
 /// Test that when the leader dies, the replica acquires leadership and becomes the new leader.
 #[tokio::test(flavor = "multi_thread")]
 async fn test_db_elected_leader_failover() {
-    let postgres = PostgresData::create_postgres().await;
-
-    let postgres = match postgres {
-        Ok(pg) => Some(pg),
-        Err(CreatePostgresError::DockerNotSupported) => return,
-        Err(CreatePostgresError::DockerError(e)) => {
-            panic!("Failed to create Postgres container: {e}");
-        }
+    let Some(setup) = DbElectedTestSetup::new().await else {
+        return;
     };
 
-    let (_, da_shutdown, addr) = create_da_service_periodic().await;
-
-    // Start both DbElected nodes
-    let node1 = postgres
-        .clone()
-        .map(|pg| (pg, "node1".into(), NodeRole::DbElected));
-    let rollup1 = start_rollup(addr, node1).await;
-
-    let node2 = postgres.map(|pg| (pg, "node2".into(), NodeRole::DbElected));
-    let rollup2 = start_rollup(addr, node2).await;
-
-    // Wait for both nodes to be ready
-    rollup1.wait_for_sequencer_ready().await.unwrap();
-    rollup2.wait_for_sequencer_ready().await.unwrap();
-
-    // Discover roles via the /sequencer/role endpoint
-    let role1 = rollup1.sequencer_role().await.unwrap();
-    let role2 = rollup2.sequencer_role().await.unwrap();
-
-    // Determine which node is the leader and which is the replica
-    let (leader_rollup, replica_rollup) = match (role1, role2) {
-        (SequencerRole::Leader, SequencerRole::Replica) => (rollup1, rollup2),
-        (SequencerRole::Replica, SequencerRole::Leader) => (rollup2, rollup1),
-        _ => panic!(
-            "Expected one Leader and one Replica, got {:?} and {:?}",
-            role1, role2
-        ),
-    };
+    let DbElectedTestSetup {
+        leader,
+        replica,
+        da_shutdown,
+    } = setup;
 
     // Kill the leader
-    let _ = leader_rollup.shutdown().await;
+    let _ = leader.shutdown().await;
 
     // Wait for replica to shutdown (it will acquire leadership and call exit_rollup)
     let timeout_duration = Duration::from_secs(5);
     let start = std::time::Instant::now();
-    while !replica_rollup.is_rollup_crashed() {
+    while !replica.is_rollup_crashed() {
         if start.elapsed() > timeout_duration {
             panic!("Timeout waiting for replica to acquire leadership and shutdown");
         }
@@ -132,7 +132,7 @@ async fn test_db_elected_leader_failover() {
     }
 
     // Get the builder from the replica (task already finished due to exit_rollup)
-    let replica_builder = replica_rollup.shutdown().await.unwrap();
+    let replica_builder = replica.shutdown().await.unwrap();
 
     // Restart the former replica
     let restarted_rollup = replica_builder.start_test_rollup().await.unwrap();
