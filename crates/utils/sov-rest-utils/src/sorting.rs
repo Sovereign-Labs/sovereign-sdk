1607b7f54740750d686c15c5a8e28ff4db184785
majtte <139552589+majtte@users.noreply.github.com>
2026-01-19 15:17:18 +0100

    Update sorting.rs

--- crates/utils/sov-rest-utils/src/sorting.rs
+++ crates/utils/sov-rest-utils/src/sorting.rs
@@ -123,7 +123,6 @@ mod tests {
     fn ok_cases() {
         try_deserialize(&[("sort", "+100")]).unwrap();
         try_deserialize(&[("sort", "-100")]).unwrap();
-        try_deserialize(&[("sort", "+100")]).unwrap();
         try_deserialize(&[("sort", "--100")]).unwrap();
         try_deserialize(&[("sort", "+0")]).unwrap();
     }
