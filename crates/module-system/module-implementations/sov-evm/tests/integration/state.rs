e753a7e70afcd868d0c525f9bb6dc53ca21d3073
Preston Evans <preston.b.evans@gmail.com>
2026-01-24 21:52:07 +0100

    Fmt

--- crates/module-system/module-implementations/sov-evm/tests/integration/state.rs
+++ crates/module-system/module-implementations/sov-evm/tests/integration/state.rs
@@ -52,7 +52,11 @@ fn test_block_updates() {
             let txs = current_block.transactions();
             assert_eq!(txs.start, 0);
             assert_eq!(txs.end, 1);
-            let block_height = evm.get_block_by_hash(current_block.header().hash(), None, state).unwrap().unwrap().number();
+            let block_height = evm
+                .get_block_by_hash(current_block.header().hash(), None, state)
+                .unwrap()
+                .unwrap()
+                .number();
             assert_eq!(block_height, 1);
         }),
     });
