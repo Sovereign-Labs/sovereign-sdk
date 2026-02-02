2dca4f6a5cb2bf639abd70c53c5e6ca9f7f16621
Nikolai Golub <nikolai@sovlabs.io>
2026-02-02 11:50:02 +0100

    Addig missed files

---
+++ examples/demo-rollup/provers/risc0/guest-mock-nomt/src/bin/mock_da_nomt.rs
@@ -0,0 +1,38 @@
+#![no_main]
+use demo_stf::runtime::Runtime;
+use demo_stf::StfVerifier;
+use sov_address::MultiAddressEvm;
+use sov_mock_da::{MockDaSpec, MockDaVerifier};
+pub use sov_mock_zkvm::MockZkvm;
+use sov_modules_api::configurable_spec::ConfigurableSpec;
+use sov_modules_api::execution_mode::Zk;
+use sov_modules_stf_blueprint::StfBlueprint;
+use sov_risc0_adapter::guest::Risc0Guest;
+use sov_risc0_adapter::Risc0;
+use sov_state::nomt::zk_storage::NomtVerifierStorage;
+use sov_state::DefaultStorageSpec;
+
+type NomtStorage = NomtVerifierStorage<DefaultStorageSpec<sha2::Sha256>>;
+
+risc0_zkvm::guest::entry!(main);
+
+#[cfg_attr(feature = "bench", sov_modules_api::cycle_tracker)]
+fn cycles_per_block() {
+    let guest = Risc0Guest::new();
+    let storage = NomtStorage::new();
+
+    let stf: StfBlueprint<
+        ConfigurableSpec<MockDaSpec, Risc0, MockZkvm, MultiAddressEvm, Zk, sov_risc0_adapter::Risc0CryptoSpec, NomtStorage>,
+        Runtime<_>,
+    > = StfBlueprint::new();
+
+    let stf_verifier = StfVerifier::<_, _, _, _, _>::new(stf, MockDaVerifier {});
+
+    stf_verifier
+        .run_block(guest, storage)
+        .expect("Prover must be honest");
+}
+
+pub fn main() {
+    cycles_per_block();
+}
