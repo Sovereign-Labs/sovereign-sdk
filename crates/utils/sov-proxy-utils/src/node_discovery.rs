195b289f480b1ffc2c547b6e550ac0e702e57277
bkolad <blazejkolad@gmail.com>
2026-02-12 19:57:33 +0100

    Make notifier mutable

--- crates/utils/sov-proxy-utils/src/node_discovery.rs
+++ crates/utils/sov-proxy-utils/src/node_discovery.rs
@@ -101,7 +101,7 @@ impl NodeInfo {
 #[async_trait]
 pub trait ClusterUpdateNotifier: Send + Sync + 'static {
     /// Called when cluster membership or leadership changes.
-    async fn on_cluster_update(&self, cluster_info: &ClusterInfo);
+    async fn on_cluster_update(&mut self, cluster_info: &ClusterInfo);
 }
 
 /// Handle returned when subscribing to cluster updates.
@@ -285,7 +285,7 @@ impl NodeDiscovery {
         self.prev_leader_id = leader_id;
 
         // Notify watchers that the cluster was updated.
-        if let Some(notifier) = &self.notifier {
+        if let Some(notifier) = &mut self.notifier {
             notifier.on_cluster_update(&info).await;
         }
         let _ = self.sender.send(info);
