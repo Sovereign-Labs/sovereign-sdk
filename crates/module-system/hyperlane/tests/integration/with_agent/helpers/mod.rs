b403ede6b4b707e1e118ef922b9aaf2aded8e999
ross-weir <29697678+ross-weir@users.noreply.github.com>
2026-02-14 00:52:18 +1100

    update comment

--- crates/module-system/hyperlane/tests/integration/with_agent/helpers/mod.rs
+++ crates/module-system/hyperlane/tests/integration/with_agent/helpers/mod.rs
@@ -214,7 +214,7 @@ impl HyperlaneBuilder {
         let docker_image = env::var("CUSTOM_HLP_DOCKER_IMAGE");
         let has_custom_image = !matches!(docker_image, Err(env::VarError::NotPresent));
 
-        // Current image is based on https://github.com/Sovereign-Labs/hyperlane-monorepo/tree/integration-2025-09-17-rebase branch
+        // Current image is based on https://github.com/Sovereign-Labs/hyperlane-monorepo/tree/sovereign-lander-integration
         let docker_image = docker_image
             .unwrap_or_else(|_| "ghcr.io/ross-weir/hyperlane-agent:integration-lander-1".into());
         let (name, tag) = docker_image
