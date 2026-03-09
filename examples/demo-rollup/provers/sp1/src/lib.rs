dc948bc818d6f18bc5bb40efa159b513d9d7e12a
bkolad <blazejkolad@gmail.com>
2026-03-09 18:31:18 +0100

    fix

--- examples/demo-rollup/provers/sp1/src/lib.rs
+++ examples/demo-rollup/provers/sp1/src/lib.rs
@@ -15,7 +15,7 @@ fn load_elf(path: &str) -> &'static [u8] {
 // but because we don't include the guest ELFs in the GitHub build, they may potentially not exist.
 lazy_static! {
     pub static ref SP1_GUEST_MOCK_ELF: &'static [u8] = load_elf(&format!(
-        "{}/guest-mock/elf/riscv64im-succinct-zkvm-elf",
+        "{}/guest-mock/target/elf-compilation/riscv64im-succinct-zkvm-elf/release/sov-demo-prover-guest-mock-sp1",
         env!("CARGO_MANIFEST_DIR")
     ));
     pub static ref SP1_GUEST_CELESTIA_ELF: &'static [u8] = load_elf(&format!(
