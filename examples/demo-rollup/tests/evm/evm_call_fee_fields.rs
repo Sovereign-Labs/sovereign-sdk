331d1a3a4a51ac8052a65617abd0179171244215
Nikolai Golub <nikolai@sovlabs.io>
2026-03-13 12:12:15 +0100

    Manually update conflicting test
    # Conflicts:
    #	examples/demo-rollup/tests/evm/evm_call_fee_fields.rs

--- examples/demo-rollup/tests/evm/evm_call_fee_fields.rs
+++ examples/demo-rollup/tests/evm/evm_call_fee_fields.rs
@@ -223,20 +223,18 @@ async fn eth_estimate_gas_rejects_unfunded_caller_with_max_fee_per_gas() -> anyh
 }
 
 #[tokio::test(flavor = "multi_thread")]
-async fn eth_call_accepts_below_base_fee_with_max_fee_per_gas() -> anyhow::Result<()> {
+async fn eth_call_rejects_below_base_fee_with_max_fee_per_gas() -> anyhow::Result<()> {
     let (_rollup, client) = setup_client().await;
     let caller = client.address();
     let base_fee = current_base_fee(&client).await?;
-    assert!(base_fee > 0, "base fee should be non-zero for this test");
+    assert!(base_fee > 1, "base fee should be non-zero for this test");
 
     let request = TransactionRequest {
         max_fee_per_gas: Some(base_fee - 1),
         max_priority_fee_per_gas: Some(0),
         ..base_request(caller)
     };
-    let result: String = assert_rpc_succeeds(&client, "eth_call", &request).await;
-    assert_eq!(result, "0x");
-
+    assert_rpc_rejects::<String>(&client, "eth_call", &request, "TEST").await;
     Ok(())
 }
 
