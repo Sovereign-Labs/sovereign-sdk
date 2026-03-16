397ae8ab14738fef4fd80edd585e33b89e985821
bkolad <blazejkolad@gmail.com>
2026-03-16 11:35:21 +0100

    cleanup

--- crates/full-node/sov-aggregated-proof/program/src/main.rs
+++ crates/full-node/sov-aggregated-proof/program/src/main.rs
@@ -2,21 +2,20 @@
 
 sp1_zkvm::entrypoint!(main);
 
+use demo_stf::MultiAddressEvmSolana;
 use sha2::{Digest, Sha256};
 use sov_aggregated_proof_shared::DeferredProofInput;
 use sov_mock_da::MockDaSpec;
 use sov_mock_zkvm::MockZkvm;
+use sov_modules_api::configurable_spec::ConfigurableSpec;
 use sov_modules_api::da::BlockHeaderTrait;
-use sov_modules_api::default_spec::ConfigurableSpec;
 use sov_modules_api::execution_mode::Zk;
 use sov_modules_api::Spec;
 use sov_modules_api::StateTransitionPublicData;
 use sov_modules_api::Storage;
 use sov_sp1_adapter::SP1;
 
-type S = ConfigurableSpec<MockDaSpec, SP1, MockZkvm, MultiAddressEvmSolana, Zk>
-
-
+type S = ConfigurableSpec<MockDaSpec, SP1, MockZkvm, MultiAddressEvmSolana, Zk>;
 
 pub fn main() {
     let proof_inputs = sp1_zkvm::io::read::<Vec<DeferredProofInput>>();
