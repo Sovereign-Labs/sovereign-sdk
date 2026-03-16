7caa26492490d88a16e8f7866f2691d986e24635
Nikolai Golub <nikolai@sovlabs.io>
2026-03-16 16:13:34 +0100

    Adding simple viem-based for demo-rollup

---
+++ typescript/apps/demo-rollup-viem-tests/vitest.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    include: ["tests/**/*.integration-test.ts"],
+    testTimeout: 120_000,
+    hookTimeout: 120_000
+  }
+});
