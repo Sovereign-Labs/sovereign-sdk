f4083ec7be89f00d0e955c79db71bf9690d9e2d3
Bernhard Kauer <bernhard@sierra.team>
2026-03-27 14:21:59 +0100

    - fix usize overflow when compiling for 32-bit archs

--- crates/module-system/module-implementations/sov-uniqueness/src/window.rs
+++ crates/module-system/module-implementations/sov-uniqueness/src/window.rs
@@ -15,7 +15,7 @@ impl<S: Spec> Uniqueness<S> {
 	    nonce >= start,
 	    "Tx outdated for credential id: {credential_id}, expected at least: {start}, but found: {nonce}");
 
-        let delta = (nonce - start) as usize;
+        let delta = (nonce - start).try_into().unwrap_or(usize::MAX);
         let v = bits.get(delta / 8).copied().unwrap_or_default();
         anyhow::ensure!(
             v & (1 << (delta % 8)) == 0,
@@ -33,11 +33,14 @@ impl<S: Spec> Uniqueness<S> {
     ) -> anyhow::Result<()> {
         let (mut start, mut bits) = self.window.get(credential_id, state)?.unwrap_or_default();
 
+        // this assertion ensures that `check_window_uniqueness()` was executed beforehand
         assert!(nonce >= start);
 
         // drop outdated bits at the front first
         let drop = (nonce - start).saturating_sub(config_value!("PAST_TRANSACTION_WINDOW")) / 8;
-        let mut bits = bits.split_off((drop as usize).min(bits.len()));
+        let mut bits = bits.split_off(drop.min(bits.len() as u64) as usize);
+
+        // forward start to trail nonce by at most `PAST_TRANSACTION_WINDOW` bits
         start += drop * 8;
 
         // add new entries at the back
