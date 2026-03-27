432a0bff8647b70c2f39677a0d05ad01e437ecd7
Nikolai Golub <nikolai@sovlabs.io>
2026-03-27 11:59:35 +0100

    Adjusting tests after merging dev

--- examples/demo-rollup/tests/evm/evm_fee_history.rs
+++ examples/demo-rollup/tests/evm/evm_fee_history.rs
@@ -20,8 +20,9 @@ use sov_test_utils::test_rollup::TestRollup;
 
 use crate::evm::evm_test_helper::{
     alloy_client, create_simple_storage_client, deploy_contract_check,
-    estimate_gas_and_check_affordability, set_value_check, setup_test_rollup, EVM_EXTENSION,
-    HIGH_PRIORITY_FEE_PER_GAS, MAX_FEE_PER_GAS, SENDER_PRIV_KEY,
+    estimate_gas_and_check_affordability, set_value_check, setup_test_rollup,
+    setup_test_rollup_with_ideal_lag, EVM_EXTENSION, HIGH_PRIORITY_FEE_PER_GAS, MAX_FEE_PER_GAS,
+    SENDER_PRIV_KEY,
 };
 
 #[derive(Debug, Deserialize)]
@@ -1453,7 +1454,7 @@ async fn test_fee_history_consistent_query_methods() -> anyhow::Result<()> {
 /// TC34: Mixed history pattern - verify ratios match pattern [0, >0, 0, >0, >0]
 #[tokio::test(flavor = "multi_thread")]
 async fn test_fee_history_mixed_pattern() -> anyhow::Result<()> {
-    let rollup = setup_test_rollup(0, EVM_EXTENSION).await;
+    let rollup = setup_test_rollup_with_ideal_lag(0, EVM_EXTENSION, 0).await;
     let client = alloy_client(rollup.http_addr);
     rollup.wait_for_rollup_height_advance_by(2).await;
 
