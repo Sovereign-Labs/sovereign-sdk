77e9bca467d3506a083ed81a4a5292e25c06268b
Nikolai Golub <nikolai@sovlabs.io>
2026-04-09 08:59:58 +0200

    Mark test `bank_tx_tests_periodic_da_instant_finality` as flaky

--- examples/demo-rollup/tests/bank/zk_rollup/bank_periodic_da_tests.rs
+++ examples/demo-rollup/tests/bank/zk_rollup/bank_periodic_da_tests.rs
@@ -21,7 +21,7 @@ use crate::test_helpers::DemoRollupSpec;
 type TestSpec = DemoRollupSpec;
 
 #[tokio::test(flavor = "multi_thread")]
-async fn bank_tx_tests_periodic_da_instant_finality() -> anyhow::Result<()> {
+async fn flaky_bank_tx_tests_periodic_da_instant_finality() -> anyhow::Result<()> {
     sov_test_utils::initialize_logging();
     inner(0).await
 }
