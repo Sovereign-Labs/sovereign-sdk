5dda17fb6ac48bfe26769622f6e5d2ae4cd52983
Ross Weir <29697678+ross-weir@users.noreply.github.com>
2026-04-09 23:20:12 +1000

    Add network proving to soak tests (#2707)
    * wip
    
    * remove custom rollup blueprint impl
    
    * more work
    
    * add code commitment to genesis
    
    * use demo stf in sp1 setup
    
    * wip
    
    * updates
    
    * lints
    
    * re-add docs
    
    * refactor split soak testing code

--- examples/demo-rollup/sov-soak-testing/src/lib.rs
+++ examples/demo-rollup/sov-soak-testing/src/lib.rs
@@ -1,30 +1,27 @@
-use demo_stf::MultiAddressEvmSolana;
-use sov_celestia_adapter::verifier::CelestiaSpec;
-use sov_mock_da::{BlockProducingConfig, MockDaSpec};
-use sov_mock_zkvm::{MockZkvm, MockZkvmCryptoSpec};
-use sov_modules_api::configurable_spec::ConfigurableSpec;
+mod celestia;
+mod mock;
+mod sp1;
+
+pub use celestia::*;
+pub use mock::*;
+pub use sp1::*;
+
+use sov_mock_da::BlockProducingConfig;
+use sov_mock_zkvm::MockZkvmCryptoSpec;
 use sov_modules_api::Amount;
 use sov_paymaster::{
     PayeePolicy, PayerGenesisConfig, Paymaster, PaymasterConfig, PaymasterPolicyInitializer,
     SafeVec,
 };
-use sov_rollup_interface::da::DaSpec;
-use sov_rollup_interface::execution_mode::Native;
 use sov_rollup_interface::zk::CryptoSpec;
-use sov_sequencer::preferred::{ConfiguredNodeRole, PostgresConfig, PreferredSequencerConfig};
-use sov_sequencer::SequencerKindConfig;
+use sov_sequencer::preferred::{ConfiguredNodeRole, PostgresConfig};
 pub use sov_soak_testing_lib::*;
-use sov_state::nomt::prover_storage::NomtProverStorage;
-use sov_state::DefaultStorageSpec;
 use sov_synthetic_load::SyntheticLoad;
 use sov_test_utils::runtime::genesis::zk::config::HighLevelZkGenesisConfig;
 use sov_test_utils::runtime::genesis::zk::MinimalZkGenesisConfig;
-use sov_test_utils::test_rollup::{GenesisSource, RollupBuilder, StoragePath, TestRollup};
 use sov_test_utils::{
-    generate_runtime, RtAgnosticBlueprint, TestProver, TestSequencer, TestSpec, TestUser,
-    TEST_DEFAULT_USER_BALANCE,
+    generate_runtime, TestProver, TestSequencer, TestSpec, TestUser, TEST_DEFAULT_USER_BALANCE,
 };
-use std::path::PathBuf;
 
 pub const DEFAULT_BLOCK_TIME_MS: u64 = 200;
 pub const DEFAULT_BLOCK_PRODUCING_CONFIG: BlockProducingConfig = BlockProducingConfig::Periodic {
@@ -34,38 +31,8 @@ pub const DEFAULT_BLOCK_PRODUCING_CONFIG: BlockProducingConfig = BlockProducingC
 pub const DEFAULT_FINALIZATION_BLOCKS: u32 = 5;
 
 pub type TestRT = TestRuntime<TestSpec>;
-pub type RollupBlueprint = RtAgnosticBlueprint<TestSpec, TestRT>;
-pub type TestRollupBuilder = RollupBuilder<RollupBlueprint>;
 
-type SoakHasher = <MockZkvmCryptoSpec as CryptoSpec>::Hasher;
-
-// Celestia
-type CelestiaNativeStorage =
-    NomtProverStorage<DefaultStorageSpec<SoakHasher>, <CelestiaSpec as DaSpec>::SlotHash>;
-pub type CelestiaRollupSpec = ConfigurableSpec<
-    CelestiaSpec,
-    MockZkvm,
-    MockZkvm,
-    MultiAddressEvmSolana,
-    Native,
-    MockZkvmCryptoSpec,
-    CelestiaNativeStorage,
->;
-pub type DemoCelestiaRT = demo_stf::runtime::Runtime<CelestiaRollupSpec>;
-
-// Mock
-type MockNativeStorage =
-    NomtProverStorage<DefaultStorageSpec<SoakHasher>, <MockDaSpec as DaSpec>::SlotHash>;
-pub type MockDemoRollupSpec = ConfigurableSpec<
-    MockDaSpec,
-    MockZkvm,
-    MockZkvm,
-    MultiAddressEvmSolana,
-    Native,
-    MockZkvmCryptoSpec,
-    MockNativeStorage,
->;
-pub type DemoMockRT = demo_stf::runtime::Runtime<MockDemoRollupSpec>;
+pub(crate) type SoakHasher = <MockZkvmCryptoSpec as CryptoSpec>::Hasher;
 
 generate_runtime! {
     name: TestRuntime,
@@ -83,9 +50,9 @@ pub struct Setup {
     /// A user who is pre-registered as a payer for [`Setup::sequencer`].
     #[allow(dead_code)]
     pub paymaster: TestUser<TestSpec>,
-    /// The pre-registered sequencer
+    /// The pre-registered sequencer.
     pub sequencer: TestSequencer<TestSpec>,
-    /// The pre-registered prover
+    /// The pre-registered prover.
     pub prover: TestProver<TestSpec>,
     #[allow(missing_docs)]
     pub genesis_config: GenesisConfig<TestSpec>,
@@ -106,8 +73,8 @@ pub fn setup_roles_and_config() -> Setup {
         .push(paymaster.clone());
 
     let users: Vec<TestUser<TestSpec>> = vec![TestUser::generate_with_default_balance(); 20];
-
     genesis_config.additional_accounts_mut().extend(users);
+
     let genesis_config = GenesisConfig::from_minimal_config(
         genesis_config.into(),
         PaymasterConfig {
@@ -140,49 +107,11 @@ pub fn setup_roles_and_config() -> Setup {
     }
 }
 
-pub async fn setup_rollup(
-    storage_path: PathBuf,
-    axum_port: u16,
-    setup: Setup,
-    db_connection_url: Option<String>,
-) -> TestRollup<RollupBlueprint> {
-    let postgres_config = db_connection_url.map(|url| PostgresConfig {
+pub(crate) fn make_postgres_config(db_connection_url: Option<String>) -> Option<PostgresConfig> {
+    db_connection_url.map(|url| PostgresConfig {
         postgres_connection_string: url,
         node_id: "Primary".to_string(),
         node_role: ConfiguredNodeRole::Leader,
         leader_election: Default::default(),
-    });
-
-    let rollup_builder = TestRollupBuilder::new_with_storage_path(
-        GenesisSource::CustomParams(setup.genesis_config.clone().into_genesis_params()),
-        DEFAULT_BLOCK_PRODUCING_CONFIG,
-        DEFAULT_FINALIZATION_BLOCKS,
-        StoragePath::Buf(storage_path),
-        false,
-    )
-    .set_config(|config| {
-        config.telegraf_address = sov_metrics::MonitoringConfig::standard().telegraf_address;
-        config.automatic_batch_production = true;
-        config.rollup_prover_config = None;
-        config.sequencer_config = SequencerKindConfig::Preferred(PreferredSequencerConfig {
-            minimum_profit_per_tx: 0,
-            postgres_config,
-            ..Default::default()
-        });
-        config.prover_address = setup.prover.user_info.address().to_string();
-        config.aggregated_proof_block_jump = 3;
-        config.axum_port = axum_port;
-        if let SequencerKindConfig::Preferred(preferred_sequencer_config) =
-            &mut config.sequencer_config
-        {
-            preferred_sequencer_config.batch_execution_time_limit_millis = 400;
-        }
     })
-    .set_da_config(|da_config| {
-        da_config.sender_address = setup.sequencer.da_address;
-    });
-    rollup_builder
-        .start()
-        .await
-        .expect("Impossible to start rollup")
 }
