e6c217aa9ca7a750e9113dfddddf8a8f24bc2052
Theodore Bugnet <theodore@sovlabs.io>
2026-04-15 11:13:28 +0100

    fmt

--- crates/universal-wallet/schema/tests/integration/eip712.rs
+++ crates/universal-wallet/schema/tests/integration/eip712.rs
@@ -1,8 +1,8 @@
 use std::collections::BTreeMap;
 
 use borsh::{BorshDeserialize, BorshSerialize};
-use sov_universal_wallet::schema::Schema;
 use sov_universal_wallet::schema::safe_string::SafeString;
+use sov_universal_wallet::schema::Schema;
 use sov_universal_wallet::UniversalWallet;
 
 use crate::sov_rollup_interface;
