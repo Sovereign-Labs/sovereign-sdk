5735a46202a334ec60d29d5e68e6b240230ba32e
bkolad <blazejkolad@gmail.com>
2026-04-19 11:15:56 +0200

    fix

--- examples/demo-rollup/tests/resync/mod.rs
+++ examples/demo-rollup/tests/resync/mod.rs
@@ -15,9 +15,8 @@ use sov_api_spec::types::{SyncStatus, TxStatus};
 use sov_demo_rollup::{mock_zkvm_host_args, MockDemoRollup};
 use sov_full_node_configs::sequencer::SequencerKindConfig;
 use sov_modules_api::execution_mode::Native;
-use sov_modules_api::{OperatingMode, RawTx, Runtime, TxHash};
+use sov_modules_api::{CryptoSpec, OperatingMode, RawTx, Runtime, Spec, TxHash};
 use sov_modules_rollup_blueprint::logging::default_rust_log_value;
-use sov_sp1_adapter::crypto::private_key::SP1PrivateKey;
 use sov_test_utils::logging::LogCollector;
 use sov_test_utils::test_rollup::StoragePath;
 use sov_test_utils::test_rollup::{read_private_key, RollupBuilder, TestRollup};
@@ -64,7 +63,11 @@ const CHECK_TRANSACTION_VALUE: u64 = 13;
 const DA_SLOTS_TO_GENERATE: u64 = 100;
 const FINALIZATION_SLOTS: u32 = 5;
 
-fn tx_set_value_for_check(key: SP1PrivateKey, value: u64, generation: u64) -> RawTx {
+fn tx_set_value_for_check(
+    key: <<DemoRollupSpec as Spec>::CryptoSpec as CryptoSpec>::PrivateKey,
+    value: u64,
+    generation: u64,
+) -> RawTx {
     let msg: RuntimeCall<DemoRollupSpec> =
         RuntimeCall::SyntheticLoad(sov_synthetic_load::CallMessage::ReadAndSetHeavyState {
             number_of_new_values: value,
