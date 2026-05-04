dd05e3ebf05502dfa047d4c5b6e7f1b49f833500
bkolad <blazejkolad@gmail.com>
2026-05-04 17:27:54 +0200

    max_concurrent_proof_blob

--- crates/utils/sov-test-utils/src/lib.rs
+++ crates/utils/sov-test-utils/src/lib.rs
@@ -122,6 +122,9 @@ pub const TEST_NUM_CACHE_WARMUP_WORKERS: usize = 3;
 /// The maximum number of concurrent blobs.
 pub const TEST_MAX_CONCURRENT_BATCH_BLOBS: usize = 16;
 
+/// The maximum number of concurrent proof blobs.
+pub const TEST_MAX_CONCURRENT_PROOF_BLOBS: usize = 8;
+
 /// The default max fee to set for a transaction. This should be enough to be able to execute most standard transactions for the test rollup.
 pub const TEST_DEFAULT_MAX_FEE: Amount = Amount::new(100_000_000_000);
 /// The default gas limit to set for a transaction. This is an optional parameter.
