4cf139470a07a5fe32d046147183a61f9cfe0aaa
Nikolai Golub <nikolai@sovlabs.io>
2026-05-07 11:37:20 +0200

    update hyperlane image so tests are suppose to pass

--- crates/module-system/hyperlane/tests/integration/with_agent/helpers/mod.rs
+++ crates/module-system/hyperlane/tests/integration/with_agent/helpers/mod.rs
@@ -215,9 +215,9 @@ impl HyperlaneBuilder {
         let docker_image = env::var("CUSTOM_HLP_DOCKER_IMAGE");
         let has_custom_image = !matches!(docker_image, Err(env::VarError::NotPresent));
 
-        // Current image is based on https://github.com/Sovereign-Labs/hyperlane-monorepo/tree/sovereign-lander-integration
+        // Current image is based on https://github.com/Sovereign-Labs/hyperlane-monorepo/tree/sovereign-rust-integration-for-multisig
         let docker_image = docker_image
-            .unwrap_or_else(|_| "ghcr.io/theodorebugnet/hyperlane-agent:multisig_upgrade".into());
+            .unwrap_or_else(|_| "ghcr.io/citizen-stig/hyperlane-agent:multisig-upgrade-address-override".into());
         let (name, tag) = docker_image
             .split_once(':')
             .unwrap_or((&docker_image, "latest"));
