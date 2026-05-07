6c0316d27656ce06fc7757a4ba7ee2d2f698c2a3
bkolad <blazejkolad@gmail.com>
2026-05-07 09:55:38 +0200

    fix

--- examples/demo-rollup/tests/zk_proving/skip_proving_on_resync.rs
+++ examples/demo-rollup/tests/zk_proving/skip_proving_on_resync.rs
@@ -9,7 +9,15 @@ async fn skip_proving_on_resync() -> anyhow::Result<()> {
     let test_rollup = start_test_rollup(3, &external_da).await?;
     test_rollup.wait_for_rollup_height_advance_by(10);
 
-    let b = test_rollup.shutdown().await;
+    let _ = test_rollup.shutdown().await;
+
+    let header_subscription = external_da.service.subscribe_finalized_header().await?;
+    for i in 0..10 {
+        let header = header_subscription.next().await.unwrap().unwrap().await;
+        println!("Header {}", header.height)
+    }
+
+    let test_rollup = start_test_rollup(3, &external_da).await?;
 
     Ok(())
 }
