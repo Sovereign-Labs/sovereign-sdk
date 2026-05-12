58d81010aafd72b0a5beb343a2827954e01b7d84
Nikolai Golub <nikolay.v.golub@gmail.com>
2026-05-12 14:56:58 +0200

    Updates after rename

--- examples/demo-rollup/README_CELESTIA.md
+++ examples/demo-rollup/README_CELESTIA.md
@@ -290,7 +290,7 @@ Adding the following transaction to batch:
       }
     }
   },
-  "chain_hash": "0xde48058585f3408f8ad65e2b09c62e1f00fec71e1debd88ce23c40eb7e25b2c8",
+  "chain_hash": "0x09259df3baa3f9711d1077e2fab361e26aad7bdd332a5556d918afffdb193248",
   "details": {
     "max_priority_fee_bips": 0,
     "max_fee": "100000000",
--- examples/demo-rollup/tests/wallet/mod.rs
+++ examples/demo-rollup/tests/wallet/mod.rs
@@ -151,6 +151,9 @@ fn test_display_signed_tx() {
 #[ignore = "Ignored for rapid schema iteration, re-enable when CI timebomb goes off"]
 #[test]
 fn detect_schema_has_breaking_change() {
-    let current_hash = [0u8; 32];
+    let current_hash: [u8; 32] = [
+        9, 37, 157, 243, 186, 163, 249, 113, 29, 16, 119, 226, 250, 179, 97, 226, 106, 173, 123,
+        221, 51, 42, 85, 86, 217, 24, 175, 255, 219, 25, 50, 72,
+    ];
     assert_eq!(CHAIN_HASH, current_hash, "The chain hash changed. Update the \"current_hash\" value in this test but be aware: this is a breaking change for any production rollups.");
 }
