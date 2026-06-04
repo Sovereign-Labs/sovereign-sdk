723a330605e58b3c92211386eac57a4e2e0b8a41
Theodore Bugnet <theodore@sovlabs.io>
2026-06-04 21:28:23 +0100

    Correct the size tracking after tx is modified. Add big warning comment about misusing the finalize method.

--- crates/module-system/sov-modules-api/src/runtime/capabilities/sequencing_data.rs
+++ crates/module-system/sov-modules-api/src/runtime/capabilities/sequencing_data.rs
@@ -23,6 +23,24 @@ pub trait SequencingDataHandler<S: Spec> {
     fn create_sequencing_data(&self) -> Self::SequencingData;
 
     /// Finalizes sequencing data after native transaction execution.
+    ///
+    /// WARNING: this allows the sequencer to modify concensus data. If used carelessly, this can
+    /// easily result in a broken rollup. Implementations must take great care that their usecase
+    /// is consensus-safe.
+    ///
+    /// There are two main pitfalls to avoid:
+    /// 1. Causing a modified execution outcome. If the final `sequencing_data` is mutated in such
+    ///    a way that the execution output differs in *any* way (including subtle effects such as
+    ///    gas usage), this result cause a faulty sequencer.
+    /// 2. Growing the data size. The initial data size is used for batch size limit checks. If
+    ///    finalization here grows the data, this could result in an invalid batch, soft-locking
+    ///    and penalizing the preferred sequencer.
+    ///
+    /// A safe use for this would be if access logic uses the `scratchpad` to record
+    /// used entries in the SequencingData, and after execution the sequencer prunes any unused
+    /// entries to trim the transaction size. Since any entries accessed during execution remain
+    /// unchanged this would be guaranteed to not change execution outcomes, and shrinking the data
+    /// size is safe.
     #[cfg(feature = "native")]
     fn finalize_sequencing_data(
         &mut self,
